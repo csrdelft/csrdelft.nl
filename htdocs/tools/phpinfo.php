777ba7682078334af968ad8abb1d2270258634c0
jaep <jaep@localhost>
2007-07-22 19:21:44 +0000

    20:58 <@Jieter> Jaep: er is ook een p_admin

--- htdocs/tools/phpinfo.php
+++ htdocs/tools/phpinfo.php
@@ -1,7 +1,7 @@
 <?php
 require_once('/srv/www/www.csrdelft.nl/lib/include.config.php');
 
-if(!$lid->hasPermission('P_FORUM_MOD')){
+if(!$lid->hasPermission('P_ADMIN')){
 	header('location: '.CSR_ROOT);
 	exit;
 }
