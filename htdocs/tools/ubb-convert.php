e9e163bb500f3d748cd0b4ed57d5c1139f22bd1b
jieter <jieter@localhost>
2007-08-13 09:20:25 +0000

    Hier een eenvoudig scriptje om de bbcode-uid's uit de forumberichten en uit het nieuws te filteren...

---
+++ htdocs/tools/ubb-convert.php
@@ -0,0 +1,25 @@
+<?php
+#
+# Ubb-codes converteren naar de nieuwe parser...
+#
+
+require_once('include.config.php');
+
+
+//forum-posts
+$sPosts="SELECT id, tekst, bbcode_uid FROM forum_post;";
+$rPosts=$db->query($sPosts);
+while($aPost=$db->next($rPosts)){
+	$sPost=str_replace(':'.$aPost['bbcode_uid'], '', $aPost['tekst']);
+	$sNewPost="UPDATE forum_post SET tekst='".$sPost."' WHERE id=".$aPost['id']." LIMIT 1;";
+	$db->query($sNewPost);
+}
+//nieuws-berichten
+$sPosts="SELECT id, tekst, bbcode_uid FROM nieuws;";
+$rPosts=$db->query($sPosts);
+while($aPost=$db->next($rPosts)){
+	$sPost=str_replace(':'.$aPost['bbcode_uid'], '', $aPost['tekst']);
+	$sNewPost="UPDATE nieuws SET tekst='".$sPost."' WHERE id=".$aPost['id']." LIMIT 1;";
+	$db->query($sNewPost);
+}
+?>
