bd5030dbee0cbc4489834dad6b1b4dc3562c0fdb
jaep <jaep@localhost>
2007-08-14 21:57:27 +0000

    Wel ook bij nieuws die htmlentities eruitrossen natuurlijk

--- htdocs/tools/ubb-convert.php
+++ htdocs/tools/ubb-convert.php
@@ -11,6 +11,7 @@ $sPosts="SELECT id, tekst, bbcode_uid FROM forum_post;";
 $rPosts=$db->query($sPosts);
 while($aPost=$db->next($rPosts)){
 	$sPost=str_replace(':'.$aPost['bbcode_uid'], '', $aPost['tekst']);
+	$sPost=html_entity_decode($sPost);
 	$sNewPost="UPDATE forum_post SET tekst='".$sPost."' WHERE id=".$aPost['id']." LIMIT 1;";
 	$db->query($sNewPost);
 }
