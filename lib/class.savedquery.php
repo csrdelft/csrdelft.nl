50571bbaf8b9660f8a66e4e35e03adea78017de8
jieter <jieter@localhost>
2007-09-12 17:41:40 +0000

    - als het geselecteerde veld uid_naam is wordt het uid omgezet naar een link naar het profiel met de naam als tekst.

--- lib/class.savedquery.php
+++ lib/class.savedquery.php
@@ -39,36 +39,52 @@ class savedQuery{
 			$queryResult=$db->query($querydata['savedquery']);
 			$this->result=$db->result2array($queryResult);
 		}
-		
 	}
 	public function getHtml(){
-		
 		if(is_array($this->result)){
-			$return=$this->beschrijving.'<br />' .
-				'<table class="query_table">';
+			$return=$this->beschrijving.'<br /><table class="query_table">';
 			$keysPrinted=false;
 			$return.='<tr>';
 			foreach(array_keys($this->result[0]) as $kopje){
-				$return.='<th>'.$kopje.'</th>';
+				$return.='<th>';
+				if($kopje=='uid_naam'){
+					$return.='Naam';
+				}else{
+					$return.=$kopje;
+				}
+				$return.='</th>';
 			}
 			$return.='</tr>';
 			$rowColor=false;
 			foreach($this->result as $rij){
+				//kleurtjes omwisselen
 				if($rowColor){
 					$style='style="background-color: #ccc;"';
 				}else{
 					$style='';
 				}
 				$rowColor=(!$rowColor);
-
+				
+				//uit te poepen html maken
 				$return.='<tr>';
-				foreach($rij as $veld){
-					$return.='<td '.$style.'>'.$veld.'</td>';
+				foreach($rij as $key=>$veld){
+					$return.='<td '.$style.'>';
+					//als het veld uid als uid_naam geselecteerd wordt, een linkje 
+					//weergeven
+					if($key=='uid_naam'){
+						$lid=Lid::get_Lid();
+						$return.=$lid->getNaamLink($veld, 'full', true);
+					}else{
+						$return.=$veld;
+					}
+					$return.='</td>';
 				}
 				$return.='</tr>';
 			}
 			$return.='</table>';
 		}else{
+			//foutmelding in geval van geen resultaat, dus of geen query die bestaat, of niet
+			//voldoende rechten.
 			$return='Query ('.$this->queryID.') bestaat niet, of u heeft niet voldoende rechten.';
 		}
 		return $return;
