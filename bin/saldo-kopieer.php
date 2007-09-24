74bc2c7cf3e7f338a58ac6f724a7a0afe9cd237c
jieter <jieter@localhost>
2007-09-24 08:50:22 +0000

    - Scriptje om de SocCie-saldo-gegevens naar de lid-tabel te kopieren...

---
+++ bin/saldo-kopieer.php
@@ -0,0 +1,26 @@
+#!/usr/bin/php
+<?php 
+require_once('include.config.php');
+
+
+$saldiQuery="SELECT uid, soccieID, saldo, createTerm, maalSaldo FROM socciesaldi;";
+
+$result=$db->query($saldiQuery);
+
+while($data=$db->next($result)){
+	$updateLid=
+		"UPDATE lid 
+		SET
+			soccieID=".$data['soccieID'].",
+			createTerm=".$data['createTerm'].",
+			soccieSaldo=".$data['saldo'].",
+			maalcieSaldo=".$data['maalSaldo']."
+		WHERE uid='".$data['uid']."'
+		LIMIT 1;";
+	$db->query($updateLid);	 
+	
+}
+
+
+
+?>
