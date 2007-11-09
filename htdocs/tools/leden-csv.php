ff801ec7bf6a71e6307eb7919cfa9abbf2dab49a
jieter <jieter@localhost>
2007-11-09 11:21:38 +0000

    Hier een scriptje voor (o.a.) Daudey om zijn en mijn pauper te voorzien van recente gegevens...

---
+++ htdocs/tools/leden-csv.php
@@ -0,0 +1,34 @@
+<?php
+# C.S.R. Delft
+# -------------------------------------------------------------------
+# htdocs/tools/leden-csv.php
+# -------------------------------------------------------------------
+# Geeft de leden, gastleden, kringels en novieten terug in een csv-bestand.
+
+
+require_once('include.config.php');
+
+
+if(!$lid->hasPermission('P_LEDEN_READ')){ header('location: '.CSR_ROOT); }
+
+
+//header('content-type: text/csv');
+$sLedenQuery="
+	SELECT 
+		uid, voornaam, achternaam, tussenvoegsel, CONCAT(moot, '.', kring) AS kring,
+		adres, postcode, woonplaats, telefoon, mobiel, email,
+		o_adres, o_postcode, o_woonplaats, o_telefoon, 
+		gebdatum
+	FROM
+		lid
+	WHERE
+		status='S_LID' OR status='S_GASTLID' OR status='S_NOVIET' OR status='S_KRINGEL'
+	ORDER BY achternaam, voornaam;";
+
+$rLeden=$db->query($sLedenQuery);
+
+while($aLid=$db->next($rLeden)){
+	echo implode($aLid, ';')."\n";
+}
+
+?>
