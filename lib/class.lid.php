39011f95a266daabd79cd9cc9f251542f260b2f8
jaep <jaep@localhost>
2008-01-04 20:46:48 +0000

    `lart Jaep
    Nu heeft het bestuur dus rechten om courantdingen aan te passen.

--- lib/class.lid.php
+++ lib/class.lid.php
@@ -364,7 +364,7 @@ class Lid {
 		# extra dingen, waarvoor de array perm_user zelf nodig is
 		$this->_perm_user['P_PUBCIE']  = $this->_perm_user['P_MODERATOR'];
 		$this->_perm_user['P_MAALCIE'] = $this->_perm_user['P_LID'] | $p['P_MAAL_MOD'];
-		$this->_perm_user['P_BESTUUR'] = $this->_perm_user['P_LID'] | $p['P_LEDEN_MOD'] | $p['P_OUDLEDEN_READ'] | $p['P_NEWS_MOD'] | $p['P_MAAL_MOD'] | 'P_MAIL_COMPOSE' | $p['P_AGENDA_MOD'] | $p['P_FORUM_MOD'] | $p['P_DOCS_MOD'];
+		$this->_perm_user['P_BESTUUR'] = $this->_perm_user['P_LID'] | $p['P_LEDEN_MOD'] | $p['P_OUDLEDEN_READ'] | $p['P_NEWS_MOD'] | $p['P_MAAL_MOD'] | $p['P_MAIL_COMPOSE'] | $p['P_AGENDA_MOD'] | $p['P_FORUM_MOD'] | $p['P_DOCS_MOD'];
 		$this->_perm_user['P_VAB']     = $this->_perm_user['P_BESTUUR']  | $p['P_OUDLEDEN_MOD'];
 		$this->_perm_user['P_KNORRIE'] = $this->_perm_user['P_LID'] | $p['P_MAAL_MOD'];
 
