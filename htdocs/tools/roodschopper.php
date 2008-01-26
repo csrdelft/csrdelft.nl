c57c461973f05b4faba83f36df9124a420c0febd
jieter <jieter@localhost>
2008-01-26 13:00:59 +0000

    schopscriptje voor roode rakkers

---
+++ htdocs/tools/roodschopper.php
@@ -0,0 +1,40 @@
+<?php
+/*
+ * roodschopper.php	| 	Jan Pieter Waagmeester (jieter@jpwaag.com)
+ * 
+ * 
+ */
+ # instellingen & rommeltjes
+require_once('include.config.php');
+
+require_once('class.commissie.php');
+$soccie=$maalcie=new Commissie($db, $lid);
+$soccie->loadCommissie('SocCie');
+
+if($lid->getUid()!='0436'){ 
+	header('location: http://csrdelft.nl'); 
+}
+
+
+$query="SELECT uid, soccieSaldo FROM lid WHERE soccieSaldo<-6;";
+
+$result=$db->query($query);
+
+
+while($data=$db->next($result)){
+	$mail=new Smarty_csr();
+	
+	$mail->assign('uid', $data['uid']);
+	$mail->assign('saldo', number_format($data['soccieSaldo'], 2, ',', ''));	
+	
+	
+	$body=$mail->fetch('socciemail.tpl');
+	$to=$data['uid'].'@csrdelft.nl';
+	
+	mail($to, 'U staat rood bij de SocCie.', $body, "From: soccie@csrdelft.nl.nl\n\r");
+	
+	echo nl2br($body).'<hr />';
+}
+	
+	
+?>
---
+++ lib/templates/socciemail.tpl
@@ -0,0 +1,18 @@
+Beste {$uid|csrnaam:civitas:false},
+
+U staat rood bij de SocCie, uw saldo is  € {$saldo}. U wordt verzocht zo snel 
+mogelijk te zorgen dat saldo weer boven het nulpunt komt. Dat kan door in te 
+leggen bij de bar op de borrel, of geld over te maken naar de SocCie-fiscus:
+
+C.S.R. fiscus sociëteitscommissie
+31.94.36.403
+
+Zo helpt u de liquiditeit van de SocCie op peil te houden, en teleurstellingen 
+aan de bar te voorkomen.
+Mocht u recent geld hebben overgemaakt, dan kunt u dit bericht als niet 
+verzonden beschouwen.
+
+Met vriendelijke groet,
+
+am. Waagmeester, 
+h.t. praeses der SocCie der C.S.R.
