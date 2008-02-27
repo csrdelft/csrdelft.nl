9d6b6508a407778ca065e9c779bacba9fd27bdf8
csrdelft <csrdelft@localhost>
2008-02-27 22:03:17 +0000

    (Jieter)  - Ook nog maar even wat toegangscontrole, wel zo lief...

--- htdocs/tools/saldografiek.php
+++ htdocs/tools/saldografiek.php
@@ -9,7 +9,7 @@ require_once('include.config.php');
 require_once('chart-0.8/chart.php');
 require_once('class.saldi.php');
 
-if(isset($_GET['uid']) AND $lid->isValidUid($_GET['uid'])){
+if(isset($_GET['uid']) AND $lid->isValidUid($_GET['uid']) AND ($lid->hasPermission('P_ADMIN') OR $lid->getUid()==$_GET['uid'])){
 	
 	
 	$maalcie=new Saldi($_GET['uid'], 'maalcie');
@@ -23,7 +23,7 @@ if(isset($_GET['uid']) AND $lid->isValidUid($_GET['uid'])){
 	$chart->add_legend('SocCie', 'blue');
 	$chart->add_legend('MaalCie', 'red');
 
-	$chart->set_margins(40, 10, 20, 23);
+	$chart->set_margins(60, 10, 20, 23);
 	$chart->set_labels(false, 'Saldo [euro]');
 	$chart->stroke();
 }
