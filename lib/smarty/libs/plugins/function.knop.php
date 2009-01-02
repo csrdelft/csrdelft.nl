faeb3f1c435b5656eeee5fab3ba07b57ff15f159
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2009-01-02 14:31:11 +0000

    - knopjesklasse toegevoegd  - knopjes smarty-functie toegevoegd.

---
+++ lib/smarty/libs/plugins/function.knop.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Smarty plugin
+ * @package Smarty
+ * @subpackage plugins
+ */
+
+
+/**
+ * Smarty {knop} function plugin
+ *
+ * Type:     function<br>
+ * Name:     knop<br>
+ * Date:     May 21, 2002
+ * Purpose:  Knopjes generator zodat je dat niet 100 keer opniew intypt.
+ * Input:
+ *
+ * Examples:
+ * <pre>
+	{knop }
+ * </pre>
+ * @link http://feuten.csrdelft.nl
+ * @version  1.2
+ * @author   Jan Pieter Waagmeester <jpwaag@jpwaag.com>
+ * @param    array
+ * @param    Smarty
+ * @return   string
+ */
+require_once('class.knop.php');
+
+function smarty_function_knop($params, &$smarty){
+    if(!isset($params['url'])){
+        $smarty->trigger_error("knop: url niet opgegeven");
+        return;
+    }
+    $knop=new Knop($params['url']);
+
+    if(isset($params['type'])){
+    	$knop->setType($params['type']);
+    }
+	if(isset($params['text'])){
+		$knop->setText($params['text']);
+	}elseif(isset($params['tekst'])){
+		$knop->setText($params['tekst']);
+	}
+	if(isset($params['confirm'])){
+		$knop->setConfirm($params['confirm']);
+	}
+
+    return $knop->getHtml();
+}
+
+
+/* vim: set expandtab: */
+
+?>
