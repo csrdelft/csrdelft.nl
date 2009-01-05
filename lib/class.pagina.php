34cdec025a6fd16c899cfa6beb020b2cab83bfe4
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2009-01-05 12:54:01 +0000

    - gerommel in de marge aka mieren-intimiteiten.

--- lib/class.pagina.php
+++ lib/class.pagina.php
@@ -17,27 +17,28 @@ class Pagina{
 	private $sRechtenBekijken='P_NOBODY';
 	private $sRechtenBewerken='P_ADMIN';
 
-	function Pagina($sNaam){
+	public function __construct($sNaam){
 		$this->_lid=Lid::instance();
 		$this->_db=MySql::instance();
 
 		$this->sNaam=$sNaam;
 		$this->load();
 	}
-	
-	function getPaginas(){
+
+	public static function getPaginas(){
+		$db=MySql::instance();
 		$sPaginasQuery="SELECT naam, titel, rechten_bewerken FROM pagina ORDER BY titel ASC";
-		$rPaginas=$this->_db->query($sPaginasQuery);
+		$rPaginas=$db->query($sPaginasQuery);
 		$aPaginas=array();
-		while($aPagina=$this->_db->next($rPaginas)){
-			if($this->_lid->hasPermission($aPagina['rechten_bewerken'])){
+		while($aPagina=$db->next($rPaginas)){
+			if(Lid::instance()->hasPermission($aPagina['rechten_bewerken'])){
 				$aPaginas[]=$aPagina;
-			}			
+			}
 		}
 		return $aPaginas;
 	}
 
-	function load(){
+	public function load(){
 		$sPaginaQuery="SELECT titel, inhoud, rechten_bekijken, rechten_bewerken FROM pagina WHERE naam='".$this->_db->escape($this->sNaam)."'";
 		$rPagina=$this->_db->query($sPaginaQuery);
 		if($this->_db->numRows($rPagina)>0){
@@ -54,7 +55,7 @@ class Pagina{
 		}
 	}
 
-	function save(){
+	public function save(){
 		$sPaginaQuery = "UPDATE pagina SET titel='".$this->_db->escape($this->getTitel())."', inhoud='".$this->_db->escape($this->getInhoud())."', rechten_bekijken='".$this->_db->escape($this->sRechtenBekijken)."', rechten_bewerken='".$this->_db->escape($this->sRechtenBewerken)."' WHERE naam = '".$this->_db->escape($this->getNaam())."'";
 		$this->_db->query($sPaginaQuery);
 		if($this->_db->affected_rows()==0){
@@ -63,45 +64,45 @@ class Pagina{
 		}
 	}
 
-	function setRechtenBekijken($sRechten){
+	public function setRechtenBekijken($sRechten){
 		$this->sRechtenBekijken=$sRechten;
 	}
-	function getRechtenBekijken(){
+	public function getRechtenBekijken(){
 		return $this->sRechtenBekijken;
 	}
-	function magBekijken(){
+	public function magBekijken(){
 		return $this->_lid->hasPermission($this->sRechtenBekijken);
 	}
-	function setRechtenBewerken($sRechten){
+	public function setRechtenBewerken($sRechten){
 		$this->sRechtenBewerken=$sRechten;
 	}
-	function getRechtenBewerken(){
+	public function getRechtenBewerken(){
 		return $this->sRechtenBewerken;
 	}
-	function magBewerken(){
+	public function magBewerken(){
 		return $this->_lid->hasPermission($this->sRechtenBewerken);
 	}
-	function magPermissiesBewerken(){
+	public function magPermissiesBewerken(){
 		return $this->_lid->hasPermission('P_ADMIN');
 	}
 
-	function setTitel($sTitel){
+	public function setTitel($sTitel){
 		$this->sTitel=$sTitel;
 	}
 
-	function setInhoud($sInhoud){
+	public function setInhoud($sInhoud){
 		$this->sInhoud=$sInhoud;
 	}
 
-	function getNaam(){
+	public function getNaam(){
 		return $this->sNaam;
 	}
 
-	function getTitel(){
+	public function getTitel(){
 		return $this->sTitel;
 	}
 
-	function getInhoud(){
+	public function getInhoud(){
 		return $this->sInhoud;
 	}
 }
