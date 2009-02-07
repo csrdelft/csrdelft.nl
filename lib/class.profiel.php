3f5f59d88e7143b9c97f62528990f61964813fca
Hans van Kranenburg <hans.van.kranenburg@localhost>
2009-02-07 21:26:08 +0000

    P_LEDEN_EDIT kan wachtwoord wijzigen zonder oude wachtwoord in te vullen... graag code review w.b. de logica if-else etc

--- lib/class.profiel.php
+++ lib/class.profiel.php
@@ -355,12 +355,11 @@ class Profiel extends Lid{
 			$nwpass2 = strval($form['nwpass2']);
 
 			$tmperror='';
-			# alleen actie ondernemen als er een oud password is ingevuld
-			if ($oldpass != "" or $nwpass != "" or $nwpass2 != "") {
-				if ($oldpass == "" and ($nwpass != "" or $nwpass2 != "")) {
-					$this->_formerror['oldpass'] = "Vul ook uw huidige wachtwoord in:";
+			# alleen actie ondernemen als er een oud password is ingevuld of als 
+			# de uitvoerende instantie P_LEDEN_EDIT heeft
+			if ($oldpass != "" or $this->hasPermission('P_LEDEN_EDIT') ) {
 				# we kijken of het oude wachtwoord klopt
-				} elseif (!$this->_checkpw($this->_profile['password'], $oldpass)) {
+				if (!$this->hasPermission('P_LEDEN_EDIT') and !$this->_checkpw($this->_profile['password'], $oldpass)) {
 					$this->_formerror['oldpass'] = "Het huidige wachtwoord is onjuist:";
 				# of er wat nieuws is ingevuld...
 				} elseif ($nwpass == "" and $nwpass2 == "") {
@@ -379,6 +378,8 @@ class Profiel extends Lid{
 					# nieuwe waarde voor diff_to_*
 					$this->_tmpprofile['password'] = $hash;
 				}
+			} else {
+				$this->_formerror['oldpass'] = "Vul ook uw huidige wachtwoord in:";
 			}
 		}
 
@@ -386,7 +387,7 @@ class Profiel extends Lid{
 		# als hun naam, hun studiejaar etc, om de oudledenlijst compleet te krijgen.
 		# De Vice-Abactis kan de info van iedereen wijzigen.
 
-		if ($this->_profile['status'] == 'S_OUDLID' or $this->hasPermission('P_LEDEN_MOD')) {
+		if ($this->_profile['status'] == 'S_OUDLID' or $this->hasPermission('P_LEDEN_EDIT')) {
 
 			# Info over naam => verplichte velden! (ook vanwege sn/cn velden in ldap!)
 			$velden = array('voornaam' => 50, 'achternaam' => 50);
@@ -500,7 +501,7 @@ class Profiel extends Lid{
 		}
 
 		# Extra velden die gewijzigd kunnen worden door am. Vice-Abactis
-		if ($this->hasPermission('P_LEDEN_MOD')) {
+		if ($this->hasPermission('P_LEDEN_EDIT')) {
 
 			$velden = array('postfix' => 7, 'voornamen' => 100, 'kerk' => 50, 'muziek' => 100);
 			# voor al deze veldnamen...
@@ -739,8 +740,6 @@ class Profiel extends Lid{
 				*/
 				# lege velden er uit gooien
 				foreach ($entry as $i => $e) if ($e == '') unset ($entry[$i]);
-
-				# if ($this->hasPermission('P_LEDEN_MOD')) print_r($entry);
 
 				# LDAP verbinding openen
 				$ldap = new LDAP();
