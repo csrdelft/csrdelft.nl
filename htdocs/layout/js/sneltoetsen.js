c2a99c5555049f2953e2d6e9406d514f3789f83e
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2009-06-25 13:00:45 +0000

    - Profiel en instellingen erbij

--- htdocs/layout/js/sneltoetsen.js
+++ htdocs/layout/js/sneltoetsen.js
@@ -1,4 +1,5 @@
 $(document).observe("keydown", function(event){
+	//alert(event.keyCode);
 	if(event.keyCode==66){ //66 = b voor besturen.
 		location.href = "http://csrdelft.nl/actueel/groepen/Besturen/";
 		event.stop();
@@ -11,10 +12,19 @@ $(document).observe("keydown", function(event){
 		location.href = "http://csrdelft.nl/communicatie/forum/categorie/laatste";
 		event.stop();
 	}
+	if(event.keyCode==73){ //73 = i voor instellingen
+		location.href = "http://csrdelft.nl/instellingen";
+		event.stop();
+	}
 	if(event.keyCode==77){ //77 = m voor forum
 		location.href = "http://csrdelft.nl/actueel/mededelingen";
 		event.stop();
 	}
+	if(event.keyCode==80){ //80 = p voor instellingen
+		location.href = "http://csrdelft.nl/communicatie/profiel";
+		event.stop();
+	}
+	
 	
 });
 
