88036d9045e2070b2ad13ed98711f75c1d4ac92a
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2009-09-01 15:51:55 +0000

    ook weer terug naar de gewone lijst met linkjes.

--- htdocs/layout/js/verticalen.js
+++ htdocs/layout/js/verticalen.js
@@ -1,14 +1,20 @@
-var orig;
+var kringen=new Array();
 
 function toggleEmails(vertkring){
-
-	//request doen voor de tab-inhoud
-	http.abort();
-	http.open("GET", "/communicatie/verticalen.php?email="+vertkring, true);
-	http.onreadystatechange=function(){
-		if(http.readyState == 4){
-			document.getElementById('leden'+vertkring).innerHTML=http.responseText;
-		}
-	};
-	http.send(null);
+	if(typeof(kringen[vertkring])=='undefined'){
+		//request doen voor de tab-inhoud
+		http.abort();
+		http.open("GET", "/communicatie/verticalen.php?email="+vertkring, true);
+		http.onreadystatechange=function(){
+			if(http.readyState == 4){
+				kringen[vertkring]=document.getElementById('leden'+vertkring).innerHTML;
+				document.getElementById('leden'+vertkring).innerHTML=http.responseText;
+			}
+		};
+		http.send(null);
+	}else{
+		document.getElementById('leden'+vertkring).innerHTML=kringen[vertkring];
+		delete kringen[vertkring];
+	}
+	console.log(kringen);
 }
