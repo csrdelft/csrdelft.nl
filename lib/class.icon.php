0da4562466cc54068322a2047d8e251915596aae
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2009-11-04 13:23:22 +0000

    - Centrale Icon-fabriek.

---
+++ lib/class.icon.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * Icon dingetje voor csrdelft.nl.
+ *
+ *
+ * Icon::get('bewerken'); geeft bijvoorbeeld http://plaetjes.csrdelft.nl/famfamfam/pencil.png
+ */
+define('ICON_PATH', PICS_PATH.'/famfamfam/');
+
+class Icon{
+
+	//array met alle icons
+	public static $index=null;
+
+	//handige dingen die we graag gebruiken in csrdelft.nl. Moeten geen namen zijn die al voorkomen
+	//in de lijst met icons.
+	public static $alias=array(
+		'bewerken'=>'pencil',
+		'verwijderen'=>'cross',
+		'citeren'=>'comments',
+		'plakkerig'=>'note',
+		'rss'=>'feed'
+	);
+
+	
+	private static function loadIndex(){
+		if(!file_exists(ICON_PATH.'.index')){
+			self::generateIndex();
+		}
+		self::$index=explode(',', file_get_contents( ICON_PATH.'.index'));
+	}
+	
+	public static function has($key){
+		if(self::$index===null){ self::loadIndex(); }
+		//Bestaat er een $key in de index of een alias met met $key?
+		return in_array($key, self::$index) OR (self::isAlias($key) AND self::has(self::getKeyForAlias($key)));
+	}
+	public static function isAlias($alias){
+		return array_key_exists($alias, self::$alias);
+	}
+	public static function getKeyForAlias($alias){
+		if(!self::isAlias($alias)){
+			throw new Exception('Alias ('.$alias.') bestaat niet');
+		}
+		return self::$alias[$alias];
+	}
+	
+	public static function get($key){
+		if(!self::has($key)){
+			throw new Exception('Icon ('.$key.') bestaat niet in images/famfamfam/');
+		}
+		if(in_array($key, self::$index)){
+			return CSR_PICS.'famfamfam/'.$key.'.png';
+		}else{
+			return CSR_PICS.'famfamfam/'.self::$alias[$key].'.png';
+		}
+	}
+	public static function getTag($key, $class='icon'){
+		$icon=self::get($key);
+		return '<img src="'.$icon.'" alt="'.$key.'" class="'.htmlspecialchars($class).'" />';
+	}
+
+	/*
+	 * Bouw de index op in een bestand in de image-map.
+	 */
+	public static function generateIndex(){
+		$handler = opendir(ICON_PATH);
+		while($file = readdir($handler)) {
+			//we willen geen directories en geen verborgen bestanden.
+			if(!is_dir(ICON_PATH.$file) AND substr($file,0,1)!='.' AND substr($file, -3)=='png'){
+				$icons[]=substr($file, 0, (strlen($file)-4));
+			}
+		}
+		closedir($handler);
+		file_put_contents(ICON_PATH.'.index', implode($icons, ','));
+	}
+}
+?>
