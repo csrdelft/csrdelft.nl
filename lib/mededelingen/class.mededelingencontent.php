9b0f7fe199354ad6278dae7c19ede00eb479a578
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2009-11-15 18:45:31 +0000

    - wat handige smarty-meuk in de mededelingtemplate toegepast

--- lib/mededelingen/class.mededelingencontent.php
+++ lib/mededelingen/class.mededelingencontent.php
@@ -15,16 +15,14 @@ class MededelingenContent extends SimpleHTML{
 
 		$this->topMost=Mededeling::getTopmost(self::aantalTopMostBlock); // Get the n most important mededelingen.
 		
-		if($mededelingId!=0)
-		{
+		if($mededelingId!=0){
 			try{
 				$this->selectedMededeling=new Mededeling($mededelingId);
 			} catch (Exception $e) {
 				// Do nothing, keeping $selectedMededeling equal to null.
 			}
 		}
-		if($this->selectedMededeling===null)
-		{
+		if($this->selectedMededeling===null){
 			// If there is at least one topmost, make it the selected one.
 			// Otherwise, keep $this->selectedMededeling equal to null.
 			if(isset($this->topMost[0]))
@@ -33,31 +31,28 @@ class MededelingenContent extends SimpleHTML{
 	}
 	
 	public function setPaginaNummer($pagina){
-		if(is_numeric($pagina) AND $pagina>=1)
-		{
+		if(is_numeric($pagina) AND $pagina>=1){
 			$this->paginaNummerOpgevraagd=true;
 			$this->paginaNummer=$pagina;
 		}
 	}
 
 	public function view(){
-		if(!$this->paginaNummerOpgevraagd)
+		if(!$this->paginaNummerOpgevraagd){
 			$this->paginaNummer = $this->selectedMededeling->getPaginaNummer();
-
+		}
+		
 		$content=new Smarty_csr();
 		define( 'NIEUWS_ROOT', '/actueel/mededelingen/');
 
 		$content->assign('melding', $this->getMelding());
 		$content->assign('nieuws_root', NIEUWS_ROOT);
-		$content->assign('csr_pics', CSR_PICS);
 		
 		$content->assign('topmost', $this->topMost);
 		$content->assign('lijst', Mededeling::getLijstVanPagina($this->paginaNummer, self::aantalPerPagina));
 		// The following attribute can't be null. Otherwise, the page will
 		// not display a full Mededeling.
 		$content->assign('selectedMededeling', $this->selectedMededeling);
-		$content->assign('lidtag', '[lid='.$this->selectedMededeling->getUid().']');
-		$content->assign('ubb', CsrUBB::instance());
 		
 		$content->assign('huidigePagina', $this->paginaNummer);
 		$content->assign('totaalAantalPaginas', (ceil(Mededeling::getAantal()/self::aantalPerPagina)));
