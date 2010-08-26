2ad28efca22661385de325832f12bc2a87dd4994
Jurjen de Jong <jurjen.de.jong@localhost>
2010-08-26 16:06:09 +0000

    * Owee layout teruggedraaid  * Een aantal NovCie vragen weer verwijderd  * Nieuwe sql die de vorige twee vervangt.

---
+++ sql/2003-novcievragen.sql
@@ -0,0 +1,3 @@
+ALTER TABLE  `lid` ADD  `ovkaart` VARCHAR( 50 ) CHARACTER SET ucs2 COLLATE ucs2_general_ci NOT NULL ,
+ADD  `zingen` VARCHAR( 100 ) CHARACTER SET ucs2 COLLATE ucs2_general_ci NOT NULL ,
+ADD  `novitiaat` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL';
