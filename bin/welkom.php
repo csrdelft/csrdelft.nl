7f9ba7f6e6cc5e4a3e814606f8c2a13bfb7703c6
Jurjen de Jong <jurjen.de.jong@localhost>
2010-08-29 18:48:40 +0000

    * Update van welkom mail aan de novieten

--- bin/welkom.php
+++ bin/welkom.php
@@ -3,7 +3,7 @@
 # Scriptje om voor sjaars een wachtwoord te genereren en dat toe te mailen.
 # Vergeet niet voor gebruik hieronder het jaar aan te passen.
 
-$jaar = '09';
+$jaar = '10';
 
 require_once('include.config.php');
 
@@ -29,7 +29,7 @@ Beste noviet {$sjaars['voornaam']},
 Bij je lidmaatschap van C.S.R. hoort ook de mogelijkheid om in te loggen op de C.S.R.-webstek.
 
 Via de webstek kun je onder andere:
-- Je aanmelden voor verenigingsmaaltijden, en je maaltijdabonnementen beheren
+- Je aanmelden voor verenigingsmaaltijden, en je maaltijdabonnementen beheren. (Direct doen onder: Actueel->Maaltijden->Instellingen)
 - Berichten lezen en plaatsen op het forum
 - Berichten plaatsen in de C.S.R.-courant, die wekelijks aan alle leden wordt verzonden
 - Gegevens van andere leden opzoeken
@@ -41,12 +41,12 @@ Wachtwoord: {$pass}
 Nadat je bent ingelogd kun je het wachtwoord veranderen, en een bijnaam instellen die je in plaats van je lidnummer kunt gebruiken om in te loggen.
 
 Wanneer je problemen hebt met inloggen, of andere vragen over de webstek, kun je terecht bij de PubliciteitsCommissie.
-Kom even langs in ons IRC-kanaal #pubcie (zie de pagina daarover op de webstek onder Communicatie), of stuur een e-mail.
+Stuur dan een e-mail of kom even langs in ons IRC-kanaal #pubcie (zie Communicatie->IRC), of stuur een e-mail.
 
 Met vriendelijke groet,
 
-Gerrit Uitslag
-h.t. Praeses der PubliciteitsCommissie der Civitas Studiosorum Reformatorum
+Jurjen de Jong
+h.t. PubCie-Praeses der Civitas Studiosorum Reformatorum
 EOD;
 
 		mail ($sjaars['email'],"Inloggegevens C.S.R.-webstek",$tekst,"From: PubliciteitsCommissie C.S.R. Delft <pubcie@csrdelft.nl>\nContent-Type: text/plain; charset=utf-8\nBcc: pubcie@csrdelft.nl");
