e118d96ad3607e27a587594e69e496238f1ee42d
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2010-09-26 13:29:29 +0000

    minder avonden, spaties kunnen dan ook wel

--- lib/class.eetplancontent.php
+++ lib/class.eetplancontent.php
@@ -123,7 +123,7 @@ class EetplanContent extends SimpleHTML {
 			echo '<tr class="kleur'.($row%2).'"><td><a href="/actueel/eetplan/sjaars/'.$aEetplanVoorPheut[0]['uid'].'">'.$aEetplanVoorPheut[0]['naam'].'</a></td>';
 			for($iTeller=1;$iTeller<=4;$iTeller++){
 				$huisnaam=$aHuizenArray[$aEetplanVoorPheut[$iTeller]-1]['huisNaam'];
-				$huisnaam=str_replace(array('Huize ', 'De ', '-', ' '), '', $huisnaam);
+				$huisnaam=str_replace(array('Huize ', 'De '), '', $huisnaam);
 				$huisnaam=substr($huisnaam, 0, 15);
 
 				echo '<td class="huis"><a href="/actueel/eetplan/huis/'.$aEetplanVoorPheut[$iTeller].'">'.
