04c40a35ab08afd41007114f78fd24396de3bdd9
Jan Pieter Waagmeester <jan.pieter.waagmeester@localhost>
2010-12-27 15:32:17 +0000

    nog een 'inlog'constructie en een beetje opruimdingen en werkelijk uit de db selecteren van bestellingen

--- htdocs_confide/index.php
+++ htdocs_confide/index.php
@@ -4,6 +4,8 @@
  */
 
 define('ETC_PATH', '.');
+session_start();
+
 require_once 'common.functions.php';
 require_once 'mysql.class.php';
 require_once 'streeplijstrapportage.php';
@@ -29,23 +31,13 @@ $weekrapportQuery="
 	WHERE Tijdstip>'".$start."' AND Bedrag!=0
 	GROUP BY week
 	ORDER BY week DESC;";
+$weekResult=$db->query($weekrapportQuery);
 
-$res=array(
-array(
-	'week' => "49",
-	'omzet' => "157.65",
-	'aantal_bestellingen' => "198",
-	'bestelstring'=>"rbbbblbbbflllffffbbbbblllllbbbbllfxllllllfflflllllfmlmlmlmlmfllmlliifb4b3bfbf3fffb4ffffffbbblll5blsffbbb4bibllfl4bllbfmgmffbbbbbbfbbffbfffflllll6ilbfbbbbbllmmffbbbbbiibbliiiihihibbbfllffblllbbbflbbppfllfbbssggpffbbffbbbbbbbfllbzzbbblbflllfppppbbbfbfflllllliiifsffbbbpflbsssssggpbbbll35bfflfbbxbbllffllllfbbllllfibbbbbbbb5bf4bf4bll4bbfbbbb2l3bllf5b5bbbllfllbbbbblfllliiiillll5bfii500ollbll5bbbll3bllddl6bxssbbllbbbblflllllllbbllzzsfblffblls2b2f2g2lbb2l4bllbbbbbbbzbii3b"),
-array(
-	'week' => "48",
-	'omzet' => "70.00",
-	'aantal_bestellingen' => "29",
-	'bestelstring' => "zllssssssssssgffxbbxdddlllffxfxgfbbxgfxlgff4bgw6b2f2l3l1b2l"));
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xml:lang="nl" xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<title>SocCie streeepcomputerrapportage</title>
+	<title>SocCie streepcomputerrapportagegenereertool</title>
 	<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
 	<meta name="author" content="PubCie C.S.R. Delft - Jieter" />
 	<meta name="robots" content="nfollow" />
@@ -53,15 +45,38 @@ array(
 	<link rel="shortcut icon" href="http://plaetjes.csrdelft.nl/layout/favicon.ico" />
 </head>
 <body>
+
+<?php
+// 'inloggen'
+if(!(isset($_SESSION['authenticated']) AND $_SESSION['authenticated'])){
+	if(isset($_POST['password']) AND md5($_POST['password'])=='3f83075f710a248c7786cf72d0e501ce'){
+		$_SESSION['authenticated']=true;
+	}else{
+		?>
+		<h1>SoccieStreeplijstrapportagegeneratortool login</h1>
+
+		<form method="post">
+			<input type="password" name="password" /><input type="submit" value="inloggen" />
+		</form>
+		<?php
+
+		exit;
+	}
+}
+
+?>
 <h1>Streeplijstrapportage SocCie</h1>
-Let op: omzetcijfers kloppen niet voor de weken  de geschiedenis v&oacute;&oacute;r prijswijzigingen.
+Let op: cijfers kloppen na prijswijzigingen niet voor de weken v&oacute;&oacute;r de wijziging. Weeknummer zijn <a href="http://en.wikipedia.org/wiki/ISO_week_date">ISO-weken</a>: van maandag tot zondag dus...
+<a href="?start=6">half jaar</a>
+<a href="?start=12">1 jaar</a>
+<a href="?start=24">2 jaar</a>
 <?php
 echo '<table class="weken">
 		<tr>
 			<th>Week</th><th>#bestellingen</th><th>detail</th><th>inleg</th><th>Omzet</th>
 		</tr>';
 
-foreach($res as $row){
+while($row=$db->next($weekResult)){
 	$week=$row['week'];
 	echo '<tr>';
 	echo '<td>'.$week.'</td>';
@@ -74,28 +89,28 @@ foreach($res as $row){
 
 	echo '<td>';
 
-	echo '<a class="handje" onclick="document.getElementById(\'details-'.$week.'\').style.display=\'block\'; this.parentNode.removeChild(this);">Toon details</a>';
-	echo '<table id="details-'.$week.'" class="artikelen verborgen"><tr><th>artikel</th><th>aantal</th><th>omzet</th></tr>';
+	echo '<a class="handje knop" onclick="document.getElementById(\'details-'.$week.'\').style.display=\'block\'; this.parentNode.removeChild(this);">&raquo; Toon details</a>';
+	echo '<table id="details-'.$week.'" class="artikelen verborgen"><tr><th>artikel</th><th>#</th><th>omzet</th></tr>';
 	foreach($bestellingen as $letter => $aantal){
 		$artikelomzet=$artikelen[$letter]['prijs']*$aantal;
 		echo '<tr><td>'.$artikelen[$letter]['naam'].'</td>';
 		echo '<td>'.$aantal.'</td>';
 		echo '<td class="euro">'.euro($artikelomzet).'</td>';
 		echo '</tr>';
-		if($artikelomzet<0){
+		if($letter=='h'){
 			$inleg+=$artikelomzet;
 		}else{
-			$omzet+=$artikelomzet;
+			if($artikelomzet<0){
+				$inleg+=$artikelomzet;
+			}else{
+				$omzet+=$artikelomzet;
+			}
 		}
-
 	}
 	echo '</table></td>';
 	echo '<td>'.euro(0-$inleg).'</td>';
-		echo '<td>'.euro($omzet).'</td>';
-
-
+	echo '<td>'.euro($omzet).'</td>';
 	echo '</tr>';
-
 }
 ?>
 </table>
