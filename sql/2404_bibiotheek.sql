9be5ff6c0a43e0cec815174cb9338c64cf6c96e1
Gerrit Uitslag <gerrit.uitslag@localhost>
2011-08-14 23:24:02 +0000

    sql ontdaan van database vullen. Kladbestand opgeruimd. Zover ik weet alle wijzigingen van de db.

--- sql/2404_bibiotheek.sql
+++ sql/2404_bibiotheek.sql
@@ -1,3 +1,4 @@
+ 
 
 UPDATE  `csrdelft`.`biebboek` SET  `uitgavejaar` =  '0' WHERE  `uitgavejaar` IS NULL ;
 UPDATE  `csrdelft`.`biebboek` SET  `paginas` =  '0' WHERE  `paginas` IS NULL ;
@@ -13,13 +14,6 @@ CHANGE  `taal`  `taal` VARCHAR( 25 ) CHARACTER SET utf8 COLLATE utf8_general_ci 
 
 ALTER TABLE  `biebbeschrijving` 
 CHANGE  `toegevoegd`  `toegevoegd` DATETIME NOT NULL DEFAULT  '0000-00-00 00:00:00';
-
-UPDATE  `csrdelft`.`biebbeschrijving` SET  `toegevoegd` =  '2007-07-21 14:38:03' WHERE  `biebbeschrijving`.`id` =1;
-UPDATE  `csrdelft`.`biebbeschrijving` SET  `toegevoegd` =  '2007-07-21 14:44:52' WHERE  `biebbeschrijving`.`id` =2;
-UPDATE  `csrdelft`.`biebbeschrijving` SET  `toegevoegd` =  '2007-07-21 15:59:25' WHERE  `biebbeschrijving`.`id` =3;
-UPDATE  `csrdelft`.`biebbeschrijving` SET  `toegevoegd` =  '2007-07-22 20:33:06' WHERE  `biebbeschrijving`.`id` =4;
-UPDATE  `csrdelft`.`biebbeschrijving` SET  `toegevoegd` =  '2007-07-22 20:42:25' WHERE  `biebbeschrijving`.`id` =5;
-UPDATE  `csrdelft`.`biebbeschrijving` SET  `toegevoegd` =  '2007-07-21 20:46:55' WHERE  `biebbeschrijving`.`id` =6;
 
 ALTER TABLE  `biebbeschrijving` 
 ADD  `bewerkdatum` DATETIME NOT NULL DEFAULT  '0000-00-00 00:00:00';
