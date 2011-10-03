81a8729b75f4aa1074b1eb11b4099bd252f08df1
Ruben Verboon <ruben.verboon@localhost>
2011-10-03 09:53:24 +0000

    novieten opnieuw mailen

--- bin/welkom.php
+++ bin/welkom.php
@@ -10,20 +10,22 @@ require_once('configuratie.include.php');
 $result = $db->select("SELECT * FROM `lid` WHERE status = 'S_NOVIET'");
 if ($result !== false and $db->numRows($result) > 0) {
 	while ($sjaars = $db->next($result)){
-		$tekens = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
-		$pass = '';
-		for($i = 0; $i < 8; $i++) {
-			$pass .= substr($tekens,rand(0,strlen($tekens)),1);
-		}
+		$nanonovieten = array(1114,1154,1155,1133,1103,1148,1143,1125,1144,1159,1163,1165);
+		if(!(in_array($sjaars['uid'], $nanonovieten))){
+			$tekens = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+			$pass = '';
+			for($i = 0; $i < 8; $i++) {
+				$pass .= substr($tekens,rand(0,strlen($tekens)),1);
+			}
 
-		$passwordhash=makepasswd($pass);
-		$sQuery="UPDATE lid SET password='".$passwordhash."' WHERE uid='".$sjaars['uid']."' LIMIT 1;";
-		$db->query($sQuery);
+			$passwordhash=makepasswd($pass);
+			$sQuery="UPDATE lid SET password='".$passwordhash."' WHERE uid='".$sjaars['uid']."' LIMIT 1;";
+			$db->query($sQuery);
 
-		//cache resetten.
-		LidCache::flushLid($sjaars['uid']);
+			//cache resetten.
+			LidCache::flushLid($sjaars['uid']);
 		
-		$tekst = <<<EOD
+			$tekstold = <<<EOD
 Beste noviet {$sjaars['voornaam']},
 
 Bij je lidmaatschap van C.S.R. hoort ook de mogelijkheid om in te loggen op de C.S.R.-webstek.
@@ -51,10 +53,27 @@ Met vriendelijke groet,
 Arie Bovenberg
 h.t. PubCie-Praeses der Civitas Studiosorum Reformatorum
 EOD;
-		$nanonovieten = array(1114,1154,1155,1133,1103,1148,1143,1125,1144,1159,1163,1165);
-		if((in_array($sjaars['uid'], $nanonovieten))){
-			mail ($sjaars['email'],"Inloggegevens C.S.R.-webstek",$tekst,"From: PubliciteitsCommissie C.S.R. Delft <pubcie@csrdelft.nl>\nContent-Type: text/plain; charset=utf-8\nBcc: pubcie@csrdelft.nl");
-			echo $sjaars['email']."\n";
+
+			$tekst = <<<EOD
+Beste noviet {$sjaars['voornaam']},
+
+Ik heb per ongeluk een feutje gemaakt. Excuses daarvoor. Hier zijn je nieuwe login gegevens. Mocht je nog steeds problemen hebben, schroom dan niet om te mailen.
+
+Je inloggegevens zijn als volgt:
+Lidnummer: {$sjaars['uid']}
+Wachtwoord: {$pass}
+
+
+Met vriendelijke groet,
+
+Ruben Verboon
+h.t. PubCie-Programmeur der Civitas Studiosorum Reformatorum
+EOD;
+
+		
+				mail ($sjaars['email'],"Inloggegevens C.S.R.-webstek",$tekst,"From: PubliciteitsCommissie C.S.R. Delft <pubcie@csrdelft.nl>\nContent-Type: text/plain; charset=utf-8\nBcc: pubcie@csrdelft.nl");
+				echo $sjaars['email']."\n";
+		
 		}
 	}
 }
