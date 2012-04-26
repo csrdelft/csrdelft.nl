e9f8ce076da47f3cdcd8e5b1cefa06c96818112d
René Vennik <ren.vennik@localhost>
2012-04-26 19:52:51 +0000

    SocCie gebeuren

---
+++ htdocs/soccie/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+<title>SocCie</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script type="text/javascript" src="../layout/js/jquery.js"></script>
+<script type="text/javascript" src="../layout/js/countdown.js"></script>
+    <script type="text/javascript">
+      $(function(){
+        $('#counter').countdown({
+          image: 'count.png',
+          startTime: '01:12:12:00'
+        });
+		
+		$("#container").each(function() {
+		
+			$(this).css({paddingTop: ($(document).height() - $(this).height()) / 2});
+		
+		});
+		
+      });
+    </script>
+    <style type="text/css">
+	body{
+		width: 551px;
+		margin: 0 auto;
+	}
+      .cntSeparator {
+        font-size: 54px;
+        margin: 10px 7px;
+        color: #000;
+      }
+      .desc { margin: 7px 3px; }
+      .desc div {
+        float: left;
+        font-family: Arial;
+        width: 70px;
+        margin-right: 65px;
+        font-size: 13px;
+        font-weight: bold;
+        color: #000;
+      }
+    </style>
+  </head>
+<body>
+	<div id="container">
+  <div id="counter"></div>
+  <div class="desc">
+    <div>Dagen</div>
+    <div>Uren</div>
+    <div>Minuten</div>
+    <div>Seconden</div>
+  </div>
+  </div>
+</body>
+</html>
