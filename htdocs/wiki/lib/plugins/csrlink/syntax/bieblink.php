7b9e296412397a35813e4c55e3101ce007cfe9d8
Gerrit Uitslag <gerrit.uitslag@localhost>
2012-11-13 14:12:52 +0000

    [wiki] aan plugin csrlink [[boek>]] toegevoegd.

---
+++ htdocs/wiki/lib/plugins/csrlink/syntax/bieblink.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * DokuWiki Plugin csrlink (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Andreas Gohr <andi@splitbrain.org>
+ * @author  Gerrit Uitslag
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
+if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+require_once DOKU_PLUGIN.'syntax.php';
+
+class syntax_plugin_csrlink_bieblink extends DokuWiki_Syntax_Plugin {
+    function getType() {
+        return 'substition';
+    }
+
+    function getPType() {
+        return 'normal';
+    }
+
+    function getSort() {
+        return 150;
+    }
+
+
+    function connectTo($mode) {
+        $this->Lexer->addSpecialPattern('\[\[boek>.+?\]\]',$mode,'plugin_csrlink_bieblink');
+    }
+
+    function handle($match, $state, $pos, &$handler){
+        $match = trim(substr($match,7,-2));
+
+
+        list($boekid,$title) = explode('|',$match,2);
+
+        return compact('boekid','title');
+    }
+
+    function render($mode, &$R, $data) {
+        global $auth;
+        global $conf;
+        extract($data);
+
+        if($mode != 'xhtml' || is_null($auth)){
+            $R->cdata($title?$title:$boekid);
+            return true;
+        }
+
+        require_once 'bibliotheek/boek.class.php';
+        try{
+            $boek =    new Boek($boekid);
+        }catch(Exception $e){
+            // nothing found? render as text
+            $R->cdata('[[boek>]] Geen geldig boek-id ('.$boekid.')');
+            return true;
+        }
+
+        // get a nice title
+        if(!$title){
+            $title = $boek->getTitel();
+        }
+
+        //return html
+        $R->doc .= '<a class="bieblink groeplink_plugin" href="'.$boek->getUrl().'" title="Boek: '.mb_htmlentities($boek->getTitel()).'">';
+	    $R->doc .= '<span title="'.$boek->getStatus().' boek" class="boekindicator '.$boek->getStatus().'">•</span><span class="titel">'.$title.'</span> <span class="auteur">('.mb_htmlentities($boek->getAuteur()).')</span>';
+        $R->doc .= '</a>';
+        return true;
+    }
+}
+
+// vim:ts=4:sw=4:et:enc=utf-8:
