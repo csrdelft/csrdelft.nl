359d80ef4b96b6db9a22a13f6ba5b3a27b52f609
Gerrit Uitslag <gerrit.uitslag@localhost>
2013-02-11 17:49:07 +0000

    [wiki] Add columns plugin again

---
+++ htdocs/wiki/lib/plugins/columns/conf/default.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Columns: Configuration defaults
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Mykola Ostrovskyy <spambox03@mail.ru>
+ */
+
+$conf['kwcolumns'] = '';
+$conf['kwnewcol'] = '';
+$conf['wrapnewcol'] = 1;
