08d9aa6b8c99bd49c5790349ee286f030ef4628f
Gerrit Uitslag <gerrit.uitslag@localhost>
2013-03-07 21:45:39 +0000

    [wiki] blockquote aan Wrap Plugin toegeveogd

--- htdocs/wiki/conf/userscript.js
+++ htdocs/wiki/conf/userscript.js
@@ -1,1 +1,19 @@
-
+/**
+ * Add extra button to wrap toolbar
+ *  - blockquote
+ */
+// try to add button to toolbar
+console.log(window.toolbar);
+if (window.toolbar != undefined) {
+    jQuery.each(window.toolbar, function(i, button) {
+        if(button.title == "Wrap Plugin") {
+            button.list[button.list.length] = {
+                title: "blockquote",
+                icon: "../../plugins/wrap/images/toolbar/blockquote.png",
+                open: "<WRAP blockquote>",
+                close: "</WRAP>",
+                type: "format"
+            }
+        }
+    });
+}
