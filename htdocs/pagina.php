eb911f26801e2405eea1e37d98a50a0b54ca0886
René Vennik <ren.vennik@localhost>
2013-06-21 17:19:40 +0000

    Vereniging pagina in nieuwe layout.

--- htdocs/pagina.php
+++ htdocs/pagina.php
@@ -59,7 +59,7 @@ if($_GET['naam']=='video'){
 	}
 }
 // Hier alle namen van pagina's die in de nieuwe layout moeten worden weergegeven
-$nieuwNamen = array("contact");
+$nieuwNamen = array("contact", "vereniging");
 
 if(in_array($_GET['naam'],$nieuwNamen)) {
     $depagina->view("content");
