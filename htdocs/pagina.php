25673b51b881b1b1dd37a545977d40a248502e46
René Vennik <ren.vennik@localhost>
2013-06-21 21:47:49 +0000

    Vorming in nieuwe layout.

--- htdocs/pagina.php
+++ htdocs/pagina.php
@@ -59,7 +59,7 @@ if($_GET['naam']=='video'){
 	}
 }
 // Hier alle namen van pagina's die in de nieuwe layout moeten worden weergegeven
-$nieuwNamen = array("contact", "vereniging", "lidworden", "geloof", "filmpjes");
+$nieuwNamen = array("contact", "vereniging", "lidworden", "geloof", "vorming");
 
 if(in_array($_GET['naam'],$nieuwNamen)) {
     $depagina->view("content");
