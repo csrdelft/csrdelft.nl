3fceeec13f45f90e8b788cc605d4ad8642ea8a55
René Vennik <ren.vennik@localhost>
2013-08-11 00:54:53 +0000

    get_userlist.php loads now

--- htdocs/android/get_userlist.php
+++ htdocs/android/get_userlist.php
@@ -1,12 +1,30 @@
-{
-    "user": [
-        {
-            "id": "213323",
-            "name": "Pietje"
-        },
-        {
-            "id": "213",
-            "name": "Gijs"
-        }
-    ]
+<?php
+
+require_once 'configuratie.include.php';
+require_once 'lid/ledenlijstcontent.class.php';
+require_once 'groepen/groep.class.php';
+
+if(!($loginlid->hasPermission('P_LOGGED_IN') AND $loginlid->hasPermission('P_OUDLEDEN_READ'))){
+	# geen rechten
+	echo 'false';
+	exit;
 }
+
+$zoeker=new LidZoeker();
+$zoeker->parseQuery($_GET);
+
+$leden = array();
+$json = "";
+
+foreach($zoeker->getLeden() as $lid) {
+
+	$leden[] = array("id" => $lid->getUid(), "name" => $lid->getNaam());
+	//print_r($lid->getUid(). " " . $lid->getNaam());
+
+}
+
+echo '{
+    "user": ' . json_encode($leden) . '
+}';
+
+?>
