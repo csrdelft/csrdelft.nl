33eca4ebf881c2a18f9aa8d641f3ff7571f904cf
René Vennik <ren.vennik@localhost>
2013-08-11 11:29:38 +0000

    print_r removed

--- htdocs/android/get_user.php
+++ htdocs/android/get_user.php
@@ -11,8 +11,6 @@ if(!($loginlid->hasPermission('P_LOGGED_IN') AND $loginlid->hasPermission('P_OUD
 
 $lid = LidCache::getLid($_GET['id']);
 
-print_r($lid);
-
 echo '{
     "user": ' . json_encode(array("id" => $lid->getUid(), "name" => $lid->getNaam(), "mobile" => $lid->getProperty('mobiel'), "phone" => $lid->getProperty('telefoon'))) . '
 }';
