38b273584cba7ad7f8827936715c408b46ddb87e
Paul Brussee <paul.brussee@localhost>
2013-12-28 08:52:17 +0000

    drop oude maaltijdsysteem;

---
+++ sql/3744_drop_oude_maaltijdsysteem.sql
@@ -0,0 +1,12 @@
+DROP TABLE  `maaltijd` ,
+`maaltijdaanmelding` ,
+`maaltijdabo` ,
+`maaltijdabosoort` ,
+`maaltijdcorvee` ,
+`maaltijdcorveeinstellingen` ,
+`maaltijdgesloten` ;
+
+ALTER TABLE `lid` DROP `corvee_wens` ,
+DROP `corvee_vrijstelling` ,
+DROP `corvee_kwalikok` ,
+DROP `corvee_voorkeuren` ;
