7776e95bae28abd03e3902d617fda6d056eaa3ef
Paul Brussee <paul.brussee@localhost>
2014-01-06 22:50:47 +0000

    form visitekaartjes werkt;

--- lib/formulier.class.php
+++ lib/formulier.class.php
@@ -677,6 +677,7 @@ $('.UidField').each(function(index, tag){
 				url: "/tools/naamlink.php?uid="+field.val(),
 				success: function(response){
 					$('#uidPreview_'+field.attr('id').substring(6)).html(response);
+					init_visitekaartjes();
 				}
 			});
 		}
@@ -770,6 +771,7 @@ $('.wantsLidPreview').each(function(index, tag){
 				url: "/tools/naamlink.php?naam="+field.val()+ "&"+$.param({ zoekin: suggesties[suggesties.length-1] }),
 				success: function(response){
 					$('#lidPreview_'+field.attr('id').substring(6)).html(response);
+					init_visitekaartjes();
 				}
 			});
 		}else{
