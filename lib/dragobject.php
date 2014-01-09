fc1de22bcd23c1ce81c7e7f66c2eff13417ea4bc
Paul Brussee <paul.brussee@localhost>
2014-01-09 03:31:33 +0000

    test3;

--- lib/dragobject.php
+++ lib/dragobject.php
@@ -7,18 +7,18 @@ if (\LoginLid::instance()->hasPermission('P_LEDEN_READ')) {
 	$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
 	$coords = filter_input(INPUT_POST, 'coords', FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
 	
-	$_SESSION['dragobject_'. $id] = $coords;
+	$_SESSION['dragobject'][$id] = $coords;
 	
-	echo print_r($_SESSION);
+	echo print_r($_SESSION['dragobject']);
 }
 
 function getDragObjectCoords($id, &$top, &$left) {
 	
-	if (array_key_exists('dragobject_'. $id, $_SESSION)) {
+	if (array_key_exists('dragobject', $_SESSION) && array_key_exists($id, $_SESSION['dragobject'])) {
 		
-		$top = (int) $_SESSION['dragobject_'. $id]['top'];
-		$left = (int) $_SESSION['dragobject_'. $id]['left'];
+		$top = (int) $_SESSION['dragobject'][$id]['top'];
+		$left = (int) $_SESSION['dragobject'][$id]['left'];
 	}
 	
-	echo print_r($_SESSION);
+	echo print_r($_SESSION['dragobject']);
 }
