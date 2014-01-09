f13c1844f5675ffdc66ed92a754452bc5b836fff
Paul Brussee <paul.brussee@localhost>
2014-01-09 04:21:47 +0000

    doc;

--- lib/dragobject.class.php
+++ lib/dragobject.class.php
@@ -1,7 +1,13 @@
 <?php
 
+/**
+ * DragObject.class.php	| 	P.W.G. Brussee (brussee@live.nl)
+ * 
+ * Stores the screen coordinates of a dragable object in the session variable.
+ * @see /htdocs/tools/dragobject.php
+ */
 class DragObject {
-	
+
 	public static function getCoords($id, &$top, &$left) {
 
 		if (array_key_exists('dragobject', $_SESSION) && array_key_exists($id, $_SESSION['dragobject'])) {
