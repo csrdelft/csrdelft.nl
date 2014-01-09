73c7b66840661757e1432be1f21a231583e7e766
Paul Brussee <paul.brussee@localhost>
2014-01-09 03:33:54 +0000

    fix4;

--- lib/dragobject.php
+++ lib/dragobject.php
@@ -1,17 +1,5 @@
 <?php
 
-require_once 'configuratie.include.php';
-
-if (\LoginLid::instance()->hasPermission('P_LEDEN_READ')) {
-
-	$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_STRING);
-	$coords = filter_input(INPUT_POST, 'coords', FILTER_SANITIZE_STRING, FILTER_REQUIRE_ARRAY);
-	
-	$_SESSION['dragobject'][$id] = $coords;
-	
-	echo print_r($_SESSION['dragobject']);
-}
-
 function getDragObjectCoords($id, &$top, &$left) {
 	
 	if (array_key_exists('dragobject', $_SESSION) && array_key_exists($id, $_SESSION['dragobject'])) {
