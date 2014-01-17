16c975e9afc2826aaec5d6e7c8fe89875b105ff7
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-01-17 12:48:46 +0000

    update profiellink

--- htdocs/wiki/lib/plugins/csrlink/syntax/profiellink.php
+++ htdocs/wiki/lib/plugins/csrlink/syntax/profiellink.php
@@ -10,30 +10,60 @@
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
-
-require_once DOKU_PLUGIN.'syntax.php';
-
+/**
+ * Create link to C.S.R. profiel
+ */
 class syntax_plugin_csrlink_profiellink extends DokuWiki_Syntax_Plugin {
+
+    /**
+     * Syntax Type
+     *
+     * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
+     */
     function getType() {
         return 'substition';
     }
 
+    /**
+     * Paragraph Type
+     *
+     * Defines how this syntax is handled regarding paragraphs. This is important
+     * for correct XHTML nesting. Should return one of the following:
+     *
+     * 'normal' - The plugin can be used inside paragraphs
+     * 'block'  - Open paragraphs need to be closed before plugin output
+     * 'stack'  - Special case. Plugin wraps other paragraphs.
+     *
+     * @see Doku_Handler_Block
+     */
     function getPType() {
         return 'normal';
     }
 
+    /**
+     * Sort order when overlapping syntax
+     * @return int
+     */
     function getSort() {
         return 150;
     }
 
-
+    /**
+     * @param $mode
+     */
     function connectTo($mode) {
         $this->Lexer->addSpecialPattern('\[\[lid>.+?\]\]',$mode,'plugin_csrlink_profiellink');
     }
 
+    /**
+     * Handler to prepare matched data for the rendering process
+     *
+     * @param   string       $match   The text matched by the patterns
+     * @param   int          $state   The lexer state for the match
+     * @param   int          $pos     The character position of the matched text
+     * @param   Doku_Handler $handler Reference to the Doku_Handler object
+     * @return  array Return an array with all data you want to use in render
+     */
     function handle($match, $state, $pos, &$handler){
         $match = trim(substr($match,6,-2));
 
@@ -43,13 +73,23 @@ class syntax_plugin_csrlink_profiellink extends DokuWiki_Syntax_Plugin {
         return compact('uid','title');
     }
 
-    function render($mode, &$R, $data) {
+    /**
+     * Handles the actual output creation.
+     *
+     * @param   $format   string        output format being rendered
+     * @param   $renderer Doku_Renderer reference to the current renderer object
+     * @param   $data     array         data created by handler()
+     * @return  boolean                 rendered correctly?
+     */
+    function render($format, &$renderer, $data) {
+        /** @var DokuWiki_Auth_Plugin $auth */
         global $auth;
-        global $conf;
+        /** @var string $title */
+        /** @var string $uid */
         extract($data);
 
-        if($mode != 'xhtml' || is_null($auth) || !$auth instanceof auth_csr){
-            $R->cdata($title?$title:$uid);
+        if($format != 'xhtml' || is_null($auth) || !$auth instanceof auth_csr){
+            $renderer->cdata($title?$title:$uid);
             return true;
         }
 
@@ -58,7 +98,7 @@ class syntax_plugin_csrlink_profiellink extends DokuWiki_Syntax_Plugin {
 
         // nothing found? render as text
         if(!$uinfo){
-            $R->doc .='<span class="csrlink invalid" title="[[lid>]] Geen geldig lidnummer ('.hsc($uid).')">'.hsc($title?$title:$uid).'</span>';
+            $renderer->doc .='<span class="csrlink invalid" title="[[lid>]] Geen geldig lidnummer ('.hsc($uid).')">'.hsc($title?$title:$uid).'</span>';
             return true;
         }
 
@@ -72,16 +112,16 @@ class syntax_plugin_csrlink_profiellink extends DokuWiki_Syntax_Plugin {
         if(!$title) $title = $uid;
 
 
-        $R->doc .= '<a href="'.$this->getConf('profileurl').$uid.'" class="profiellink_plugin">';
-        $R->doc .= hsc($title);
+        $renderer->doc .= '<a href="'.$this->getConf('profileurl').$uid.'" class="profiellink_plugin">';
+        $renderer->doc .= hsc($title);
 
-        $R->doc .= '<span class="profiellink_popup" title="Bekijk Profiel">';
-        $R->doc .= '<img src="'.$uinfo['pasfoto'].'" class="medialeft" width="48" height="64" alt="" />';
-        $R->doc .= '<b>'.hsc($uinfo['name']).'</b><br />';
-        if($uinfo['name'] != $uid) $R->doc .= '<i>'.hsc($uid).'</i><br />';
-        $R->doc .= '</span>';
+        $renderer->doc .= '<span class="profiellink_popup" title="Bekijk Profiel">';
+        $renderer->doc .= '<img src="'.$uinfo['pasfoto'].'" class="medialeft" width="48" height="64" alt="" />';
+        $renderer->doc .= '<b>'.hsc($uinfo['name']).'</b><br />';
+        if($uinfo['name'] != $uid) $renderer->doc .= '<i>'.hsc($uid).'</i><br />';
+        $renderer->doc .= '</span>';
 
-        $R->doc .= '</a>';
+        $renderer->doc .= '</a>';
 
         return true;
     }
