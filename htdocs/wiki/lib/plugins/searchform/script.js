9ee9de46804f0e3e345e0d9e70f94f74e235d59c
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-01-17 13:50:13 +0000

    more general name

--- htdocs/wiki/lib/plugins/searchform/script.js
+++ htdocs/wiki/lib/plugins/searchform/script.js
@@ -2,7 +2,7 @@ jQuery(function () {
     var $searchform__input = jQuery('#qsearch2__in');
 
     $searchform__input.dw_qsearch({
-        output_id: '#qsearch2__out',
+        output: '#qsearch2__out',
         getSearchterm: function() {
             var sf_ns = jQuery('#searchform__ns').val();
             return $searchform__input.val() + (sf_ns ? ' @' + sf_ns : '');
