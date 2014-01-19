ea5430e8c42f2a70804363246bd2bae3769241d1
Paul Brussee <paul.brussee@localhost>
2014-01-19 14:25:04 +0000

    image access controller;

---
+++ images/AccessController.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once 'configuratie.include.php';
+
+if (!LoginLid::instance()->hasPermission('P_LEDEN_READ')) {
+
+	$filter = '/(pasfoto|intern|novitiaat|ontvoering|feuten|slachten|zuipen|prive|privé)/i';
+	$request = filter_input(INPUT_GET, 'request', FILTER_SANITIZE_URL);
+
+	if (preg_match($filter, $request)) {
+		header('HTTP/1.0 403 Forbidden');
+		header('Location: http://csrdelft.nl/');
+		exit;
+	}
+}
+
+$path = PICS_PATH . $request;
+$fp = fopen($path, 'rb'); // open in a binary mode
+$ext = pathinfo($path, PATHINFO_EXTENSION);
+
+header("Content-Type: image/" . $ext);
+header("Content-Length: " . filesize($path));
+header("Cache-Control: maxage=21000"); // 6 dagen
+header("Expires: " . gmdate('D, d M Y H:i:s', (time() + 21000)) . ' GMT');
+
+fpassthru($fp);
+exit;
