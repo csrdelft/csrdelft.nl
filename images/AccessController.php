da8b227ced5678b63ca938020467ebcee0fa4153
Paul Brussee <paul.brussee@localhost>
2014-01-19 14:45:38 +0000

    test;

--- images/AccessController.php
+++ images/AccessController.php
@@ -3,23 +3,18 @@
 chdir('../lib/');
 require_once 'configuratie.include.php';
 
-if (!LoginLid::instance()->hasPermission('P_LEDEN_READ')) {
+if (!LoginLid::instance()->hasPermission('P_LOGGED_IN')) {
 
 	$filter = '/(pasfoto|intern|novitiaat|ontvoering|feuten|slachten|zuipen|prive|privé)/i';
 	$request = filter_input(INPUT_GET, 'request', FILTER_SANITIZE_URL);
 
 	if (preg_match($filter, $request)) {
-
-		header('HTTP/1.0 403 Forbidden');
-		//header('Location: http://csrdelft.nl/');
-
-		echo $request;
-
+		header('Location: http://csrdelft.nl/');
 		exit;
 	}
 }
 
-$path = PICS_PATH . '/' . $request;
+$path = PICS_PATH . $request;
 $ext = pathinfo($path, PATHINFO_EXTENSION);
 
 header("Content-Type: image/" . $ext);
