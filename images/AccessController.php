ba9a141b34f2f87d6ba84cba51d19c514743c511
Paul Brussee <paul.brussee@localhost>
2014-01-19 14:59:07 +0000

    test 9;

--- images/AccessController.php
+++ images/AccessController.php
@@ -13,14 +13,14 @@ if (!LoginLid::instance()->hasPermission('P_LOGGED_IN')) {
 		exit;
 	}
 }
+chdir('../images/');
+$path = $request;
+$ext = pathinfo($path, PATHINFO_EXTENSION);
 
-$path = PICS_PATH . $request;
-/* $ext = pathinfo($path, PATHINFO_EXTENSION);
+header("Content-Type: image/" . $ext);
+header("Content-Length: " . filesize($path));
+header("Cache-Control: maxage=21000"); // 6 dagen
+header("Expires: " . gmdate('D, d M Y H:i:s', (time() + 21000)) . ' GMT');
 
-  header("Content-Type: image/" . $ext);
-  header("Content-Length: " . filesize($path));
-  header("Cache-Control: maxage=21000"); // 6 dagen
-  header("Expires: " . gmdate('D, d M Y H:i:s', (time() + 21000)) . ' GMT');
- */
 $fp = fopen($path, 'rb'); // open in a binary mode
 fpassthru($fp);
