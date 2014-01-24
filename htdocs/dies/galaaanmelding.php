8a02383b9cfad8431a0e3902687211b14134c169
Benjamin Komen <benjamin.komen@localhost>
2014-01-24 13:43:46 +0000

    gala is vol tekst aangepast

--- htdocs/dies/galaaanmelding.php
+++ htdocs/dies/galaaanmelding.php
@@ -10,7 +10,7 @@ if ($loginlid->instance()->getUid() == 'x999') {
 } else {
 	$dies = new DiesAanmelding($loginlid->getUid());
 	if ($dies->galaVol()) {
-		$bericht = '<p>gala is vol</p>';
+		$bericht = '<h3>Gala is vol</h3><p>Helaas, er zijn inmiddels 80 inschrijvingen voor het gala, daarom is de inschrijving nu gesloten.</p>';
 		$ingelogd = false;
 	} else {
 		if ($_POST) {
@@ -119,10 +119,10 @@ if ($ingelogd) {
 	$form[] = new SelectField('eetDate', $data['eetDate'], 'Eetvoorkeur', $eetopties);
 	$form[] = new TextField('allerDate', $data['allerDate'], 'Allergie-informatie:');
 	$form[] = new SelectField('date18', $data['date18'], 'Is uw Diesdame of Diesheer meerderjarig op de dag van het gala?', $leeftijdopties);
-	$form[] = new SubmitButton('opslaan', '<a class="knop" href="/dies">Annuleren</a>');
+	$form[] = new SubmitButton('opslaan', '<a class="knop" href="/dies2014">Annuleren</a>');
 
 
-	$form = new Formulier('galaaanmelding', '/dies/galaaanmelding.php', $form);
+	$form = new Formulier('galaaanmelding', '/dies2014/galaaanmelding.php', $form);
 	$form->view();
 } else {
 	?>
