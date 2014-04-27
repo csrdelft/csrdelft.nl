9d8fb5ca08f422af150eaddd0fdda4f21905589a
Paul Brussee <paul.brussee@localhost>
2014-04-27 17:54:05 +0000

    escape pageover outside click;

--- htdocs/layout2/js/init.js
+++ htdocs/layout2/js/init.js
@@ -1,9 +1,9 @@
 // On Document.ready
-$(function(){
+$(function() {
 
 
 	// Random rotation between -2 and +2 degrees
-	$('.rotate').each(function(){
+	$('.rotate').each(function() {
 
 		// Random int between -2 and +2
 		var random = Math.random() * 4 - 2;
@@ -20,20 +20,20 @@ $(function(){
 
 
 	// Login form enhancing
-	$('.login-form input.text').focus(function(){
+	$('.login-form input.text').focus(function() {
 		$(this).parents('.flip').addClass('flipped');
-	}).blur(function(){
+	}).blur(function() {
 		$(this).parents('.flip').removeClass('flipped');
 	});
 	// Submit form by clicking link
-	$('.login-submit').click(function(e){
+	$('.login-submit').click(function(e) {
 		$('.login-form form').submit();
 		e.preventDefault();
 	});
 
 
 	if (is_touch_device()) {
-		$('.flip').click(function(){
+		$('.flip').click(function() {
 			$(this).addClass("flipped");
 		});
 	}
@@ -43,63 +43,75 @@ $(function(){
 
 
 	// Modal popup closing
-	if($("#blackout").size())
+	if ($("#blackout").size())
 		$("body").css({overflowY: "hidden"});
-	// TODO: close when clicking outside of the pageover
-	$(document).on('click', '.close', function(){
-		$("#blackout").css({overflowY: "hidden"});
-		$("body").css({overflowY: "scroll"});
-		$(this).parents('#pageover').animate({ top: '100%' }, 300);
-		$(this).parents('#blackout').fadeOut(300, function(){
-			$(this).remove();
-		});
-		return false;
+
+	$('body').bind('click.outside', function(e) {
+		if (!$(e.target).parents('#pageover').length > 0) {
+			escape(e);
+		}
 	});
+	$(document).keyup(function(e) {
+		if (e.keyCode == 27) { // esc
+			escape(e);
+		}
+	});
+	$('.close').click(escape);
 
 	// take first image and put it in the clip
 	/* var imageHolder = $("div.pg-mid img.REPLACE-ANCHOR");
-	var image = $("div.pg-mid div.content img.ubb_image").first();
-	imageHolder.attr("src", image.attr("src"));
-	image.hide(); */
-	
+	 var image = $("div.pg-mid div.content img.ubb_image").first();
+	 imageHolder.attr("src", image.attr("src"));
+	 image.hide(); */
+
 	$("div.pg-mid img.rotate-left, div.pg-mid img.rotate-right").each(function() {
-	
+
 		$(this).css({margin: 0}).wrap('<figure class="' + $(this).attr("class") + '" />').after('<div class="clip"></div>');
-	
+
 	});
 
 
 	// Random rotation
-	$('figure.rotate-left, figure.rotate-right').each(function(){
+	$('figure.rotate-left, figure.rotate-right').each(function() {
 		var random = Math.random() * 4 - 2; // Random between -2 and +2 degrees
 		$(this).css('-webkit-transform', 'rotate(' + random + 'deg)');
 		$(this).css('-moz-transform', 'rotate(' + random + 'deg)');
 	});
-	
+
 	// Filmpjes meuk
 	$("#video_unit").each(function() {
-		
+
 		el = $(".videos > li");
 		el.find("> a").click(function() {
-		
+
 			el.removeClass("active");
 			$(this).parent().addClass("active");
-			
+
 			var index = $(this).parent().parent().find("> li").index($(this).parent());
-			
-			
+
+
 			$("#video_unit .video iframe").attr("src", video_unit_videos[index]);
-			
+
 			return false;
-		
+
 		});
-	
+
 	});
-	
+
 });
 
+function escape(e) {
+	$('#blackout').css({overflowY: "hidden"});
+	$('body').css({overflowY: "scroll"});
+	$('#pageover').animate({top: '100%'}, 300);
+	$('#blackout').fadeOut(300, function() {
+		$(this).remove();
+	});
+	return false;
+}
 
 function is_touch_device() {
-  return !!('ontouchstart' in window) // works on most browsers
-      || !!('onmsgesturechange' in window); // works on ie10
-};
+	return !!('ontouchstart' in window) // works on most browsers
+			|| !!('onmsgesturechange' in window); // works on ie10
+}
+;
