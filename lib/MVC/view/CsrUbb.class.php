a708aef26e5cb4284f320dab85f637dc8cf17376
Ruben Verboon <ruben.verboon@localhost>
2014-06-13 15:37:56 +0000

    hidden cash ding toegevoegd

--- lib/MVC/view/CsrUbb.class.php
+++ lib/MVC/view/CsrUbb.class.php
@@ -50,6 +50,15 @@ class CsrUbb extends eamBBParser {
 	}
 
 	/**
+	 * Dit laad de twitter account van het hidden cash spel.
+	 */
+	function ubb_hidden($parameters) {
+				$html = '<a class="twitter-timeline" href="https://twitter.com/HiddenCashCSR" data-widget-id="477465734352621568">Tweets by @HiddenCashCSR</a>
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+		return $html;		
+	}
+
+	/**
 	 * Rul = url
 	 */
 	function ubb_rul($arguments = array()) {
