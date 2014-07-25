8e67723f90de17305da5abb92678cd825236e9bc
Дональд Дак <.@localhost>
2014-07-25 08:42:03 +0000

    owee als default layout gezet voor komende maanden

--- lib/MVC/model/LidInstellingenModel.class.php
+++ lib/MVC/model/LidInstellingenModel.class.php
@@ -44,7 +44,7 @@ class LidInstellingen extends PersistenceModel {
 			'bijbel'		 => array('Bijbelvertaling voor bijbelrooster', T::Enumeration, array('NBV', 'NBG', 'Herziene Statenvertaling', 'Statenvertaling (Jongbloed)', 'Groot Nieuws Bijbel', 'Willibrordvertaling'), 'NBV')
 		),
 		'layout'		 => array(
-			'layout'		 => array('Websteklayout', T::Enumeration, array('normaal', 'owee', 'lustrum', 'sineregno', 'roze'), 'normaal'),
+			'layout'		 => array('Websteklayout', T::Enumeration, array('normaal', 'owee', 'lustrum', 'sineregno', 'roze'), 'owee'),
 			'beeld'			 => array('Schermmodus', T::Enumeration, array('normaal', 'breedbeeld'), 'normaal'),
 			'fixed'			 => array('Lijstmodus', T::Enumeration, array('normaal', 'vast'), 'normaal'),
 			'visitekaartjes' => array('Visitekaartjes', T::Enumeration, array('ja', 'nee'), 'ja'),
