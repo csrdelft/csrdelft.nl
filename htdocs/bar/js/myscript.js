1ec986071bc5a02fc20c9eff1454c72a6d9a9628
Bertje Buidelrat <bertje.buidelrat@localhost>
2014-08-05 09:41:09 +0000

    SBS

--- htdocs/bar/js/myscript.js
+++ htdocs/bar/js/myscript.js
@@ -92,7 +92,7 @@ $(function () {
 
     function zetInTabel(persoon) {
         var naam = persoon.naam;
-        $("#selectieTabel > tbody").append("<tr id='persoon" + persoon.socCieId + "'><td>" + persoon.bijnaam + "</td><td>" + naam + "</td><td>" + saldoStr(persoon.saldo) + "</td></tr>");
+        $("#selectieTabel > tbody").append("<tr id='persoon" + persoon.socCieId + "'><td>" + persoon.bijnaam + "</td><td>" + naam + "</td><td class=\"" + (persoon.saldo < 0 ? "bg-danger" : "bg-success") +"\">" + saldoStr(persoon.saldo) + "</td></tr>");
         $("#persoon" + persoon.socCieId).click(function () {
             cancel();
             $.ajax({
@@ -399,6 +399,7 @@ $(function () {
         $("#besteLijstBeheerContent tbody").empty();
         $("#besteLijstBeheerLaadPersoon").html("Laad bestellingen van: -");
         $("#persoonselectieVeld").trigger("click");
+		laadPersonen();
     } cancel();
 
     $("#krijgBestellingen").click(function () {
