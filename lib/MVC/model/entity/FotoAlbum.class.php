addac78143acb7574dd20426f8b53f516bca02cc
Paul Brussee <paul.brussee@localhost>
2014-08-07 18:38:13 +0000

    bugfix;

--- lib/MVC/model/entity/FotoAlbum.class.php
+++ lib/MVC/model/entity/FotoAlbum.class.php
@@ -33,7 +33,7 @@ class FotoAlbum extends Map {
 	 * Bestaat er een map met de naam van het pad.
 	 */
 	public function exists() {
-		return file_exists($this->path) && is_dir($this->path);
+		return is_readable($this->path) && file_exists($this->path) && is_dir($this->path);
 	}
 
 	/**
