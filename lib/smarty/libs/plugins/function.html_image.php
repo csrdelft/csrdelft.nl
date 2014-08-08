2568b741d1b09368a408a60f7f9ee9ebf49904e5
Paul Brussee <paul.brussee@localhost>
2014-08-08 08:53:50 +0000

    src fix;

--- lib/smarty/libs/plugins/function.html_image.php
+++ lib/smarty/libs/plugins/function.html_image.php
@@ -14,7 +14,7 @@
  * Date:     Feb 24, 2003<br>
  * Purpose:  format HTML tags for the image<br>
  * Examples: {html_image file="/images/masthead.gif"}<br>
- * Output:   <img src="/images/masthead.gif" width=400 height=23><br>
+ * Output:   <img src="{$CSR_PICS}/images/masthead.gif" width=400 height=23><br>
  * Params:
  * <pre>
  * - file        - (required) - file (and path) of image
