c5c0694eb9c3fa29b0fbc378656fd90ca5596a3f
Paul Brussee <paul.brussee@localhost>
2014-08-08 09:04:05 +0000

    http:// fix;

--- lib/MVC/view/CsrUbb.class.php
+++ lib/MVC/view/CsrUbb.class.php
@@ -67,7 +67,7 @@ class CsrUbb extends eamBBParser {
 	 */
 	function ubb_hidden($arguments = array()) {
 		$html = '<a class="twitter-timeline" href="https://twitter.com/HiddenCashCSR" data-widget-id="477465734352621568">Tweets by @HiddenCashCSR</a>
-							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
+							<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http://.test(d.location)?\'http\':\'https\';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>';
 		return $html;
 	}
 
