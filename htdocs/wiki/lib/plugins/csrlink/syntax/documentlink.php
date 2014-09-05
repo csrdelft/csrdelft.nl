65188f1b76e10aed0e2fe122762e92fe93f0329b
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-09-05 10:36:03 +0000

    [wiki]fix documentsyntax: functies waren niet hernoemd

--- htdocs/wiki/lib/plugins/csrlink/syntax/documentlink.php
+++ htdocs/wiki/lib/plugins/csrlink/syntax/documentlink.php
@@ -16,25 +16,62 @@ if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 
 require_once DOKU_PLUGIN.'syntax.php';
 
+
+/**
+ * Syntax for refering documents from csrdelft.nl
+ */
 class syntax_plugin_csrlink_documentlink extends DokuWiki_Syntax_Plugin {
-    function getType() {
+	/**
+	 * Syntax Type
+	 *
+	 * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
+	 */
+	function getType() {
         return 'substition';
     }
 
-    function getPType() {
+	/**
+	 * Paragraph Type
+	 *
+	 * Defines how this syntax is handled regarding paragraphs. This is important
+	 * for correct XHTML nesting. Should return one of the following:
+	 *
+	 * 'normal' - The plugin can be used inside paragraphs
+	 * 'block'  - Open paragraphs need to be closed before plugin output
+	 * 'stack'  - Special case. Plugin wraps other paragraphs.
+	 *
+	 * @see Doku_Handler_Block
+	 */
+	function getPType() {
         return 'normal';
     }
 
-    function getSort() {
+	/**
+	 * @return int
+	 */
+	function getSort() {
         return 150;
     }
 
 
-    function connectTo($mode) {
+	/**
+	 * @param string $mode
+	 */
+	function connectTo($mode) {
         $this->Lexer->addSpecialPattern('\[\[document>.+?\]\]',$mode,'plugin_csrlink_documentlink');
     }
 
-    function handle($match, $state, $pos, &$handler){
+
+	/**
+	 * Handler to prepare matched data for the rendering process
+	 *
+	 * @param   string       $match   The text matched by the patterns
+	 * @param   int          $state   The lexer state for the match
+	 * @param   int          $pos     The character position of the matched text
+	 * @param   Doku_Handler $handler The Doku_Handler object
+	 * @return  array Return an array with all data you want to use in render
+	 */
+	function handle($match, $state, $pos, &$handler){
         $match = trim(substr($match,11,-2));
 
 
@@ -43,13 +80,25 @@ class syntax_plugin_csrlink_documentlink extends DokuWiki_Syntax_Plugin {
         return compact('documentid','title');
     }
 
-    function render($mode, &$R, $data) {
+
+	/**
+	 * Handles the actual output creation.
+	 *
+	 * The contents of the $data array depends on what the handler() function above
+	 * created
+	 *
+	 * @param   $format   string        output format being rendered
+	 * @param   $R Doku_Renderer the current renderer object
+	 * @param   $data     array         data created by handler()
+	 * @return  boolean                 rendered correctly?
+	 */
+	function render($format, Doku_Renderer $R, $data) {
         global $auth;
         /** @var string $title */
         /** @var string $documentid */
         extract($data);
 
-        if($mode != 'xhtml' || is_null($auth) || !$auth instanceof auth_plugin_authcsr){
+        if($format != 'xhtml' || is_null($auth) || !$auth instanceof auth_plugin_authcsr){
             $R->cdata($title?$title:$documentid);
             return true;
         }
@@ -67,18 +116,18 @@ class syntax_plugin_csrlink_documentlink extends DokuWiki_Syntax_Plugin {
 
         // get a nice title
         if($title=='fname'){
-            $title = $document->getBestandsnaam();
+            $title = $document->getFileName();
         }elseif(!$title){
             $title = $document->getNaam();
         }
 
         //DokuWiki mimetype icons
         $documenturl = $document->getDownloadurl();
-        list($ext,$mime,$dl) = mimetype($documenturl,false);
+        list($ext, /* $mime */, /* $dl */) = mimetype($documenturl,false);
         $class = preg_replace('/[^_\-a-z0-9]+/i','_',$ext);
 
         //return html
-        $R->doc .= '<a href="'.$documenturl.'" class="documentlink csrlink_plugin mediafile mf_'.$class.'">'.hsc($title).'</a> <span class="documentlink csrlink_plugin size">('.format_filesize((int)$document->getSize()).')</span>';
+        $R->doc .= '<a href="'.$documenturl.'" class="documentlink csrlink_plugin mediafile mf_'.$class.'">'.hsc($title).'</a> <span class="documentlink csrlink_plugin size">('.format_filesize((int)$document->getFileSize()).')</span>';
 
         return true;
     }
