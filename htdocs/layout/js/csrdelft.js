60b669d0d33e1e56b2d4f70df905dfa0c0a307a4
Paul Brussee <paul.brussee@localhost>
2014-09-23 05:45:39 +0000

    doc;

--- htdocs/layout/js/csrdelft.js
+++ htdocs/layout/js/csrdelft.js
@@ -83,6 +83,14 @@ var bShiftPressed = false;
 var bCtrlPressed = false;
 var bAltPressed = false;
 var bMetaPressed = false;
+/**
+ * Houdt bij welke modifier keys worden ingedrukt.
+ * Werkt dankzij jQuery voor Mac & PC.
+ * 
+ * Slimme keydown constructie vanwege repeated
+ * calls op die functie zolang er een toets wordt
+ * ingedrukt.
+ */
 function init_key_pressed() {
 	$(window).on('keyup', function (event) {
 		$(window).one('keydown', function (event) {
