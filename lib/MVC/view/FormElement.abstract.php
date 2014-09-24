cd0d54794bbf4e6b9d19a94c4d689e1f60fc010c
Paul Brussee <paul.brussee@localhost>
2014-09-24 22:33:15 +0000

    locale;

--- lib/MVC/view/FormElement.abstract.php
+++ lib/MVC/view/FormElement.abstract.php
@@ -1377,7 +1377,7 @@ class DatumField extends InputField {
 				echo ' selected="selected"';
 			}
 
-			echo '>' . date('M', mktime(0, 0, 0, $value, 1, 0)) . '</option>';
+			echo '>' . strftime('%b', mktime(0, 0, 0, $value, 1, 0)) . '</option>';
 		}
 		echo '</select> ';
 
