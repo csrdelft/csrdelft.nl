3dd9318f98c4adfe627367bfdc3cff7ae6688c31
Paul Brussee <paul.brussee@localhost>
2014-10-02 12:45:54 +0000

    default;

--- lib/MVC/model/LidInstellingenModel.class.php
+++ lib/MVC/model/LidInstellingenModel.class.php
@@ -58,7 +58,7 @@ class LidInstellingen extends PersistenceModel {
 			'filter2008'			 => array('Berichten van 2008 eerst verbergen', T::Enumeration, array('ja', 'nee'), 'nee')
 		),
 		'zijbalk'		 => array(
-			'scrollen'				 => array('Scrollen', T::Enumeration, array('met pagina mee', 'apart scrollen'), 'apart scrollen'),
+			'scrollen'				 => array('Scrollen', T::Enumeration, array('met pagina mee', 'apart scrollen'), 'met pagina mee'),
 			'quicknav'				 => array('Navigatie knopjes weergeven', T::Enumeration, array('ja', 'nee'), 'ja'),
 			'ishetal'				 => array('Is het al… weergeven', T::Enumeration, array('niet weergeven', 'willekeurig', 'wist u dat', 'weekend', 'kring', 'lezing', 'borrel', 'jarig', 'dies', 'happie', 'lunch', 'studeren'), 'willekeurig'),
 			'agendaweken'			 => array('Aantal weken in agenda weergeven', T::Integer, array(0, 10), 2),
