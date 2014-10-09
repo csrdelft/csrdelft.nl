33be2772830e920cc21e62315d7ed315281efd7d
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-10-09 14:25:03 +0000

    [wiki] Upgrade to Hrun 2014-09-29a

--- htdocs/wiki/lib/plugins/config/settings/config.metadata.php
+++ htdocs/wiki/lib/plugins/config/settings/config.metadata.php
@@ -117,7 +117,6 @@ $meta['typography']  = array('multichoice','_choices' => array(0,1,2));
 $meta['dformat']     = array('string');
 $meta['signature']   = array('string');
 $meta['showuseras']  = array('multichoice','_choices' => array('loginname','username','username_link','email','email_link'));
-$meta['profiellink'] = array('string'); 
 $meta['toptoclevel'] = array('multichoice','_choices' => array(1,2,3,4,5));   // 5 toc levels
 $meta['tocminheads'] = array('multichoice','_choices' => array(0,1,2,3,4,5,10,15,20));
 $meta['maxtoclevel'] = array('multichoice','_choices' => array(0,1,2,3,4,5));
