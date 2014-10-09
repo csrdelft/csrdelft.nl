7a4ab0f7f1b792c11556e7b0cdfd551d93fe4222
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-10-09 17:54:56 +0000

    [wiki] Add an alternative renderer, which extends default XHTML renderer, but modified the _simpleTitle() method. This approach is not overwritten on update.

---
+++ htdocs/wiki/lib/plugins/xhtmlniceurltitles/renderer.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Render Plugin for XHTML output with preserved linebreaks
+ *
+ * @author Chris Smith <chris@jalakai.co.uk>
+ */
+
+if(!defined('DOKU_INC')) die();
+if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
+
+require_once DOKU_INC . 'inc/parser/xhtml.php';
+
+/**
+ * The Renderer
+ */
+class renderer_plugin_xhtmlniceurltitles extends Doku_Renderer_xhtml {
+
+	function canRender($format) {
+		return ($format=='xhtml');
+	}
+
+	/**
+	 * Removes any Namespace from the given name but keeps
+	 * casing and special chars
+	 *
+	 * [2014-10-09: copy of Doku_Renderer::_simpleTitle() , extended with sepchar replacement ]
+	 *
+	 * @author Andreas Gohr <andi@splitbrain.org>
+	 * @author Gerrit Uitslag <klapinklapin@gmail.com>
+	 */
+	function _simpleTitle($name) {
+		global $conf;
+
+		//if there is a hash we use the ancor name only
+		@list($name, $hash) = explode('#', $name, 2);
+		if($hash) return $hash;
+
+		if($conf['useslash']) {
+			$name = strtr($name, ';/', ';:');
+		} else {
+			$name = strtr($name, ';', ':');
+		}
+
+		$name = noNSorNS($name);
+
+ 		//replace the sepchar i.e. _, - or . by spaces in titles
+ 		return strtr($name, $conf['sepchar'], ' ');
+	}
+
+}
+
+//Setup VIM: ex: et ts=4 enc=utf-8 :
