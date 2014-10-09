fbcaf3c7dddc67302ec062833f4a4d3a3c75ad1e
Paul Brussee <paul.brussee@localhost>
2014-10-09 22:54:21 +0000

    wiki main menu werkt; remove wiki iframe;

--- htdocs/layout/js/wiki.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function () {
-	var $iframe = jQuery('#wikiframe');
-	jQuery(window).resize(function () {
-		$iframe.css({
-			'height': window.innerHeight - 3
-		});
-	});
-	jQuery(document).trigger('resize');
-});
--- htdocs/wiki/lib/tpl/dokuwiki/csr_main_open.php
+++ htdocs/wiki/lib/tpl/dokuwiki/csr_main_open.php
@@ -1,6 +1,7 @@
 <?php
 
 $smarty = CsrSmarty::instance();
+$smarty->assign('mainmenu', $wiki->getBody());
 $smarty->display('MVC/layout/pagina_header.tpl');
 
 echo '<main class="cd-main-content">';
