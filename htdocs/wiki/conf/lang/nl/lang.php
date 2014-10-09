8d3eee07fb233a039dc306875ec502fcdb796af8
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-10-09 15:39:21 +0000

    [wiki] modify some language strings in tabs of vector template, to fit into C.S.R. layout.

---
+++ htdocs/wiki/conf/lang/nl/lang.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Overrule een paar lokalisatie strings
+ */
+
+//tabjes mogen kortere naam (vector template + csr layout)
+$lang['btn_subscribe']         = 'Volgen';
+$lang['btn_revs']              = 'Versies';
+
