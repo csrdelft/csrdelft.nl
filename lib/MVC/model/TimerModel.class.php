0fa613c129cc1312dedc264d4c3ec5ba6e2f3ae0
Paul Brussee <paul.brussee@localhost>
2014-10-10 09:24:48 +0000

    separate times;

--- lib/MVC/model/TimerModel.class.php
+++ lib/MVC/model/TimerModel.class.php
@@ -12,7 +12,7 @@ class TimerModel extends PersistenceModel {
 
 	protected static $instance;
 	/**
-	 * Log twice: before and after view
+	 * Time before and after view
 	 * @var float
 	 */
 	private $time_before_view;
@@ -27,14 +27,14 @@ class TimerModel extends PersistenceModel {
 		if ($measurement) {
 			$measurement->counter++;
 			$measurement->total_time += $this->time_before_view;
-			$measurement->total_time_view += $time;
+			$measurement->total_time_view += $time - $this->time_before_view;
 			$this->update($measurement);
 		} else {
 			$measurement = new ExecutionTime();
 			$measurement->request = REQUEST_URI;
 			$measurement->counter = 1;
 			$measurement->total_time = $this->time_before_view;
-			$measurement->total_time_view = $time;
+			$measurement->total_time_view = $time - $this->time_before_view;
 			$this->create($measurement);
 		}
 	}
