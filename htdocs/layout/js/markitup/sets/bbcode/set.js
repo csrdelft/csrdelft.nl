410a08ebe4d73318b52cee255fde6c7455754471
Paul Brussee <paul.brussee@localhost>
2014-10-16 01:10:11 +0000

    meer knopjes;

--- htdocs/layout/js/markitup/sets/bbcode/set.js
+++ htdocs/layout/js/markitup/sets/bbcode/set.js
@@ -17,27 +17,42 @@ mySettings = {
 		{name: 'Onderstreept', key: 'U', openWith: '[u]', closeWith: '[/u]'},
 		{name: 'Doorgestreept', key: 'S', openWith: '[s]', closeWith: '[/s]'},
 		{separator: ' '},
-		{name: 'Afbeelding', replaceWith: '[img][![Url]!][/img]'},
-		{name: 'Link', key: 'L', openWith: '[url=[![Url]!]]', closeWith: '[/url]', placeHolder: 'Link tekst'},
+		{name: 'Offtopic', key: 'O', openWith: '[offtopic]', closeWith: '[/offtopic]'},
+		{name: 'Citaat', key: 'Q', openWith: '[citaat=Naam_of_lidnummer]', closeWith: '[/citaat]'},
 		{separator: ' '},
-		/*{name: 'Grootte', key: 'G', openWith: '[size=[![Text size]!]]', closeWith: '[/size]',
+		{name: 'Link', key: 'L', openWith: '[url=[![Url]!]]', closeWith: '[/url]', placeHolder: 'Link tekst'},
+		{name: 'Email', key: 'E', openWith: '[email=[![Email adres]!]]', closeWith: '[/email]', placeHolder: 'Link tekst'},
+		{separator: ' '},
+		{name: 'Fotoalbum', replaceWith: '[fotoalbum][![Url]!][/fotoalbum]'},
+		{name: 'Poster of foto uit album', replaceWith: '[foto][![Url]!][/foto]'},
+		{name: 'Afbeelding', replaceWith: '[img][![Url]!][/img]'},
+		{name: 'Video', replaceWith: '[video][![Url]!][/video]'},
+		{separator: ' '},
+		{name: 'Kaart', openWith: '[locatie]', closeWith: '[/locatie]', placeHolder: 'C.S.R. Delft'},
+		{name: 'Verklapper', openWith: '[verklapper]', closeWith: '[/verklapper]'},
+		{name: 'Privé', openWith: '[prive]', closeWith: '[/prive]', placeHolder: 'Afgeschermde gegevens'}
+		/*
+		{name: 'Kop',
 			dropMenu: [
-				{name: 'Groot', openWith: '[size=200]', closeWith: '[/size]'},
-				{name: 'Normaal', openWith: '[size=100]', closeWith: '[/size]'},
-				{name: 'Klein', openWith: '[size=50]', closeWith: '[/size]'}
+				{name: 'H1', openWith: '[h=1]', closeWith: '[/h]'},
+				{name: 'H2', openWith: '[h=2]', closeWith: '[/h]'},
+				{name: 'H3', openWith: '[h=3]', closeWith: '[/h]'},
+				{name: 'H4', openWith: '[h=4]', closeWith: '[/h]'},
+				{name: 'H5', openWith: '[h=5]', closeWith: '[/h]'},
+				{name: 'H6', openWith: '[h=6]', closeWith: '[/h]'}
 			]},
-		{separator: '---------------'},
+		{separator: ' '},
 		{name: 'Genummerde lijst', openWith: '[list=[![Starting number]!]]\n', closeWith: '\n[/list]'},
 		{name: 'Ongenummerde lijst', openWith: '[list]\n', closeWith: '\n[/list]'},
 		{name: 'Lijstpunt', openWith: '[*] '},
-		{separator: '---------------'},*/
-		{name: 'Offtopic', key: 'O', openWith: '[offtopic]', closeWith: '[/offtopic]'},
-		{name: 'Citaat', key: 'Q', openWith: '[citaat=Naam_of_lidnummer]', closeWith: '[/citaat]'},
+		{separator: ' '},
 		{name: 'Code', openWith: '[code]', closeWith: '[/code]'},
+		{name: 'Opmaakcode tonen', openWith: '[ubboff]', closeWith: '[/ubboff]'},
 		{separator: ' '},
 		{name: 'Opmaak wissen', className: "clean", replaceWith: function (markitup) {
 				return markitup.selection.replace(/\[(.*?)\]/g, "")
-			}}/*,
-		{name: 'Voorbeeld', className: "preview", call: 'preview'}*/
+			}},
+		{name: 'Voorbeeld', className: "preview", call: 'preview'}
+		 */
 	]
 };
