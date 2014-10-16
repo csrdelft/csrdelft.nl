85bbecc195a93b3f5f071a6ee6e3bc7037f8ae50
Paul Brussee <paul.brussee@localhost>
2014-10-16 11:59:30 +0000

    tweaks;

--- htdocs/layout/js/markitup/sets/bbcode/set.js
+++ htdocs/layout/js/markitup/sets/bbcode/set.js
@@ -16,18 +16,18 @@ mySettings = {
 		{name: 'Cursief', key: 'I', openWith: '[i]', closeWith: '[/i]'},
 		{name: 'Onderstreept', key: 'U', openWith: '[u]', closeWith: '[/u]'},
 		{name: 'Doorgestreept', key: 'S', openWith: '[s]', closeWith: '[/s]'},
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Offtopic', key: 'O', openWith: '[offtopic]', closeWith: '[/offtopic]'},
 		{name: 'Citaat', key: 'Q', openWith: '[citaat=Naam_of_lidnummer]', closeWith: '[/citaat]'},
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Link', key: 'L', openWith: '[url=[![Url]!]]', closeWith: '[/url]', placeHolder: 'Link tekst'},
 		{name: 'Email', key: 'E', openWith: '[email=[![Email adres]!]]', closeWith: '[/email]', placeHolder: 'Link tekst'},
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Fotoalbum', replaceWith: '[fotoalbum][![Url]!][/fotoalbum]'},
 		{name: 'Poster of foto uit album', replaceWith: '[foto][![Url]!][/foto]'},
 		{name: 'Afbeelding', replaceWith: '[img][![Url]!][/img]'},
 		{name: 'Video', replaceWith: '[video][![Url]!][/video]'},
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Kaart', openWith: '[locatie]', closeWith: '[/locatie]', placeHolder: 'C.S.R. Delft'},
 		{name: 'Verklapper', openWith: '[verklapper]', closeWith: '[/verklapper]'},
 		{name: 'Privé', openWith: '[prive]', closeWith: '[/prive]', placeHolder: 'Afgeschermde gegevens'},/*
@@ -40,14 +40,14 @@ mySettings = {
 				{name: 'H5', openWith: '[h=5]', closeWith: '[/h]'},
 				{name: 'H6', openWith: '[h=6]', closeWith: '[/h]'}
 			]},
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Genummerde lijst', openWith: '[list=[![Starting number]!]]\n', closeWith: '\n[/list]'},
 		{name: 'Ongenummerde lijst', openWith: '[list]\n', closeWith: '\n[/list]'},
 		{name: 'Lijstpunt', openWith: '[*] '},*/
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Code', openWith: '[code]', closeWith: '[/code]'},
 		{name: 'Opmaakcode tonen', openWith: '[ubboff]', closeWith: '[/ubboff]'},
-		{separator: ' '},
+		{separator: '|'},
 		{name: 'Opmaak wissen', className: "clean", replaceWith: function (markitup) {
 				return markitup.selection.replace(/\[(.*?)\]/g, "")
 			}}
