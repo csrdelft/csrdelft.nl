497cd8ae5ee59d846630f1e76d9774029f397309
Paul Brussee <paul.brussee@localhost>
2014-10-21 00:52:11 +0000

    tweak;

--- lib/MVC/model/MenuModel.class.php
+++ lib/MVC/model/MenuModel.class.php
@@ -72,7 +72,7 @@ class MenuModel extends CachedPersistenceModel {
 
 	public function getChildren(MenuItem $parent) {
 		$children = array();
-		foreach ($this->find('parent_id = ?', array($parent->item_id), 'prioriteit ASC') as $child) {
+		foreach ($this->find('parent_id = ?', array($parent->item_id), 'prioriteit ASC, tekst ASC') as $child) {
 			$children[] = $child;
 			// cache for getParent()
 			$key = $this->cacheKey($child->getValues(true));
