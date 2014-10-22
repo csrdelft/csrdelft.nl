c85d90413ca0cfcf620b5f3f797ac9a47d12edfd
Gerben Oolbekkink <gerben.oolbekkink@localhost>
2014-10-22 11:03:55 +0000

    Eetplangenerator

--- htdocs/tools/eetplan.php
+++ htdocs/tools/eetplan.php
@@ -21,7 +21,7 @@ for ($es = 1; $es <= 47; $es++) {
 
 
 # koppel de huizennummers aan huizen
-$kh = array(0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
+$kh = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
 $khd = array(
 	0 => array('', '', ''),
 	1 => array('Huize Ihnshthabhielh', 1438, ''),
