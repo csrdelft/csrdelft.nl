e94f663b53a14b3ebdc5c523ec5febf06bbabe87
Paul Brussee <paul.brussee@localhost>
2014-11-18 15:21:51 +0000

    item form;

--- lib/MVC/view/happie/MenukaartView.class.php
+++ lib/MVC/view/happie/MenukaartView.class.php
@@ -40,7 +40,18 @@ class HappieMenukaartItemForm extends Formulier {
 
 	public function __construct(HappieMenukaartItem $item, $action = '/nieuw', $titel = 'Nieuw menukaart-item') {
 		parent::__construct($item, get_class($this), happieUrl . $action, $titel);
-		$this->generateFields();
+
+		$groepen = HappieMenukaartGroepenModel::instance()->prefetch();
+		$opties = array();
+		foreach ($groepen as $groep) {
+			$opties[$groep->groep_id] = $groep->titel;
+		}
+		$fields[] = new SelectField('groep_id', $item->groep_id, 'Menugroep', $opties);
+		$fields[] = new RequiredTextField('naam', $item->naam, 'Gerechtnaam');
+		$fields[] = new TextareaField('beschrijving', $item->beschrijving, 'Omschrijving');
+		$fields[] = new TextareaField('allergie_info', $item->allergie_info, 'Allergie-informatie');
+		$fields[] = new BedragField('prijs', $item->prijs, 'Prijs');
+		$fields[] = new RequiredIntField('aantal_beschikbaar', $item->aantal_beschikbaar, 'Beschikbaar #');
 
 		$fields[] = new FormDefaultKnoppen();
 		$this->addFields($fields);
@@ -67,7 +78,7 @@ class HappieMenukaartGroepenJson extends DataTableResponse {
 class HappieMenukaartGroepenView extends DataTable {
 
 	public function __construct() {
-		parent::__construct(HappieMenukaartGroepenModel::orm, get_class($this), 'Menukaart', 'gang');
+		parent::__construct(HappieMenukaartGroepenModel::orm, get_class($this), 'Menukaart groepen', 'gang');
 		$this->dataSource = happieUrl . '/data';
 
 		$toolbar = new DataTableToolbar();
