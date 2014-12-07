916712b71129c2b9bf15b1ed6a9cd452e7589cb7
Paul Brussee <paul.brussee@localhost>
2014-12-07 00:45:00 +0000

    refactor leesbaar;

--- lib/MVC/model/entity/Afbeelding.class.php
+++ lib/MVC/model/entity/Afbeelding.class.php
@@ -11,6 +11,11 @@ require_once 'MVC/model/entity/Bestand.class.php';
 class Afbeelding extends Bestand {
 
 	/**
+	 * Geaccepteerde afbeelding types
+	 * @var array
+	 */
+	public static $mimeTypes = array('image/jpeg', 'image/png', 'image/gif', 'image/tiff');
+	/**
 	 * Breedte in pixels
 	 * @var int
 	 */
@@ -20,23 +25,22 @@ class Afbeelding extends Bestand {
 	 * @var int
 	 */
 	public $height;
-	/**
-	 * Mime-types van afbeeldingen
-	 * @var array
-	 */
-	public static $mimeTypes = array('image/png', 'image/jpeg', 'image/gif');
 
 	public function __construct($path, $parse = true) {
 		parent::__construct();
 		if ($parse) {
-			$this->filesize = @filesize($path);
-			$image = @getimagesize($path);
-			if ($image AND $this->filesize !== false) {
-				$this->width = $image[0];
-				$this->height = $image[1];
-				$this->mimetype = $image['mime'];
+			if ($this->exists()) {
+				$this->filesize = @filesize($path);
+				$image = @getimagesize($path);
+				if ($image AND $this->filesize !== false) {
+					$this->width = $image[0];
+					$this->height = $image[1];
+					$this->mimetype = $image['mime'];
+				} else {
+					throw new Exception('Afbeelding parsen mislukt: ' . $path);
+				}
 			} else {
-				throw new Exception('Afbeelding parsen mislukt: ' . $path);
+				throw new Exception('Afbeelding niet leesbaar: ' . $path);
 			}
 		}
 	}
