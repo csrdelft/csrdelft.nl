7d9c9fd1b4240f03a766e884c8c2a1dde589312c
Paul Brussee <paul.brussee@localhost>
2014-12-10 22:23:36 +0000

    revert previous commit;

--- lib/MVC/model/MenuModel.class.php
+++ lib/MVC/model/MenuModel.class.php
@@ -36,8 +36,7 @@ class MenuModel extends CachedPersistenceModel {
 		}
 		$key = $naam . '-menu';
 		if ($this->isCached($key, true)) {
-			// inladen van memcache in runtime cache
-			return $this->cacheResult($this->getCached($key, true), false);
+			return $this->getCached($key, true);
 		}
 		// not cached
 		$root = $this->getMenuRoot($naam);
