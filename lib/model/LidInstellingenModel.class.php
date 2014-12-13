2796e53475a4dd7c6333b982b6efc44a8e6441c0
Paul Brussee <paul.brussee@localhost>
2014-12-13 00:17:43 +0000

    Duckstadnaam standaard;

--- lib/model/LidInstellingenModel.class.php
+++ lib/model/LidInstellingenModel.class.php
@@ -73,7 +73,7 @@ class LidInstellingen extends Instellingen {
 			'draden_per_pagina'		 => array('Draadjes per pagina', T::Integer, array(5, 100), 20),
 			'posts_per_pagina'		 => array('Berichten per pagina', T::Integer, array(5, 100), 20),
 			'zoekresultaten'		 => array('Zoekresultaten per pagina', T::Integer, array(10, 50), 20),
-			'naamWeergave'			 => array('Naamweergave', T::Enumeration, array('civitas', 'volledig', 'bijnaam', 'aaidrom', 'Duckstad'), 'civitas'),
+			'naamWeergave'			 => array('Naamweergave', T::Enumeration, array('civitas', 'volledig', 'bijnaam', 'aaidrom', 'Duckstad'), 'Duckstad'),
 			'datumWeergave'			 => array('Datumweergave', T::Enumeration, array('relatief', 'vast'), 'relatief'),
 			'ongelezenWeergave'		 => array('Ongelezenweergave', T::Enumeration, array('cursief', 'dikgedrukt', 'onderstreept', 'alsof-gelezen'), 'cursief'),
 			'open_draad_op_pagina'	 => array('Open onderwerp op pagina', T::Enumeration, array('1', 'ongelezen', 'laatste'), 'ongelezen'),
