3aa7a55cd94796d71dc4be1bf3aa7b8866cc2e4b
Paul Brussee <paul.brussee@localhost>
2014-12-15 04:12:34 +0000

    uitzondering voor é;

--- lib/controller/FotoAlbumController.class.php
+++ lib/controller/FotoAlbumController.class.php
@@ -277,7 +277,7 @@ class FotoAlbumController extends AclController {
 			$limit = (int) $this->getParam('limit');
 		}
 		$result = array();
-		foreach ($this->model->find('subdir REGEXP ?', array($query . '[^/]*[/]{1}$'), 'subdir DESC', null, $limit) as $album) {
+		foreach ($this->model->find('replace(subdir, "é", "e") REGEXP ?', array($query . '[^/]*[/]{1}$'), 'subdir DESC', null, $limit) as $album) {
 			$result[] = array(
 				'url'	 => $album->getUrl(),
 				'value'	 => ucfirst($album->dirname) . '<span class="lichtgrijs"> - ' . $album->getParentName() . '</span>'
