347f384075537a14c4b9b6e4e6a8114a1c5a5c80
Paul Brussee <paul.brussee@localhost>
2014-12-17 07:42:21 +0000

    bugfix;

--- htdocs/wiki/lib/tpl/dokuwiki/csr_main_open.php
+++ htdocs/wiki/lib/tpl/dokuwiki/csr_main_open.php
@@ -2,6 +2,6 @@
 
 $smarty = CsrSmarty::instance();
 $smarty->assign('mainmenu', $wiki->getBody());
-$smarty->display('layout/pagina_header.tpl');
+$smarty->display('csrdelft/pagina_header.tpl');
 
 echo '<main class="cd-main-content">';
