404ba7f2cde365e722b6e43f56701645c1f0847f
Gerrit Uitslag <gerrit.uitslag@localhost>
2014-12-20 19:10:08 +0000

    [wiki]update docnavigation plugin

--- htdocs/wiki/lib/plugins/docnavigation/action.php
+++ htdocs/wiki/lib/plugins/docnavigation/action.php
@@ -11,6 +11,9 @@ if(!defined('DOKU_INC')) die();
 
 require_once(DOKU_INC.'inc/parser/xhtml.php');
 
+/**
+ * Add documentation navigation elements around page
+ */
 class action_plugin_docnavigation extends DokuWiki_Action_Plugin {
 
     /**
@@ -18,9 +21,10 @@ class action_plugin_docnavigation extends DokuWiki_Action_Plugin {
      *
      * @param Doku_Event_Handler $controller
      */
-    public function register(Doku_Event_Handler &$controller) {
+    public function register(Doku_Event_Handler $controller) {
         $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, '_addtopnavigation');
-        $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, '_addbottomnavigation');
+//        // insert bottom navigation before discussion plugin (seq=0)
+//        $controller->register_hook('TPL_CONTENT_DISPLAY', 'BEFORE', $this, '_addbottomnavigation', -1001);
     }
 
     /**
@@ -31,10 +35,12 @@ class action_plugin_docnavigation extends DokuWiki_Action_Plugin {
      */
     public function _addtopnavigation(Doku_Event &$event, $param) {
         global $ACT;
-        //if($ACT != 'show') return;
-        if($event->data[0] != 'xhtml' || !in_array($ACT, array('show'))) return;
 
-        $event->data[1] = $this->getNavbar($showtoc = false).$event->data[1];
+        if($event->data[0] != 'xhtml' || !in_array($ACT, array('show', 'preview'))) return;
+
+        $event->data[1] = $this->getNavbar($linktoToC = false)
+                        . $event->data[1]
+                        . $this->getNavbar($linktoToC = true);
     }
 
     /**
@@ -47,43 +53,66 @@ class action_plugin_docnavigation extends DokuWiki_Action_Plugin {
         global $ACT;
         if(!in_array($ACT, array('show'))) return;
 
-        //insert before the discussion plugin
-        $pos = strrpos ( $event->data , '<div class="comment_wrapper" id="comment_wrapper">');
-        if($pos) {
-            $event->data = substr_replace($event->data, $this->getNavbar($showtoc = true), $pos, 0);
-        } else {
-            $event->data = $event->data.$this->getNavbar($showtoc = true);
-        }
+//        //insert before the discussion plugin
+//        $pos = strrpos ( $event->data , '<div class="comment_wrapper" id="comment_wrapper">');
+//        if($pos) {
+//            $event->data = substr_replace($event->data, $this->getNavbar($linktoToC = true), $pos, 0);
+//        } else {
+            $event->data = $event->data . $this->getNavbar($linktoToC = true);
+//        }
     }
 
     /**
      * Return html of navigation elements
      *
-     * @param bool $showtoc add referer to ToC
+     * @param bool $linktoToC add referer to ToC
      * @return string
      */
-    private function getNavbar($showtoc = true) {
+    private function getNavbar($linktoToC = true) {
         global $ID;
-        $data = p_get_metadata($ID, 'docnavigation');
+        global $ACT;
+        if($ACT == 'preview') {
+            // the RENDERER_CONTENT_POSTPROCESS event is triggered just after rendering the instruction,
+            // so syntax instance will exists
+            /** @var syntax_plugin_docnavigation_pagenav $pagenav */
+            $pagenav = plugin_load('syntax', 'docnavigation_pagenav');
+            if($pagenav) {
+                $data = $pagenav->data[$ID];
+            } else {
+                $data = array();
+            }
+        } else {
+            $data = p_get_metadata($ID, 'docnavigation');
+        }
 
         $out = '';
         if(!empty($data)) {
-            $renderer = new Doku_Renderer_xhtml;
+            /** @var Doku_Renderer_xhtml $Renderer */
+            $Renderer = p_get_renderer('xhtml');
+            if (is_null($Renderer)) return null;
 
-            if($showtoc) $out .= '<div class="clearer"></div>'.DOKU_LF;
+            if($linktoToC) {
+                $out .= '<div class="clearer"></div>'.DOKU_LF;
+            }
 
-            $out .= '<div class="docnavbar'.($showtoc ? ' showtoc' : '').'">'.DOKU_LF.DOKU_TAB.'<div class="leftnav">';
-            if($data['previous'][0]) $out .= '← '.$renderer->internallink($data['previous'][0], $data['previous'][1], null, true);
+            $out .= '<div class="docnavbar'.($linktoToC ? ' showtoc' : '').'">'.DOKU_LF.DOKU_TAB.'<div class="leftnav">';
+            if($data['previous'][0]) {
+                $out .= '← '.$Renderer->internallink($data['previous'][0], $data['previous'][1], null, true);
+            }
             $out .= '&nbsp;</div>'.DOKU_LF;
 
-            if($showtoc) {
+            if($linktoToC) {
                 $out .= DOKU_TAB.'<div class="centernav">';
-                if($data['toc'][0]) $out .= $renderer->internallink($data['toc'][0], $data['toc'][1], null, true);
+                if($data['toc'][0]) {
+                    $out .= $Renderer->internallink($data['toc'][0], $data['toc'][1], null, true);
+                }
                 $out .= '&nbsp;</div>'.DOKU_LF;
             }
 
             $out .= DOKU_TAB.'<div class="rightnav">&nbsp;';
-            if($data['next'][0]) $out .= $renderer->internallink($data['next'][0], $data['next'][1], null, true).' →';
+            if($data['next'][0]) {
+                $out .= $Renderer->internallink($data['next'][0], $data['next'][1], null, true).' →';
+            }
             $out .= '</div>'.DOKU_LF.'</div>'.DOKU_LF;
         }
         return $out;
--- htdocs/wiki/lib/plugins/docnavigation/syntax/pagenav.php
+++ htdocs/wiki/lib/plugins/docnavigation/syntax/pagenav.php
@@ -9,25 +9,75 @@
 // must be run within Dokuwiki
 if(!defined('DOKU_INC')) die();
 
+/**
+ * Handles document navigation syntax
+ */
 class syntax_plugin_docnavigation_pagenav extends DokuWiki_Syntax_Plugin {
 
+    /**
+     * Stores data of navigation per page (for preview)
+     *
+     * @var array
+     */
+    public $data = array();
+
+    /**
+     * Syntax Type
+     *
+     * Needs to return one of the mode types defined in $PARSER_MODES in parser.php
+     *
+     * @return string
+     */
     public function getType() {
         return 'substition';
     }
 
+    /**
+     * Paragraph Type
+     *
+     * Defines how this syntax is handled regarding paragraphs. This is important
+     * for correct XHTML nesting. Should return one of the following:
+     *
+     * 'normal' - The plugin can be used inside paragraphs
+     * 'block'  - Open paragraphs need to be closed before plugin output
+     * 'stack'  - Special case. Plugin wraps other paragraphs.
+     *
+     * @see Doku_Handler_Block
+     *
+     * @return string
+     */
     public function getPType() {
         return 'block';
     }
 
+    /**
+     * Sort for applying this mode
+     *
+     * @return int
+     */
     public function getSort() {
         return 150;
     }
 
+    /**
+     * @param string $mode
+     */
     public function connectTo($mode) {
         $this->Lexer->addSpecialPattern('<-[^\n]*\^[^\n]*\^[^\n]*->', $mode, 'plugin_docnavigation_pagenav');
     }
 
-    public function handle($match, $state, $pos, &$handler) {
+    /**
+     * Handler to prepare matched data for the rendering process
+     *
+     * Usually you should only need the $match param.
+     *
+     * @param   string       $match   The text matched by the patterns
+     * @param   int          $state   The lexer state for the match
+     * @param   int          $pos     The character position of the matched text
+     * @param   Doku_Handler $handler The Doku_Handler object
+     * @return  bool|array Return an array with all data you want to use in render, false don't add an instruction
+     */
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
         global $conf, $ID;
 
         // links are: 0=previous, 1=toc, 2=next
@@ -63,16 +113,23 @@ class syntax_plugin_docnavigation_pagenav extends DokuWiki_Syntax_Plugin {
             'toc'      => $links[1],
             'next'     => $links[2]
         );
+
+        // store data for preview
+        $this->data[$ID] = $data;
+
+        // return instruction data for renderers
         return $data;
     }
 
     /**
-     * @param string                 $mode
-     * @param Doku_Renderer_metadata $renderer
-     * @param array                  $data return of handler
-     * @return bool
+     * Handles the actual output creation.
+     *
+     * @param string          $mode     output format being rendered
+     * @param Doku_Renderer   $renderer the current renderer object
+     * @param array           $data     data created by handler()
+     * @return  boolean                 rendered correctly? (however, returned value is not used at the moment)
      */
-    public function render($mode, &$renderer, $data) {
+    public function render($mode, Doku_Renderer $renderer, $data) {
         if($mode == 'metadata') {
             /** @var Doku_Renderer_metadata $renderer */
             $renderer->meta['docnavigation'] = $data;
