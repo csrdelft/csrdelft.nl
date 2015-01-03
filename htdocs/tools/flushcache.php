bc66894ead6182a41bebd02279a238e2ff2a474a
Paul Brussee <paul.brussee@localhost>
2015-01-03 13:43:29 +0000

    memcache flush meldingen;

--- htdocs/tools/flushcache.php
+++ htdocs/tools/flushcache.php
@@ -8,7 +8,11 @@ require_once 'configuratie.include.php';
 
 if (LoginModel::mag('P_ADMIN')) {
 
-	CsrMemcache::instance()->flush();
+	if (CsrMemcache::instance()->flush()) {
+		setMelding('Memcache succesvol geflushed', 1);
+	} else {
+		setMelding('Memcache flushen mislukt', -1);
+	}
 
 	redirect(CSR_ROOT . '/tools/memcachestats.php');
 }
--- htdocs/tools/memcachestats.php
+++ htdocs/tools/memcachestats.php
@@ -8,6 +8,8 @@ require_once 'configuratie.include.php';
 
 if (LoginModel::mag('P_ADMIN')) {
 
+	echo getMelding();
+
 	echo '<h1>MemCache statistieken</h1>';
 
 	debugprint(CsrMemcache::instance()->getStats());
