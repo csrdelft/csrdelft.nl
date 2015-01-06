db8b7514d664bf4b579c7912a9a80f13a0854832
Paul Brussee <paul.brussee@localhost>
2015-01-06 02:12:44 +0000

    aanroeper verantwoordelijk voor volledige link;

--- lib/common.functions.php
+++ lib/common.functions.php
@@ -264,9 +264,6 @@ function external_url($url, $label) {
 			$extern = ' target="_blank"';
 		} else {
 			$extern = '';
-			if (startsWith($url, '/')) {
-				$url = CSR_ROOT . $url;
-			}
 		}
 		$result = '<a href="' . $url . '" title="' . $url . '"' . $extern . '>' . $label . '</a>';
 	} else {
