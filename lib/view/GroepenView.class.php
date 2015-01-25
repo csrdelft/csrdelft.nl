207897e7270dc79b0aac67dc91c6b872c70841a1
Paul Brussee <paul.brussee@localhost>
2015-01-25 22:14:57 +0000

    tweak;

--- lib/view/GroepenView.class.php
+++ lib/view/GroepenView.class.php
@@ -53,7 +53,7 @@ class GroepenBeheerData extends DataTableResponse {
 		$array = $groep->jsonSerialize();
 
 		$array['detailSource'] = $groep->getUrl() . 'leden'; // TODO: 2 childrow's A::Rechten;
-		$array['naam'] = '<span title="'.$groep->naam .'&#13;' . substr($groep->samenvatting, 0, 100) . (strlen($groep->samenvatting) > 100 ? '...' : '' ) . '">' . $groep->naam . '</span>';
+		$array['naam'] = '<span title="' . $groep->naam . (empty($groep->samenvatting) ? '' : '&#13;&#13;') . substr($groep->samenvatting, 0, 100) . (strlen($groep->samenvatting) > 100 ? '...' : '' ) . '">' . $groep->naam . '</span>';
 		$array['samenvatting'] = null;
 		$array['omschrijving'] = null;
 		$array['website'] = null;
