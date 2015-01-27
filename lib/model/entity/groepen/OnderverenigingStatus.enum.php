6b6501d1b6abadfd4a0614e682a4f7dc017b2ffc
Paul Brussee <paul.brussee@localhost>
2015-01-27 15:18:11 +0000

    order;

--- lib/model/entity/groepen/OnderverenigingStatus.enum.php
+++ lib/model/entity/groepen/OnderverenigingStatus.enum.php
@@ -10,18 +10,18 @@
  */
 abstract class OnderverenigingStatus implements PersistentEnum {
 
-	const Ondervereniging = 'o';
 	const AdspirantOndervereniging = 'a';
+	const Ondervereniging = 'o';
 	const VoormaligOndervereniging = 'v';
 
 	public static function getTypeOptions() {
-		return array(self::Ondervereniging, self::AdspirantOndervereniging, self::VoormaligOndervereniging);
+		return array(self::AdspirantOndervereniging, self::Ondervereniging, self::VoormaligOndervereniging);
 	}
 
 	public static function getDescription($option) {
 		switch ($option) {
-			case self::Ondervereniging: return 'ondervereniging';
 			case self::AdspirantOndervereniging: return 'adspirant-ondervereniging';
+			case self::Ondervereniging: return 'ondervereniging';
 			case self::VoormaligOndervereniging: return 'voormalig ondervereniging';
 			default: throw new Exception('OnderverenigingStatus onbekend');
 		}
@@ -29,8 +29,8 @@ abstract class OnderverenigingStatus implements PersistentEnum {
 
 	public static function getChar($option) {
 		switch ($option) {
-			case self::Ondervereniging:
 			case self::AdspirantOndervereniging:
+			case self::Ondervereniging:
 			case self::VoormaligOndervereniging:
 				return $option;
 			default: throw new Exception('OnderverenigingStatus onbekend');
