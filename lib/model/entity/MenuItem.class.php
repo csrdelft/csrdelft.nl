2facfe731b0c49ad99fac5243ff8217b3d43ad0b
Paul Brussee <paul.brussee@localhost>
2015-01-30 07:29:24 +0000

    tweak;

--- lib/model/entity/MenuItem.class.php
+++ lib/model/entity/MenuItem.class.php
@@ -143,7 +143,7 @@ class MenuItem extends PersistentEntity {
 				$draad = ForumDradenModel::instance()->get((int) $draad_id);
 				return $draad->isOngelezen();
 			} catch (Exception $e) {
-				setMelding('Uw favoriete forumdraadje bestaat helaas niet meer: ' . htmlspecialchars($this->link), 2);
+				setMelding('Uw favoriete forumdraadje bestaat helaas niet meer: ' . htmlspecialchars($this->tekst), 2);
 			}
 		}
 		return false;
