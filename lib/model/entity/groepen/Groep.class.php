80c933c5da030cd7618ffd9f72e2c51808160b68
Paul Brussee <paul.brussee@localhost>
2015-02-01 22:02:30 +0000

    uitzondering voor jezelf;

--- lib/model/entity/groepen/Groep.class.php
+++ lib/model/entity/groepen/Groep.class.php
@@ -162,8 +162,9 @@ class Groep extends PersistentEntity {
 	 * @return boolean
 	 */
 	public function mag($action, $feed = false) {
-		// Beheerders en de aanmaker van de groep mag alles
-		if ($this->maker_uid === LoginModel::getUid() OR LoginModel::mag('P_LEDEN_MOD')) {
+		$zelf = in_array($action, array(A::Aanmelden, A::Bewerken, A::Afmelden));
+		// Beheerders en de maker van de groep mogen alles (uitzondering voor jezelf)
+		if (!$zelf AND ( $this->maker_uid === LoginModel::getUid() OR LoginModel::mag('P_LEDEN_MOD') )) {
 			return true;
 		}
 		// Rechten voor deze specifieke groep
@@ -184,8 +185,9 @@ class Groep extends PersistentEntity {
 	 * @return boolean
 	 */
 	public static function magAlgemeen($action, $soort = null) {
-		// Administrator mag alles
-		if (LoginModel::mag('P_ADMIN')) {
+		$zelf = in_array($action, array(A::Aanmelden, A::Bewerken, A::Afmelden));
+		// Administrator mag alles (uitzondering voor jezelf)
+		if (!$zelf AND LoginModel::mag('P_ADMIN')) {
 			return true;
 		}
 		if ($soort !== null) {
