f0ed1619cd4df6fb53ccba6680fef0519c936b11
Paul Brussee <paul.brussee@localhost>
2015-02-05 03:33:28 +0000

    tweak;

--- lib/view/GroepenView.class.php
+++ lib/view/GroepenView.class.php
@@ -450,28 +450,46 @@ class GroepenView implements View {
 class GroepenDeelnameGrafiek implements View {
 
 	private $series = array();
+	private $step = array();
 
 	public function __construct($groepen) {
 		$mannen = array();
 		$vrouwen = array();
+		$smallest_diff = PHP_INT_MAX;
+		$previous_time = 0;
 		foreach ($groepen as $groep) {
-			$tijd = strtotime($groep->begin_moment);
-			if (!isset($mannen[$tijd])) {
-				$mannen[$tijd] = 0;
-				$vrouwen[$tijd] = 0;
+			$time = strtotime($groep->begin_moment);
+			$smallest_diff = min($smallest_diff, abs($time - $previous_time));
+			$previous_time = $time;
+			if (!isset($mannen[$time])) {
+				$mannen[$time] = 0;
+				$vrouwen[$time] = 0;
 			}
 			foreach ($groep->getLeden() as $lid) {
 				$profiel = ProfielModel::get($lid->uid);
 				if ($profiel->geslacht === Geslacht::Man) {
-					$mannen[$tijd] += 1;
+					$mannen[$time] += 1;
 				} else {
-					$vrouwen[$tijd] += 1;
+					$vrouwen[$time] += 1;
 				}
 			}
 		}
-		foreach ($mannen as $tijd => $aantal) {
-			$this->series[0][] = array($tijd * 1000, $aantal);
-			$this->series[1][] = array($tijd * 1000, $vrouwen[$tijd]);
+		$year = 365 * 24 * 60 * 60;
+		$month = 30 * 24 * 60 * 60;
+		$day = 24 * 60 * 60;
+		if ($smallest_diff >= $year) {
+			$this->step[] = floor($smallest_diff / $year);
+			$this->step[] = 'year';
+		} elseif ($smallest_diff >= $month) {
+			$this->step[] = floor($smallest_diff / $month);
+			$this->step[] = 'month';
+		} else {
+			$this->step[] = floor($smallest_diff / $day);
+			$this->step[] = 'day';
+		}
+		foreach ($mannen as $time => $aantal) {
+			$this->series[0][] = array($time * 1000, $aantal);
+			$this->series[1][] = array($time * 1000, $vrouwen[$time]);
 		}
 	}
 
@@ -508,17 +526,21 @@ class GroepenDeelnameGrafiek implements View {
 						series: {
 							bars: {
 								show: true,
-								lineWidth: 5
+								lineWidth: 20
 							},
 							stack: true
+						}, yaxis: {
+							tickDecimals: 0
+						},
+						xaxis: {
+							autoscaleMargin: .01
 						},
 						xaxes: [{
-								mode: "time"
+								mode: "time",
+								minTickSize: <?= json_encode($this->step); ?>
 							}],
-						yaxis: {
-							tickDecimals: 0
-						}
 					};
+					console.log(options);
 					$.plot("#deelnamegrafiek", series, options);
 				});
 			</script>
