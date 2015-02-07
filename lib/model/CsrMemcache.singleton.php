e0325c75797ce6942427562eec04957106e5adae
Paul Brussee <paul.brussee@localhost>
2015-02-07 22:49:06 +0000

    freaking bugfix;

--- lib/model/CsrMemcache.singleton.php
+++ lib/model/CsrMemcache.singleton.php
@@ -29,8 +29,8 @@ class CsrMemcache {
 	public static function instance() {
 		if (!isset(self::$instance)) {
 			if (class_exists('Memcache')) {
-				self::$instance = new Memcache();
-				self::$connected = self::$instance->connect('unix://' . DATA_PATH . 'csrdelft-cache.socket', 0);
+				self::$instance = new Memcache(); // data path starts with /
+				self::$connected = self::$instance->connect('unix:/' . DATA_PATH . 'csrdelft-cache.socket', 0);
 			} else {
 				self::$instance = new DummyCache();
 			}
