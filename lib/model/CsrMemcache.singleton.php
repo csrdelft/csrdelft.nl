9f28987570d980d5680de48b79c9c3a3d64722fa
Paul Brussee <paul.brussee@localhost>
2015-02-07 23:01:05 +0000

    default port;

--- lib/model/CsrMemcache.singleton.php
+++ lib/model/CsrMemcache.singleton.php
@@ -30,7 +30,7 @@ class CsrMemcache {
 		if (!isset(self::$instance)) {
 			if (class_exists('Memcache')) {
 				self::$instance = new Memcache();
-				self::$connected = self::$instance->connect('unix://' . DATA_PATH . 'csrdelft-cache.socket', 0);
+				self::$connected = self::$instance->connect('unix://' . DATA_PATH . 'csrdelft-cache.socket');
 			} else {
 				self::$instance = new DummyCache();
 			}
