8ccac96cf44684aaadbc7e72e048093b0e27a149
Paul Brussee <paul.brussee@localhost>
2015-02-08 01:50:47 +0000

    werkomheen;

--- htdocs/tools/naamlink.php
+++ htdocs/tools/naamlink.php
@@ -25,9 +25,14 @@ if (isset($_GET['uid'])) {
 } elseif (isset($_POST['naam'])) {
 	$string = $_POST['naam'];
 	$given = 'naam';
-
+	// filter <span...
+	$pos = mb_strpos($string, '<');
+	if ($pos !== false) {
+		$string = trim(mb_substr($string, 0, $pos));
+	}
+}
 //geen input
-} else {
+else {
 	echo 'Fout in invoer in tools/naamlink.php';
 	exit;
 }
