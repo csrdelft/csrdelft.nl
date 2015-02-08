db9f336fae928c166155535bb58764295f8585f3
Paul Brussee <paul.brussee@localhost>
2015-02-08 19:10:32 +0000

    bugfix;

--- lib/controller/groepen/LichtingenController.class.php
+++ lib/controller/groepen/LichtingenController.class.php
@@ -18,7 +18,7 @@ class LichtingenController extends GroepenController {
 			$this->geentoegang();
 		}
 		$zoekterm = $this->getParam('q');
-		$data = range($this->model->getJongste(), $this->model->getOudste());
+		$data = range($this->model->getJongsteLidjaar(), $this->model->getOudsteLidjaar());
 		$found = preg_grep('/' . (int) $zoekterm . '/', $data);
 		$result = array();
 		foreach ($found as $lidjaar) {
