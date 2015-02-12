4be51dc8e283936451ccff9ca73e87f028ee1679
Paul Brussee <paul.brussee@localhost>
2015-02-12 16:39:38 +0000

    gesprekken;

--- lib/controller/GesprekkenController.class.php
+++ lib/controller/GesprekkenController.class.php
@@ -16,7 +16,7 @@ class GesprekkenController extends AclController {
 		parent::__construct($query, GesprekkenModel::instance());
 		if (!$this->isPosted()) {
 			$this->acl = array(
-				'view' => 'P_LOGGED_IN'
+				'gesprekken' => 'P_LOGGED_IN',
 			);
 		} else {
 			$this->acl = array(
@@ -31,22 +31,26 @@ class GesprekkenController extends AclController {
 	}
 
 	public function performAction(array $args = array()) {
-		$this->action = 'view';
+		$this->action = 'gesprekken';
 		if ($this->hasParam(2)) {
 			$this->action = $this->getParam(2);
 		}
 		parent::performAction($this->getParams(3));
 	}
 
-	public function view($gesprek_id = null) {
-		$body = new GesprekkenView($this->model, $gesprek_id);
-		$this->view = new CsrLayoutPage($body);
-		$this->view->addCompressedResources('gesprekken');
-	}
-
-	public function gesprekken($timestamp = null) {
-		$gesprekken = GesprekDeelnemersModel::instance()->getGesprekkenVoorLid(LoginModel::getUid(), (int) $timestamp);
-		$this->view = new DataTableResponse($gesprekken);
+	public function gesprekken($timestamp = null, $gesprek_id = null) {
+		// Data
+		if ($this->isPosted()) {
+			$gesprekken = GesprekDeelnemersModel::instance()->getGesprekkenVoorLid(LoginModel::getUid(), (int) $timestamp);
+			$this->view = new DataTableResponse($gesprekken);
+		}
+		// Table
+		else {
+			$body = new GesprekkenView($gesprek_id);
+			$this->view = new CsrLayoutPage($body);
+			$this->view->addCompressedResources('datatable');
+			$this->view->addCompressedResources('gesprekken');
+		}
 	}
 
 	public function lees($gesprek_id = null, $timestamp = null) {
@@ -69,7 +73,6 @@ class GesprekkenController extends AclController {
 				$this->geentoegang();
 			}
 			$deelnemer = $this->model->startGesprek($gesprek, LoginModel::getAccount(), $account);
-			GesprekBerichtenModel::instance()->maakBericht($gesprek, $deelnemer, $values['inhoud']);
 			$this->view = '/gesprekken/view/' . $gesprek->gesprek_id;
 		} else {
 			$this->view = $form;
@@ -84,24 +87,37 @@ class GesprekkenController extends AclController {
 		}
 		$form = new GesprekBerichtForm($gesprek);
 		if ($form->validate()) {
-			$bericht = GesprekBerichtenModel::instance()->maakBericht($gesprek, $deelnemer, $inhoud);
+			$values = $form->getValues();
+			$bericht = GesprekBerichtenModel::instance()->maakBericht($gesprek, $deelnemer, $values['inhoud']);
 			$this->view = new DataTableResponse($bericht);
 		} else {
 			$this->view = $form;
 		}
 	}
 
-	public function toevoegen($gesprek_id = null, $uid = null) {
-		$gesprek = $this->model->get($gesprek_id);
-		$account = AccountModel::get($uid);
-		if (!$gesprek OR ! $account) {
+	public function toevoegen() {
+		$selection = filter_input(INPUT_POST, 'DataTableSelection', FILTER_SANITIZE_STRING, FILTER_FORCE_ARRAY);
+		if (!isset($selection[0])) {
 			$this->geentoegang();
 		}
-		GesprekDeelnemersModel::instance()->voegToeAanGesprek($gesprek, $account);
+		$gesprek = $this->model->get($selection[0]);
+		$form = GesprekDeelenemerToevoegenForm();
+		if ($form->validate()) {
+			$values = $form->getValues();
+			$account = AccountModel::get($values['uid']);
+			if (!$gesprek OR ! $account) {
+				$this->geentoegang();
+			}
+			GesprekDeelnemersModel::instance()->voegToeAanGesprek($gesprek, $account);
+		}
 	}
 
-	public function sluiten($gesprek_id = null) {
-		$deelnemer = GesprekDeelnemersModel::get($gesprek_id, LoginModel::getUid());
+	public function sluiten() {
+		$selection = filter_input(INPUT_POST, 'DataTableSelection', FILTER_SANITIZE_STRING, FILTER_FORCE_ARRAY);
+		if (!isset($selection[0])) {
+			$this->geentoegang();
+		}
+		$deelnemer = GesprekDeelnemersModel::get($selection[0], LoginModel::getUid());
 		if (!$deelnemer) {
 			$this->geentoegang();
 		}
--- lib/view/GesprekkenView.class.php
+++ lib/view/GesprekkenView.class.php
@@ -1,23 +1,99 @@
 <?php
 
 /**
- * FotoAlbumView.class.php
+ * GesprekkenView.class.php
  * 
  * @author P.W.G. Brussee <brussee@live.nl>
  * 
  */
-class GesprekkenView extends DataTable {
-	//TODO
+class GesprekkenView implements View {
+
+	private $gesprekken;
+	private $deelnemers;
+	private $berichten;
+
+	public function __construct(Gesprek $gesprek = null) {
+		$this->gesprekken = new GesprekkenTable(GesprekkenModel::instance());
+		//TODO
+	}
+
+	public function getBreadcrumbs() {
+		return '<a href="/gesprekken" title="Gesprekken"><span class="fa fa-envelope-o module-icon"></span></a>'; //TODO: gesprek met ...
+	}
+
+	public function getModel() {
+		return null;
+	}
+
+	public function getTitel() {
+		return 'Gesprekken';
+	}
+
+	public function view() {
+		$this->gesprekken->view();
+	}
+
+}
+
+class GesprekkenTable extends DataTable {
+
+	public function __construct(GesprekkenModel $model, $gesprek_id = null) {
+		parent::__construct($model::orm);
+		$this->dataUrl = '/gesprekken/gesprekken/' . $gesprek_id;
+
+		$create = new DataTableKnop('== 0', $this->tableId, '/gesprekken/nieuw', 'post popup', 'Nieuw', 'Nieuw gesprek starten', 'add');
+		$this->addKnop($create);
+
+		$sluiten = new DataTableKnop('== 1', $this->tableId, '/gesprekken/sluiten', 'post confirm', 'Sluiten', 'Gesprek verlaten', 'delete');
+		$this->addKnop($sluiten);
+	}
+
 }
 
 class GesprekView extends DataTable {
-	//TODO
+
+	public function __construct(GesprekBerichtenModel $model, $gesprek_id = null) {
+		parent::__construct($model::orm);
+		$this->dataUrl = '/gesprekken/lees/' . $gesprek_id;
+	}
+
 }
 
 class GesprekBerichtForm extends DataTableForm {
-	//TODO
+
+	public function __construct(Gesprek $gesprek) {
+		parent::__construct(null, '/gesprekken/zeg/' . $gesprek->gesprek_id);
+
+		$fields[] = new RequiredTextareaField('inhoud', null, null);
+		$fields[] = new FormDefaultKnoppen(null, false);
+
+		$this->addFields($fields);
+	}
+
 }
 
-class GesprekForm extends GesprekBerichtForm {
-	//TODO
+class GesprekForm extends DataTableForm {
+
+	public function __construct(Gesprek $gesprek) {
+		parent::__construct(null, '/gesprekken/zeg/' . $gesprek->gesprek_id);
+
+		$fields[] = new RequiredTextareaField('inhoud', null, null);
+		$fields[] = new FormDefaultKnoppen(null, false);
+
+		$this->addFields($fields);
+	}
+
+}
+
+class GesprekDeelnemerToevoegenForm extends DataTableForm {
+
+	public function __construct(Gesprek $gesprek) {
+		parent::__construct(null, '/gesprekken/toevoegen/' . $gesprek->gesprek_id);
+
+		$fields[] = new RequiredLidField('to', null, 'Naam of lidnummer');
+		$fields[] = new FormDefaultKnoppen(null, false);
+
+		$this->addFields($fields);
+	}
+
 }
