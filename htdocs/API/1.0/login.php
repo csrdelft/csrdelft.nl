9fd5de2cf72edc7c6ab342547c3d89959eeeb6cc
Paul Brussee <paul.brussee@localhost>
2015-02-13 18:24:37 +0000

    tweak;

--- htdocs/API/1.0/login.php
+++ htdocs/API/1.0/login.php
@@ -4,7 +4,10 @@ require_once 'configuratie.include.php';
 
 $data = false;
 if (isset($_POST['user']) and isset($_POST['pass'])) {
-	$data = LoginModel::instance()->login(strval($_POST['user']), strval($_POST['pass']));
+
+	if (LoginModel::instance()->login(strval($_POST['user']), strval($_POST['pass']))) {
+		$data = true;
+	}
 }
 
 echo json_encode($data);
