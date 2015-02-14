0a884c8327046d437a5802678363862de9c5cc96
Paul Brussee <paul.brussee@localhost>
2015-02-14 12:26:10 +0000

    tweak;

--- lib/view/GesprekkenView.class.php
+++ lib/view/GesprekkenView.class.php
@@ -80,8 +80,8 @@ class GesprekkenTable extends DataTable {
 
 		$this->javascript .= <<<JS
 
-$('#{$this->dataTableId}').on('dblclick', 'tr', function (event) {
-	window.location.href = $(this).children('td:first').children('a:first').attr('href');
+$('#{$this->dataTableId}').on('click', 'td:first, td:nth-child(2)', function (event) {
+	window.location.href = $(this).parent().children('td:first').children('a:first').attr('href');
 });
 JS;
 	}
