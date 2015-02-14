51e606cf1fc816f1524f554be037df7727731a15
Paul Brussee <paul.brussee@localhost>
2015-02-14 23:09:03 +0000

    werkomheen;

--- htdocs/layout/js/csrdelft.js
+++ htdocs/layout/js/csrdelft.js
@@ -132,7 +132,7 @@ function init_geolocation() {
 	var prev_pos = false;
 
 	var position_save = function (position) {
-		if (!prev_pos || $(prev_pos.coords).not(position.coords).length === 0 && $(position.coords).not(prev_pos.coords).length === 0) {
+		if (!prev_pos || prev_pos.coords.latitude !== position.coords.latitude || prev_pos.coords.longitude !== position.coords.longitude) {
 			$.post('/geolocation/save', {
 				position: position
 			});
