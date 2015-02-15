7efa561b1942cf567d880efa5b6eca5fda96c28f
Paul Brussee <paul.brussee@localhost>
2015-02-15 01:02:53 +0000

    kleurtjes en autoupdate;

--- lib/controller/GeoLocationController.class.php
+++ lib/controller/GeoLocationController.class.php
@@ -55,9 +55,11 @@ class GeoLocationController extends AclController {
 			}
 			$profiel = ProfielModel::get($loc->uid);
 			echo '{' . "\n";
+			echo '"uid": "' . $loc->uid . '",' . "\n";
 			echo '"pasfoto": ' . json_encode($profiel->getPasfotoTag('pasfoto', true)) . ',' . "\n";
 			echo '"datetime": ' . json_encode(reldate($loc->moment)) . ',' . "\n";
-			echo '"position": ' . $loc->position . "\n";
+			echo '"position": ' . $loc->position . ',' . "\n";
+			echo '"timestamp": ' . strtotime($loc->moment) . "\n";
 			echo '}';
 		}
 		echo ']';
@@ -85,7 +87,11 @@ class GeoLocationController extends AclController {
 							mapTypeId: google.maps.MapTypeId.ROADMAP
 						});
 
+						var markers = {};
+						var infowindows = {};
+
 						var drawLocation = function (location) {
+
 
 							var geolocate;
 							// backwards compatibility
@@ -99,14 +105,32 @@ class GeoLocationController extends AclController {
 							var html = location.pasfoto + '<p style="text-align: right;">' + location.datetime + '</p>';
 							html += '<div style="max-width: 173px; word-wrap: break-word;">' + JSON.stringify(location.position) + '</div>';
 
-							var marker = new google.maps.Marker({
-								position: geolocate,
-								map: map
-							});
+							if (markers[location.uid]) {
+								google.maps.event.clearListeners(markers[location.uid], 'click');
+								delete infowindows[location.uid];
+
+								marker.setPosition(geolocate);
+							}
+							else {
+								var pinColor = Math.floor(Math.random() * 16777215).toString(16);
+								var pinImage = new google.maps.MarkerImage("//chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+										new google.maps.Size(21, 34),
+										new google.maps.Point(0, 0),
+										new google.maps.Point(10, 34)
+										);
+
+								var marker = new google.maps.Marker({
+									position: geolocate,
+									map: map,
+									icon: pinImage
+								});
+								marker[location.uid] = marker;
+							}
 
 							var infowindow = new google.maps.InfoWindow({
 								content: html
 							});
+							infowindows[location.uid] = infowindow;
 
 							google.maps.event.addListener(marker, 'click', function () {
 								infowindow.open(map, marker);
@@ -119,25 +143,34 @@ class GeoLocationController extends AclController {
 							return marker;
 						};
 
-						var getLocation = function () {
+						var getLocation = function (uid) {
 
-							$.post('/geolocation/get', <?= $data; ?>, function (data, textStatus, jqXHR) {
+							$.post('/geolocation/get', uid, function (data, textStatus, jqXHR) {
 
 								var last = false;
 
 								$.each(data, function (index) {
 									last = drawLocation(data[index]);
+
+									var autoUpdate = Math.round(new Date()) - data[index].timestamp;
+									if (autoUpdate < 86400000) { // binnen 24h
+
+										window.setTimeout(function () {
+											getLocation({
+												uid: data[index].uid
+											});
+										}, autoUpdate);
+									}
 								});
 
 								if (last) {
 									map.setCenter(last.getPosition());
 								}
-								//window.setTimeout(getLocation, Math.round(new Date()) - locations.timestamp); // auto update
 							});
 
 						};
 
-						getLocation();
+						getLocation(<?= $data; ?>);
 
 					})();
 
