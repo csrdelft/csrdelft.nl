8405154867a272adaca16dbef6bd0f57cc3400e4
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2015-07-04 12:36:48 +0200

    Removed agenda alert

--- htdocs/layout/js/gmaps.js
+++ htdocs/layout/js/gmaps.js
@@ -1,6 +1,6 @@
 
     //<![CDATA[
-	
+
 var geocoder;
 var gmarkers = [];
 var i = 0;
@@ -11,29 +11,30 @@ function loadGmaps(divId, address) {
 	    map.addControl(new GSmallMapControl());
 		var geocoder = new GClientGeocoder();
 	    geocoder.getLatLng(
-			address, 
+			address,
 			function(adr_latlng) {
 				if (!adr_latlng) {
-					alert(address + " niet gevonden");
+					// alert(address + " niet gevonden");
+					console.error(address + " niet gevonden");
 				} else {
 					map.setCenter(adr_latlng, 14);
 					var marker = createMarker(adr_latlng,address);
 			        map.addOverlay(marker);
-				}	    
+				}
 			}
-		);//close getLatLng	    
+		);//close getLatLng
 	}
 }
 function writeStaticGmap(div_id, address,width,height) {
-    var div = document.getElementById(div_id);        
+    var div = document.getElementById(div_id);
 	var geocoder = new GClientGeocoder();
 	geocoder.getLatLng(
-		address, 
+		address,
 		function(adr_latlng) {
 			if (!adr_latlng) {
 				div.innerHTML=address + " niet gevonden";
 			} else {
-				var coordinates = adr_latlng.lat()+','+adr_latlng.lng(); 
+				var coordinates = adr_latlng.lat()+','+adr_latlng.lng();
 				var imgtag = '<img src="https://maps.google.com/staticmap?\
 					size='+width+'x'+height+'&\
 					maptype=roadmap&\
@@ -42,9 +43,9 @@ function writeStaticGmap(div_id, address,width,height) {
 					sensor=false\
 					"></img>';
 				div.innerHTML='<a href="https://maps.google.nl/maps?q='+address+'">'+imgtag+'</a>';
-			}	    
+			}
 		}
-	);		
+	);
 }
 
 // This function picks up the click and opens the corresponding info window
@@ -56,11 +57,11 @@ function createMarker(point,html){
 	var marker = new GMarker(point);
 	GEvent.addListener(marker, "click", function() {
 		marker.openInfoWindowHtml(html);
-	}); 
+	});
 	// save the info we need to use later for the side_bar
 	gmarkers[i] = marker;
 	i++;
-	  
+
 	return marker;
 }
 
@@ -71,10 +72,11 @@ function showAddress(map,geocoder,address,html,label) {
     function(point) {
       if (!point) {
       //  alert(address + " niet gevonden");
+			  console.error(address + " niet gevonden");
       } else {
         var marker = createMarker(point,html+'<br/><br/>'+address,label);
         map.addOverlay(marker);
-		
+
       }
     }
   );
