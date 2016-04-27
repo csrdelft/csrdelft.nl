1cd518d94036e5a315faf5767848b973acc74510
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-04-27 17:21:17 +0200

    Don't send a Strict-Transport-Security header.
    Nginx already sends a Strict-Transport-Security header, there is no need
    for us to do this.

--- lib/configuratie.include.php
+++ lib/configuratie.include.php
@@ -100,9 +100,7 @@ define('HTTP_REFERER', $ref);
 
 // Use HTTP Strict Transport Security to force client to use secure connections only
 if (FORCE_HTTPS) {
-	if (isset($_SERVER['HTTP_X_FORWARDED_SCHEME']) && $_SERVER['HTTP_X_FORWARDED_SCHEME'] === 'https') {
-		header('Strict-Transport-Security: max-age=31536000');
-	} elseif (MODE !== 'CLI') {
+	if (MODE !== 'CLI') {
 		// check if the private token has been send over HTTP
 		$token = filter_input(INPUT_GET, 'private_token', FILTER_SANITIZE_STRING);
 		if (preg_match('/^[a-zA-Z0-9]{150}$/', $token)) {
