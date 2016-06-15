9f682029b0ba8aab68428f8ccd83f10726116cf8
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-06-15 22:26:41 +0200

    Verwijder BBCodeParser2, was ongebruikt.

--- lib/BBCodeParser2/HTML/BBCodeParser2/Filter.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'BBCodeParser2/HTML/BBCodeParser2.php';
-
-/**
- *   Dummy class that filters need to extend from.
- */
-class HTML_BBCodeParser2_Filter extends HTML_BBCodeParser2 {
-}
