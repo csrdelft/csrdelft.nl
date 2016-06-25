f06c1fba50fc391b72800861094f498ea1ed5f9d
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-06-25 08:25:21 +0200

    Verwijder dies stek uit repo

--- htdocs/dies/footer.php
+++
@@ -1,6 +0,0 @@
-  <ul>
-    <li><a href="./">Home</a></li>
-    <li><a href="thema">Themapresentatie</a></li>
-    <li><a href="programma">Programma</a></li>
-    <li><a href="etiquette">Etiquette</a></li>
-  </ul>
--- htdocs/dies/programma.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'header.php';
-?>
-<body id="thema">
-<img src="img/activiteitenposter.jpg" alt="Programma poster"/>
-<?php require 'footer.php'; ?>
-</body>
-</html>
