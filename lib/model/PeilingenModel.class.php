f72f77d54f521697e31902821862624f3fce1eb7
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-07-14 19:53:57 +0200

    Delete en validate in peilingenmodel

--- lib/model/PeilingenModel.class.php
+++ lib/model/PeilingenModel.class.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'model/entity/Peiling.class.php';
+require_once 'model/entity/PeilingOptie.class.php';
+require_once 'model/entity/PeilingStem.class.php';
 
 /**
  * PeilingenModel.class.php
@@ -24,6 +26,53 @@ class PeilingenModel extends PersistenceModel
 		return parent::update($entity);
 	}
 
+	public function delete(PersistentEntity $entity) {
+		foreach ($entity->getOpties() as $optie) {
+			PeilingOptiesModel::instance()->delete($optie);
+		}
+
+		$stemmen = PeilingStemmenModel::instance()->find('peilingid = ?', array($entity->id))->fetchAll();
+		foreach ($stemmen as $stem) {
+			echo PeilingStemmenModel::instance()->delete($stem);
+		}
+
+		return parent::delete($entity);
+	}
+	
+	public function create(PersistentEntity $entity) {
+		$peilingid = parent::create($entity);
+
+		foreach ($entity->getOpties() as $optie) {
+			$optie->peilingid = $peilingid;
+			PeilingOptiesModel::instance()->create($optie);
+		}
+
+		return $peilingid;
+	}
+
+	public function validate(Peiling $entity) {
+		$errors = '';
+		if ($entity == null) throw new Exception('Peiling is leeg');
+
+		if (trim($entity->tekst) == '') {
+			$errors .= 'Tekst mag niet leeg zijn.<br />';
+		}
+
+		if (trim($entity->titel) == '') {
+			$errors .= 'Titel mag niet leeg zijn.<br />';
+		}
+
+		if (count($entity->getOpties()) == 0) {
+			$errors .= 'Er moet tenminste 1 optie zijn.<br />';
+		}
+
+		return $errors;
+	}
+
+	/**
+	 * @param $id
+	 * @return Peiling
+	 */
 	public function get($id) {
 		return $this->retrieveByPrimaryKey(array($id));
 	}
