cf1211027384c0261f3898c4ba3a09d8fef11c34
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-09-04 20:53:38 +0200

    Update welkom.php

--- bin/welkom.php
+++ bin/welkom.php
@@ -7,14 +7,14 @@ require_once 'model/entity/Mail.class.php';
 
 # Scriptje om voor sjaars een wachtwoord te genereren en dat toe te mailen.
 # Vergeet niet voor gebruik hieronder het jaar aan te passen.
-$jaar = '15';
+$jaar = '16';
 
 foreach (ProfielModel::instance()->find('status = ?', array(LidStatus::Noviet)) as $profiel) {
     $nanonovieten = array("1519", "1536", "1545");
     if (in_array($profiel->uid, $nanonovieten)) {
         continue;
     }
-    $url = CSR_ROOT . '/wachtwoord/vergeten';
+    $url = CSR_ROOT . '/wachtwoord/aanvragen';
     $tekst = <<<TEXT
 Beste noviet {$profiel->voornaam},
 
@@ -27,16 +27,14 @@ Via de webstek kun je onder andere:
 
 Je hebt een abbonement op de donderdagmaaltijd in Confide. Dit om jullie te stimuleren deel te nemen aan C.S.R. activiteiten en te integreren met leden. Als je inlogt op de webstek zie je meteen op de hoofdpagina een blokje waarin je jezelf kunt af- of aanmelden voor de eerstvolgende maaltijd.
 Kun je niet op een maaltijd aanwezig zijn, meld je dan af op de webstek, voor omstreeks 15:00 op de dag van de maaltijd, want na dat tijdstip beginnen de koks met koken en wordt er op jou gerekend met boodschappen doen. Als je er na 15:00 achter komt dat je juist wel of juist niet aanwezig kunt zijn, bel dan even de koks of bel Confide, met een goede reden kan je dan eventueel doorgestreept of toegevoegd worden op de maaltijdlijst.
-Waarom is dit belangrijk? Omdat een maaltijd €3,- kost en als je jezelf vergeet af te melden en niet komt, dit bedrag toch van je maalcie-saldo wordt afgeschreven. Het is goed om naar maaltijden te gaan, maar als je niet kunt, meld je dan af! Dat scheelt je pieken.
+Waarom is dit belangrijk? Omdat een maaltijd €3,50 kost en als je jezelf vergeet af te melden en niet komt, dit bedrag toch van je maalcie-saldo wordt afgeschreven. Het is goed om naar maaltijden te gaan, maar als je niet kunt, meld je dan af! Dat scheelt je pieken.
 Voor verdere vragen of opmerkingen of je maalcie-saldo je kun de MaalCie fiscus altijd mailen op maalcief@csrdelft.nl
 
 Gebruik je lidnummer om in te loggen op de webstek: {$profiel->uid}
 Gebruik de eerste keer de [url={$url}]wachtwoord vergeten[/url] functie om je eigen wachtwoord in te stellen.
 Nadat je bent ingelogd kun een bijnaam instellen die je in plaats van je lidnummer kunt gebruiken om in te loggen.
 
-Als de sneeuw je computer traag maakt kan je deze onder instellingen vastzetten of uitzetten.
-
-Wanneer je problemen hebt met inloggen, of andere vragen over de webstek, kun je terecht bij de PubliciteitsCommissie.
+Wanneer je problemen hebt met inloggen, of andere vragen over de webstek, kun je terecht bij de PubCie.
 Stuur dan een e-mail.
 
 Met vriendelijke groet,
