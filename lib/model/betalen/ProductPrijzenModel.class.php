d546b38abe44901709ca8131dae99a48416747d6
Paul Brussee <brussee@live.nl>
2016-09-12 04:01:09 +0200

    add ProductPrijs entity

---
+++ lib/model/betalen/ProductPrijzenModel.class.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * ProductPrijzenModel.class.php
+ *
+ * @author P.W.G. Brussee <brussee@live.nl>
+ *
+ */
+class ProductPrijzen extends PersistenceModel {
+
+	const ORM = 'ProductPrijs';
+
+	protected static $instance;
+
+}
---
+++ lib/model/entity/betalen/ProductPrijs.class.php.php
@@ -0,0 +1,66 @@
+<?php
+
+/**
+ * ProductPrijs.class.php
+ *
+ * @author P.W.G. Brussee <brussee@live.nl>
+ *
+ */
+class ProductPrijs extends PersistentEntity {
+
+	/**
+	 * Primary key
+	 * @var int
+	 */
+	public $prijs_id;
+	/**
+	 * PrijsLijst ID
+	 * Foreign key
+	 * @var int
+	 */
+	public $lijst_id;
+	/**
+	 * Product ID
+	 * Foreign key
+	 * @var int
+	 */
+	public $product_id;
+	/**
+	 * Prijs in centen
+	 * @var int
+	 */
+	public $prijs;
+	/**
+	 * DateTime begin
+	 * @var string
+	 */
+	public $begin_moment;
+	/**
+	 * DateTime eind
+	 * @var string
+	 */
+	public $eind_moment;
+	/**
+	 * Database table columns
+	 * @var array
+	 */
+	protected static $persistent_attributes = array(
+		'prijs_id'		 => array(T::Integer, false, 'auto_increment'),
+		'lijst_id'		 => array(T::Integer),
+		'product_id'	 => array(T::Integer),
+		'prijs'			 => array(T::Integer),
+		'begin_moment'	 => array(T::DateTime, true),
+		'eind_moment'	 => array(T::DateTime, true)
+	);
+	/**
+	 * Database primary key
+	 * @var array
+	 */
+	protected static $primary_key = array('prijs_id');
+	/**
+	 * Database table name
+	 * @var string
+	 */
+	protected static $table_name = 'product_prijzen';
+
+}
