9e577e4ee4830ffcc87fa6eb10f4a45bfcd389e3
Paul Brussee <brussee@live.nl>
2016-09-19 17:24:28 +0200

    move agenda to subdir

---
+++ lib/model/entity/agenda/Agendeerbaar.interface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Agendeerbaar.interface.php
+ * 
+ * @author C.S.R. Delft <pubcie@csrdelft.nl>
+ * 
+ * Item dat in de agenda kan worden weergegeven.
+ * 
+ */
+interface Agendeerbaar {
+
+	public function getUUID();
+
+	/**
+	 * Timestamp van beginmoment.
+	 */
+	public function getBeginMoment();
+
+	/**
+	 * Timestamp van eindmoment.
+	 */
+	public function getEindMoment();
+
+	public function getTitel();
+
+	public function getBeschrijving();
+
+	public function getLocatie();
+
+	public function getLink();
+
+	public function isHeledag();
+}
