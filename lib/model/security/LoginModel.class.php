3d4e6e69675cba03cd88fa9095d08fa74c8ec083
Paul Brussee <brussee@live.nl>
2016-09-26 20:00:27 +0200

    handle remote address not set

--- lib/model/security/LoginModel.class.php
+++ lib/model/security/LoginModel.class.php
@@ -89,7 +89,7 @@ class LoginModel extends PersistenceModel implements Validator {
 
 			// Remember login
 			if (isset($_COOKIE['remember'])) {
-				$remember = RememberLoginModel::instance()->verifyToken($_SERVER['REMOTE_ADDR'], $_COOKIE['remember']);
+				$remember = RememberLoginModel::instance()->verifyToken($_COOKIE['remember']);
 				if ($remember) {
 					$this->login($remember->uid, null, false, $remember, $remember->lock_ip);
 				}
