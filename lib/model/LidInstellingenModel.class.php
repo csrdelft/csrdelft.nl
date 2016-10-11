e8148d1d9e6efce6616aa7425d5516ef8c58913d
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-10-11 18:44:14 +0200

    Maak lustrumthema kiesbaar

--- lib/model/LidInstellingenModel.class.php
+++ lib/model/LidInstellingenModel.class.php
@@ -40,7 +40,7 @@ class LidInstellingen extends Instellingen {
 		),
 		'layout'		 => array(
 			'toegankelijk'	 => array('Leesbaarheid', T::Enumeration, array('standaard', 'bredere letters'), 'standaard'),
-			'opmaak'		 => array('Opmaak', T::Enumeration, array('normaal', 'owee', 'dies', 'sineregno', 'roze'), 'normaal'),
+			'opmaak'		 => array('Opmaak', T::Enumeration, array('normaal', 'lustrum', 'owee', 'dies', 'sineregno', 'roze'), 'normaal'),
 			'fx'			 => array('Effect', T::Enumeration, array('nee', 'sneeuw', 'space', 'wolken'), 'nee'),
 			'visitekaartjes' => array('Civikaartjes', T::Enumeration, array('ja', 'nee'), 'ja'),
 			'neuzen'		 => array('Neuzen', T::Enumeration, array('2013', 'nee'), '2013'),
