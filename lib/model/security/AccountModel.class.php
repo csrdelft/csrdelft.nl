8611ec57cf2b55d1c077afc72aadbc69f68edb5a
Paul Brussee <brussee@live.nl>
2016-10-28 23:40:01 +0200

    bugfix: invalid value account aanmaken

--- lib/model/security/AccountModel.class.php
+++ lib/model/security/AccountModel.class.php
@@ -44,7 +44,7 @@ class AccountModel extends CachedPersistenceModel {
 		$account->username = $uid;
 		$account->email = $profiel->email;
 		$account->pass_hash = '';
-		$account->pass_since = '';
+		$account->pass_since = getDateTime();
 		$account->failed_login_attempts = 0;
 		$account->perm_role = AccessModel::instance()->getDefaultPermissionRole($profiel->status);
 		$this->create($account);
