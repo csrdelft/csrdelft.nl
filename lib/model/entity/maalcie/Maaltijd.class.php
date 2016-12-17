edd3095b4d7ab760f86cf5a2c11e78713a6ee363
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2016-12-17 01:25:57 +0100

    Fix maaltijden in app

--- lib/model/entity/maalcie/Maaltijd.class.php
+++ lib/model/entity/maalcie/Maaltijd.class.php
@@ -179,6 +179,8 @@ class Maaltijd extends PersistentEntity implements Agendeerbaar {
      */
     public function jsonSerialize() {
         $json = parent::jsonSerialize();
+        $json['aantal_aanmeldingen'] = $this->getAantalAanmeldingen();
+        $json['gesloten'] = $json['gesloten'] ? '1' : '0';
         $json['prijs'] = strval($json['prijs']);
         return $json;
     }
