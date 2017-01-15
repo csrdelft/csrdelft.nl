44fc99302635e415da227a03ec24112e479b2f13
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2017-01-15 21:52:11 +0100

    Doe het serializen van de datatableknoppen pas op het laatste moment.
    En maak het mogelijk voor buttons om buttons te hebben

--- lib/view/formulier/DataTable.class.php
+++ lib/view/formulier/DataTable.class.php
@@ -98,7 +98,7 @@ class DataTable implements View, FormElement {
 	}
 
 	protected function addKnop(DataTableKnop $knop) {
-		$this->settings['userButtons'][] = $knop->getSettings();
+		$this->settings['userButtons'][] = $knop;
 	}
 
 	protected function columnPosition($name) {
@@ -371,34 +371,46 @@ JS;
 	}
 }
 
-class DataTableKnop {
+class DataTableKnop implements JsonSerializable {
 
-	public $multiplicity;
+	protected $multiplicity;
 	protected $tableId;
 	protected $label;
 	protected $url;
 	protected $icon;
 	protected $id;
+	protected $extend;
+	protected $buttons;
+	protected $title;
 
-	public function __construct($multiplicity, $tableId, $url, $action, $label, $title, $icon = '') {
+	public function __construct($multiplicity, $tableId, $url, $action, $label, $title, $icon = '', $extend = 'default') {
 		$this->icon = $icon;
 		$this->label = $label;
+		$this->title = $title;
 		$this->url = $url;
 		$this->multiplicity = $multiplicity;
 		$this->tableId = $tableId;
+		$this->extend = $extend;
+		$this->buttons = array();
 	}
 
-	public function getSettings() {
+	public function addKnop(DataTableKnop $knop) {
+		$this->buttons[] = $knop;
+	}
+
+	public function jsonSerialize() {
 		return array(
 			'text' => $this->label,
+			'titleAttr' => $this->title,
 			'multiplicity' => $this->multiplicity,
-			'extend' => 'default',
+			'extend' => $this->extend,
 			'href' => $this->url,
 			'className' => $this->icon ? 'dt-button-ico dt-ico-' . Icon::get($this->icon) : '',
-			'dataTableId' => $this->tableId
+			'dataTableId' => $this->tableId,
+			'autoClose' => true,
+			'buttons' => $this->buttons
 		);
 	}
-
 }
 
 abstract class DataTableResponse extends JsonResponse {
