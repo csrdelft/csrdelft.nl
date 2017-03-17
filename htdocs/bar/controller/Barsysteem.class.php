54a3da8b7d909f2dfd1ba29555f557b3b7c5af8d
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2017-03-17 11:38:20 +0100

    Fix soccie systeem

--- htdocs/bar/controller/Barsysteem.class.php
+++ htdocs/bar/controller/Barsysteem.class.php
@@ -10,7 +10,7 @@ class Barsysteem
 
     function Barsysteem()
     {
-        $this->db = Database::instance();
+        $this->db = Database::instance()->getDatabase();
     }
 
     function isLoggedIn()
