9684e42f26722a8e3b8aa4b7fdfda08c508990fa
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2017-05-09 18:07:30 +0200

    [civisaldo] Fix link naar bestellingen van anderen

--- lib/view/ProfielView.class.php
+++ lib/view/ProfielView.class.php
@@ -127,7 +127,7 @@ class ProfielView extends SmartyTemplateView {
 
 		require_once 'model/fiscaat/CiviBestellingModel.class.php';
 		$this->smarty->assign('bestellinglog', CiviBestellingModel::instance()->getBestellingenVoorLid($this->model->uid));
-		$this->smarty->assign('bestellingenlink', '/fiscaat/bestellingen' . (LoginModel::getUid() === $this->model->uid ? '' : $this->model->uid));
+		$this->smarty->assign('bestellingenlink', '/fiscaat/bestellingen' . (LoginModel::getUid() === $this->model->uid ? '' : '/'.$this->model->uid));
 
 		$this->smarty->assign('corveepunten', $this->model->corvee_punten);
 		$this->smarty->assign('corveebonus', $this->model->corvee_punten_bonus);
