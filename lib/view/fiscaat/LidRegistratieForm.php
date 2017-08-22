b26d0116068f44cc162ffe812cdd5d5caae1f281
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2017-08-22 18:48:12 +0200

    #320 Maak het mogelijk om een CiviSaldo aan te maken zonder account. Als naam en uid leeg zijn wordt een foutmelding gegeven.

--- lib/view/fiscaat/LidRegistratieForm.php
+++ lib/view/fiscaat/LidRegistratieForm.php
@@ -10,6 +10,7 @@ namespace CsrDelft\view\fiscaat;
 use CsrDelft\model\entity\fiscaat\CiviSaldo;
 use CsrDelft\view\formulier\getalvelden\IntField;
 use CsrDelft\view\formulier\invoervelden\LidField;
+use CsrDelft\view\formulier\invoervelden\TextField;
 use CsrDelft\view\formulier\knoppen\FormDefaultKnoppen;
 use CsrDelft\view\formulier\ModalForm;
 
@@ -22,10 +23,28 @@ class LidRegistratieForm extends ModalForm {
 	public function __construct(CiviSaldo $model) {
 		parent::__construct($model, '/fiscaat/saldo/registreren/lid', false, true);
 
-		$fields[] = new LidField('uid', $model->uid, 'Lid');
+		$fields['naam'] = new TextField('naam', $model->naam, 'Bijnaam');
+		$fields['uid'] = new LidField('uid', $model->uid, 'Lid');
 		$fields[] = new IntField('saldo', $model->saldo, 'Initieel saldo');
 		$fields['btn'] = new FormDefaultKnoppen();
 
 		$this->addFields($fields);
+	}
+
+	public function validate() {
+		if (!parent::validate()) {
+			return false;
+		}
+
+		$fields = $this->getFields();
+
+		if (is_null($fields['naam']->getValue()) && is_null($fields['uid']->getValue())) {
+			$this->error = 'Vul in ieder geval een uid of een naam in.';
+			$this->css_classes[] = 'metFouten';
+
+			return false;
+		}
+
+		return true;
 	}
 }
