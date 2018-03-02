6083c45f6beed5b243da80d3646a4ee59d6fef52
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2018-03-02 17:42:19 +0100

    Update sponsor scraping

--- bin/sponsor_affiliates_download.php
+++ bin/sponsor_affiliates_download.php
@@ -6,15 +6,14 @@
  * @date 26/10/2017
  */
 
-use function CsrDelft\curl_follow_location;
 use function CsrDelft\curl_request;
-use function CsrDelft\init_xpath;
 
 /**
  * Settings constants.
  */
 const SETTINGS_CLUBID = 'clubid';
 const SETTINGS_SL_HOST = 'sl_host';
+const SETTINGS_UA = 'useragent';
 
 /**
  * Url constants.
@@ -26,48 +25,36 @@ require_once __DIR__ . '/../lib/configuratie.include.php';
 $settings = parse_ini_file(__DIR__ . '/../etc/sponsor_affiliates_download.ini');
 
 $SL_HOST = $settings[SETTINGS_SL_HOST];
-$PAGE_URL = $SL_HOST . '/products/shops.php?show=all&club=';
+$PAGE_URL = $SL_HOST . '/api/?call=webshops_club_extension&club=';
 
 $clubId = $settings[SETTINGS_CLUBID];
 $scrapeUrl = $PAGE_URL . $clubId;
 
-//1. GET affiliates pagina
-$result = curl_request($scrapeUrl);
-
-//2. Retrieve affiliate boxes
-$xpath = init_xpath($result);
-$affiliateBoxes = $xpath->query('//div[@class = "ibox-content product-box"]');
+//1. GET JSON
+$result = curl_request($scrapeUrl, [CURLOPT_USERAGENT => $settings[SETTINGS_UA]]);
+$webshops = json_decode($result)->webshops;
 
 //3. Follow links to final destination
 $data = ["club_id" => $clubId];
 $affiliates = [];
 $amount = 0;
-$shop_desc_url = $SL_HOST . "/products/shop.php?club=$clubId&id=";
-foreach ($affiliateBoxes as $affiliate) {
-	$element = $xpath->query('div/a[@class="product-name orderlink"]', $affiliate)->item(0);
-	$shopName = trim($element->textContent);
-    $href = $element->getAttribute('href');
-    $matches = [];
-    preg_match('/shop_id=([0-9]+)&/', $href, $matches);
-    $shopId = $matches[1];
-    $shopPrice = trim($xpath->query('div/span[@class="product-price"]', $affiliate)->item(0)->textContent);
+foreach ($webshops as $webshop) {
+    if ($webshop->extension == "0") {
+        continue;
+    }
 
-    // Get long shop price text
-    $shop_xpath = init_xpath(curl_request($shop_desc_url . $shopId));
-    $productDetail = $shop_xpath->query('//div[@class="ibox product-detail"]')->item(0);
-    $shopPriceLong = $shop_xpath->query('//h2', $productDetail)->item(2)->textContent;
-    $shopDesc = trim($shop_xpath->query('//h4', $productDetail)->item(0)->nextSibling->textContent);
-
-	$sponsorUrl = curl_follow_location($SL_HOST . $href);
-	$host = parse_url($sponsorUrl, PHP_URL_HOST);
+    preg_match('/shop_id=(\d+)/', $webshop->link, $shopId);
 	$entry = [
-	    "shop_name" => $shopName,
-        "shop_id" => $shopId,
-        "shop_price" => $shopPrice,
-        "shop_price_long" => $shopPriceLong,
-        "shop_description" => $shopDesc
+	    "shop_name" => $webshop->name_short,
+        "shop_name_long" => $webshop->name_long,
+        "shop_category" => $webshop->category,
+        "shop_id" => $shopId[1],
+        "shop_price" => $webshop->commission_gross,
+        "shop_description" => $webshop->description,
+        "shop_logo" => $webshop->logo_120x60
     ];
 
+	$host = $webshop->orig_url;
 	if (array_key_exists($host, $data['affiliates'])) {
 	    $affiliates[$host][] = $entry;
     } else {
@@ -76,17 +63,6 @@ foreach ($affiliateBoxes as $affiliate) {
 
 	$amount++;
 }
-
-// Delete failed ad.zanox.com links
-unset($affiliates['ad.zanox.com']);
-// Delete outdated daisycon affiliate network links
-unset($affiliates['www.manymorestores.com']);
-// Delete failed static.tradetracker.net links
-unset($affiliates['static.tradetracker.net']);
-// Delete failed/old www.paypro.nl links
-unset($affiliates['www.paypro.nl']);
-// Delete offers that don't leave sponsorkliks.com
-unset($affiliates['www.sponsorkliks.com']);
 
 // Store affiliates map in (soon to be JSON) output data
 $data["affiliates"] = $affiliates;
