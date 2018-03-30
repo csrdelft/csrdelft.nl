fcc0bc487fe304b63f6018d0de14a0eed6435e30
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-03-30 10:04:00 +0200

    Fix acties op GroepenBeheerTable

--- lib/view/groepen/GroepenBeheerTable.php
+++ lib/view/groepen/GroepenBeheerTable.php
@@ -44,33 +44,33 @@ class GroepenBeheerTable extends DataTable {
 		$this->searchColumn('status');
 		$this->searchColumn('soort');
 
-		$preview = new DataTableKnop(Multiplicity::One(), $this->dataUrl . 'voorbeeld', 'Voorbeeld', 'Voorbeeldweergave van de ketzer', 'show');
+		$preview = new DataTableKnop(Multiplicity::One(), $this->dataUrl . '/voorbeeld', 'Voorbeeld', 'Voorbeeldweergave van de ketzer', 'show');
 		$this->addKnop($preview);
 
-		$create = new DataTableKnop(Multiplicity::Zero(), $this->dataUrl . 'nieuw', 'Nieuw', 'Nieuwe toevoegen', 'toevoegen');
+		$create = new DataTableKnop(Multiplicity::Zero(), $this->dataUrl . '/nieuw', 'Nieuw', 'Nieuwe toevoegen', 'toevoegen');
 		$this->addKnop($create);
 
-		$next = new DataTableKnop(Multiplicity::One(), $this->dataUrl . 'aanmaken', 'Opvolger', 'Nieuwe toevoegen die de huidige opvolgt', 'toevoegen');
+		$next = new DataTableKnop(Multiplicity::One(), $this->dataUrl . '/aanmaken', 'Opvolger', 'Nieuwe toevoegen die de huidige opvolgt', 'toevoegen');
 		$this->addKnop($next);
 
-		$update = new DataTableKnop(Multiplicity::One(), $this->dataUrl . 'wijzigen', 'Wijzigen', 'Wijzig eigenschappen', 'bewerken');
+		$update = new DataTableKnop(Multiplicity::One(), $this->dataUrl . '/wijzigen', 'Wijzigen', 'Wijzig eigenschappen', 'bewerken');
 		$this->addKnop($update);
 
 		if (property_exists($model::ORM, 'aanmelden_vanaf')) {
-			$sluiten = new DataTableKnop(Multiplicity::Any(), $this->dataUrl . 'sluiten', 'Sluiten', 'Inschrijvingen nu sluiten', 'lock');
+			$sluiten = new DataTableKnop(Multiplicity::Any(), $this->dataUrl . '/sluiten', 'Sluiten', 'Inschrijvingen nu sluiten', 'lock');
 			$this->addKnop($sluiten);
 		}
 
-		$opvolg = new DataTableKnop(Multiplicity::Any(), $this->dataUrl . 'opvolging', 'Opvolging', 'Familienaam en groepstatus instellen', 'timeline_marker');
+		$opvolg = new DataTableKnop(Multiplicity::Any(), $this->dataUrl . '/opvolging', 'Opvolging', 'Familienaam en groepstatus instellen', 'timeline_marker');
 		$this->addKnop($opvolg);
 
-		$convert = new DataTableKnop(Multiplicity::Any(), $this->dataUrl . 'converteren', 'Converteren', 'Converteer naar ander soort groep', 'lightning');
+		$convert = new DataTableKnop(Multiplicity::Any(), $this->dataUrl . '/converteren', 'Converteren', 'Converteer naar ander soort groep', 'lightning');
 		$this->addKnop($convert);
 
-		$delete = new ConfirmDataTableKnop(Multiplicity::Any(), $this->dataUrl . 'verwijderen', 'Verwijderen', 'Definitief verwijderen (groep moet hier voor leeg zijn)', 'delete');
+		$delete = new ConfirmDataTableKnop(Multiplicity::Any(), $this->dataUrl . '/verwijderen', 'Verwijderen', 'Definitief verwijderen (groep moet hier voor leeg zijn)', 'delete');
 		$this->addKnop($delete);
 
-		$log = new DataTableKnop(Multiplicity::One(), $this->dataUrl . 'logboek', 'Logboek', 'Logboek bekijken', 'log');
+		$log = new DataTableKnop(Multiplicity::One(), $this->dataUrl . '/logboek', 'Logboek', 'Logboek bekijken', 'log');
 		$this->addKnop($log);
 	}
 
