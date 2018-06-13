d6baac57442863055e9d36be259ec985d83ae222
Sander Borst <s.borst@live.nl>
2018-06-14 00:06:48 +0200

    Fix rechtenview

---
+++ lib/view/RechtenData.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sander
+ * Date: 13-6-18
+ * Time: 23:34
+ */
+
+namespace CsrDelft\view;
+
+use CsrDelft\model\entity\security\AccessAction;
+use CsrDelft\model\entity\security\AccessControl;
+use CsrDelft\view\formulier\datatable\DataTableResponse;
+
+class RechtenData extends DataTableResponse {
+
+	/**
+	 * @param AccessControl $ac
+	 * @return string
+	 * @throws \Exception
+	 */
+	public function getJson($ac) {
+		$array = $ac->jsonSerialize();
+
+		$array['action'] = AccessAction::getDescription($ac->action);
+
+		if ($ac->resource === '*') {
+			$array['resource'] = 'Elke ' . lcfirst($ac->environment);
+		} else {
+			$array['resource'] = 'Deze ' . lcfirst($ac->environment);
+		}
+
+		return parent::getJson($array);
+	}
+
+}
---
+++ lib/view/RechtenForm.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sander
+ * Date: 13-6-18
+ * Time: 23:35
+ */
+
+namespace CsrDelft\view;
+
+use CsrDelft\model\entity\security\AccessAction;
+use CsrDelft\model\entity\security\AccessControl;
+use CsrDelft\model\security\LoginModel;
+use CsrDelft\view\formulier\elementen\HtmlComment;
+use CsrDelft\view\formulier\invoervelden\RequiredRechtenField;
+use CsrDelft\view\formulier\invoervelden\RequiredTextField;
+use CsrDelft\view\formulier\keuzevelden\SelectField;
+use CsrDelft\view\formulier\knoppen\FormDefaultKnoppen;
+use CsrDelft\view\formulier\ModalForm;
+
+class RechtenForm extends ModalForm {
+
+	/**
+	 * RechtenForm constructor.
+	 * @param AccessControl $ac
+	 * @param $action
+	 * @throws \Exception
+	 */
+	public function __construct(AccessControl $ac, $action) {
+		parent::__construct($ac, '/rechten/' . $action . '/' . $ac->environment . '/' . $ac->resource, 'Rechten aanpassen voor ', true);
+		if ($ac->resource === '*') {
+			$this->titel .= 'elke ' . $ac->environment;
+		} else {
+			$this->titel .= $ac->environment . ' ' . $ac->resource;
+		}
+
+		if ($action === 'aanmaken') {
+
+			if (LoginModel::mag('P_ADMIN')) {
+				$fields[] = new RequiredTextField('environment', $ac->environment, 'Klasse');
+				$fields[] = new RequiredTextField('resource', $ac->resource, 'Object');
+			}
+
+			$acties = array();
+			foreach (AccessAction::getTypeOptions() as $option) {
+				$acties[$option] = AccessAction::getDescription($option);
+			}
+			$fields[] = new SelectField('action', $ac->action, 'Actie', $acties);
+		} else {
+			$fields[] = new HtmlComment('<label>Actie</label><div class="dikgedrukt">' . AccessAction::getDescription($ac->action) . '</div>');
+		}
+		$fields[] = new RequiredRechtenField('subject', $ac->subject, 'Toegestaan voor');
+		$fields[] = new FormDefaultKnoppen();
+
+		$this->addFields($fields);
+	}
+
+}
