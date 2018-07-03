ba3560751cdeffbbe8a0e311dd093da679acaea8
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-07-03 10:15:43 +0200

    Fix pasfoto in Gesprekken

--- lib/model/entity/gesprekken/GesprekBericht.class.php
+++ lib/model/entity/gesprekken/GesprekBericht.class.php
@@ -63,7 +63,7 @@ class GesprekBericht extends PersistentEntity {
 
 	public function getAuteurFormatted() {
 		$profiel = ProfielModel::get($this->auteur_uid);
-		return '<img class="cd-user-avatar float-left" src="/plaetjes/' . $profiel->getPasfotoPath(true) . '"/><div class="dikgedrukt">' . $profiel->getLink('volledig') . '</div>';
+		return '<img class="cd-user-avatar float-left" src="/plaetjes/pasfoto/' . $profiel->getPasfotoPath(true) . '"/><div class="dikgedrukt">' . $profiel->getLink('volledig') . '</div>';
 	}
 
 	public function getFormatted($previous, $max = false) {
