b2d17d92095c9030436f20724784f3ed506bacd4
Sander Borst <s.borst@live.nl>
2018-08-19 15:45:17 +0200

    Make phpstan work again after merge

--- phpstan.autoload.php
+++ phpstan.autoload.php
@@ -2,6 +2,8 @@
 define('HTTP_REFERER', '');
 define('REQUEST_URI', '');
 
+include 'defines.include.php';
+include 'common.functions.php';
 include 'autoload.php';
 
 foreach (glob("lib/smarty_plugins/*.php") as $filename)
