1e965a4880716fb9eaf8941d4cca676447bc4b1d
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-08-26 21:24:34 +0200

    Fix post-install

--- bin/dev/post-install.php
+++ bin/dev/post-install.php
@@ -1,5 +1,10 @@
 <?php
 
+// defines.include.php moet bestaan anders wil er niets werken.
+if (getenv('CI')) {
+	copy(__DIR__ . '/../../lib/defines.include.php.sample', __DIR__ . '/../../lib/defines.include.php');
+}
+
 require_once 'generator.enum.php';
 
 try {
