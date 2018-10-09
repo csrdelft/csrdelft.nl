9fae5321d6051822cf9657eb3d6e45d4d5409330
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-10-09 09:41:37 +0200

    Fix breedte van sponsorkliks zijbalk ding op mobiel

--- lib/view/IsHetAlView.class.php
+++ lib/view/IsHetAlView.class.php
@@ -126,7 +126,7 @@ class IsHetAlView implements View {
 		echo '<div class="ishetal">';
 		switch ($this->model) {
 			case 'sponsorkliks':
-				echo '<iframe src="https://bannerbuilder.sponsorkliks.com/skinfo.php?&background-color=F5F5F5&text-color=000000&header-background-color=F5F5F5&header-text-color=F5F5F5&odd-row=FFFFFF&even-row=09494a&odd-row-text=09494a&even-row-text=ffffff&type=financial&club_id=3605&width=193&height=80" frameborder="0" referrerpolicy="no-referrer" width="193" height="80" style="margin-left:-10px;"></iframe>';
+				echo '<iframe src="https://bannerbuilder.sponsorkliks.com/skinfo.php?&background-color=F5F5F5&text-color=000000&header-background-color=F5F5F5&header-text-color=F5F5F5&odd-row=FFFFFF&even-row=09494a&odd-row-text=09494a&even-row-text=ffffff&type=financial&club_id=3605&width=193&height=80" frameborder="0" referrerpolicy="no-referrer" class="sponsorkliks-zijbalk"></iframe>';
 				break;
 
 			case 'jarig':
