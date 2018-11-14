f54645bd9419f3bb67073c0133f9d32ec5784fb1
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-11-14 19:58:16 +0100

    Maak CsrException en CsrGebruikerException een RuntimeException
    Ze mogen gebeuren en moeten niet direct opgevangen worden. Dit is oa om
    te voorkomen dat linters gaan miepen over missende @throws, dit is niet
    nodig voor RuntimeExceptions.

--- lib/common/CsrException.php
+++ lib/common/CsrException.php
@@ -6,6 +6,6 @@ namespace CsrDelft\common;
  * @author Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
  * @since 20170824
  */
-class CsrException extends \Exception {
+class CsrException extends \RuntimeException {
 
 }
