57cb65c0b95b0b74d82e052652ce8be38594030c
Sander Borst <s.borst@live.nl>
2018-11-19 21:26:03 +0100

    Improve boek_fields migration

--- db/migrations/20180719130843_boek_fields.php
+++ db/migrations/20180719130843_boek_fields.php
@@ -16,6 +16,13 @@ class BoekFields extends AbstractMigration
 			ALTER TABLE biebboek CHANGE uitgavejaar uitgavejaar int(11) NOT NULL;
 			 ALTER TABLE biebboek CHANGE uitgeverij uitgeverij varchar(255) NOT NULL;
 			 ALTER TABLE biebboek CHANGE code code varchar(255) NOT NULL; 
+			ALTER TABLE biebcategorie CHANGE id id int(11) NOT NULL auto_increment;
+			ALTER TABLE biebcategorie CHANGE categorie categorie varchar(255) NOT NULL;
+			ALTER TABLE biebbeschrijving CHANGE schrijver_uid schrijver_uid varchar(255) NOT NULL;
+			ALTER TABLE biebexemplaar CHANGE eigenaar_uid eigenaar_uid varchar(255) NOT NULL;
+			ALTER TABLE biebexemplaar CHANGE opmerking opmerking text NOT NULL;
+			ALTER TABLE biebexemplaar CHANGE uitgeleend_uid uitgeleend_uid varchar(255) NULL DEFAULT NULL;
+			ALTER TABLE biebexemplaar CHANGE uitleendatum uitleendatum datetime NULL DEFAULT NULL;
 SQL
 );
     }
