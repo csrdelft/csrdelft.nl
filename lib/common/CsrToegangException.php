6b1896b3e948a4c81f0d27f186764c5fbcbc84b4
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-12-18 15:54:03 +0100

    Maak de standaard CsrToegangException 'Mag niet'

--- lib/common/CsrToegangException.php
+++ lib/common/CsrToegangException.php
@@ -9,7 +9,7 @@ use Throwable;
  * @since 25/08/2018
  */
 class CsrToegangException extends CsrException {
-	public function __construct(string $message = "", int $code = 0, Throwable $previous = null) {
+	public function __construct(string $message = "", int $code = 403, Throwable $previous = null) {
 		parent::__construct($message, $code, $previous);
 	}
 
