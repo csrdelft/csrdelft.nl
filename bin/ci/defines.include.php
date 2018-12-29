cdcdcb55721e1d51411fa529ad5a13009f9a6f41
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-12-29 12:36:18 +0100

    Fix travis build blade mode í´

---
+++ bin/ci/defines.include.php
@@ -0,0 +1,10 @@
+<?php
+
+# -------------------------------------------------------------------
+# defines.include.php
+# -------------------------------------------------------------------
+# Standaard instellingen voor travis, zie ook defines.defaults.php
+# -------------------------------------------------------------------
+define('CSR_DOMAIN', 'localhost');
+define('CSR_ROOT', 'http://' . CSR_DOMAIN);
+define('BLADEONE_MODE', 2); // FAST
