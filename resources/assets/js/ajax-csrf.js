cb5716e63fc4623d2be661c5e3a3bd497d58d403
Sander Borst <s.borst@live.nl>
2018-12-29 21:56:56 +0100

    Style fixes

--- resources/assets/js/ajax-csrf.js
+++ resources/assets/js/ajax-csrf.js
@@ -1,6 +1,8 @@
-window.$.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
+import $ from 'jquery';
+
+$.ajaxPrefilter(function( options, originalOptions, jqXHR ) {
     if (!options.crossDomain) {
-        jqXHR.setRequestHeader('X-CSRF-ID', $("meta[property='X-CSRF-ID']").attr("content"));
-        jqXHR.setRequestHeader('X-CSRF-VALUE', $("meta[property='X-CSRF-VALUE']").attr("content"));
+        jqXHR.setRequestHeader('X-CSRF-ID', $('meta[property=\'X-CSRF-ID\']').attr('content'));
+        jqXHR.setRequestHeader('X-CSRF-VALUE', $('meta[property=\'X-CSRF-VALUE\']').attr('content'));
     }
 });
