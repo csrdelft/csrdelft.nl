c9c14042829b0b88acadb8f146b8c9c03d67758f
Tim Nederveen <jptned@hotmail.nl>
2018-12-30 14:46:42 +0100

    Functie om gemiddelde beoordeling en gemiddelde afwijking te bepalen

--- lib/model/maalcie/MaaltijdBeoordelingenModel.class.php
+++ lib/model/maalcie/MaaltijdBeoordelingenModel.class.php
@@ -28,16 +28,46 @@ class MaaltijdBeoordelingenModel extends PersistenceModel {
 		return $b;
 	}
 
-	public function getNormalizedBeoordelingen(Maaltijd $maaltijd) {
-		$beoordelingen = $this->find('maaltijd_id = ?', array($maaltijd->maaltijd_id));
-		foreach ($beoordelingen as $b) {
-			$normalize = Database::instance()->sqlSelect(array('AVG(kwantiteit)', 'AVG(kwaliteit)'), $this->getTableName(), 'uid = ?', array($b->uid));
-			foreach ($normalize as $avg) {
-			    if (!is_null($b->kwantiteit)) $b->kwantiteit /= $avg[0];
-				if (!is_null($b->kwaliteit)) $b->kwaliteit /= $avg[1];
-			}
-		}
-		return $beoordelingen;
-	}
+	public function getBeoordelingSamenvatting(Maaltijd $maaltijd) {
+	    // Haal beoordelingen voor deze maaltijd op
+        $beoordelingen = $this->find('maaltijd_id = ?', array($maaltijd->maaltijd_id));
+
+        // Bepaal gemiddelde en gemiddelde afwijking
+        $kwantiteit = 0;
+        $kwantiteit_afwijking = 0;
+        $kwantiteit_aantal = 0;
+        $kwaliteit_afwijking = 0;
+        $kwaliteit = 0;
+        $kwaliteit_aantal = 0;
+        foreach ($beoordelingen as $b) {
+            // Haal gemiddelde beoordeling van lid op
+            $userAverage = Database::instance()->sqlSelect(array('AVG(kwantiteit)', 'AVG(kwaliteit)'), $this->getTableName(), 'uid = ?', array($b->uid));
+            $userAverage->execute();
+            $avg = $userAverage->fetchAll();
+
+            // Alleen als waarde is ingevuld
+            if (!is_null($b->kwantiteit)) {
+                $kwantiteit += $b->kwantiteit;
+                // Bepaal afwijking en tel op
+                $kwantiteit_afwijking += $b->kwantiteit - $avg[0][0];
+                $kwantiteit_aantal++;
+            }
+            if (!is_null($b->kwaliteit)) {
+                $kwaliteit += $b->kwaliteit;
+                // Bepaal afwijking en tel op
+                $kwaliteit_afwijking += $b->kwaliteit - $avg[0][1];
+                $kwaliteit_aantal++;
+            }
+        }
+
+        // Geef resultaat terug in object, null als er geen beoordelingen zijn
+        $object = new \stdClass();
+        $object->kwantiteit = $kwantiteit_aantal === 0 ? null : $kwantiteit / $kwantiteit_aantal;
+        $object->kwantiteit_afwijking = $kwantiteit_aantal === 0 ? null : $kwantiteit_afwijking / $kwantiteit_aantal;
+        $object->kwaliteit = $kwaliteit_aantal === 0 ? null : $kwaliteit / $kwaliteit_aantal;
+        $object->kwaliteit_afwijking = $kwaliteit_aantal === 0 ? null : $kwaliteit_afwijking / $kwaliteit_aantal;
+
+        return $object;
+    }
 
 }
