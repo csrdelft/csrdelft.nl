9c456248a0b34ada202ca18ab466e7313505e019
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2018-12-31 20:05:46 +0100

    Doe geen redirect voor onderhoudsmodus

--- lib/configuratie.include.php
+++ lib/configuratie.include.php
@@ -10,9 +10,22 @@
  * Configure sessions.
  * Boot framework.
  */
+define('ONDERHOUD', true);
 require_once 'defines.defaults.php';
 if (ONDERHOUD) {
-	header('location: https://csrdelft.nl/onderhoud.html');
+	http_response_code(503);
+	echo <<<'HTML'
+<!doctype html>
+<html lang=nl>
+<title>C.S.R. Delft - Onderhoud</title>
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width, initial-scale=1.0">
+
+<body style="font-family: sans-serif; text-align: center;">
+<h1>Onderhoud</h1>
+<p>De website is momenteel in onderhoud. Dit duurt meestal niet lang.</p>
+<img alt="Beeldmerk van de Vereniging" src="/dist/images/beeldmerk.png">
+HTML;
 	exit;
 }
 
