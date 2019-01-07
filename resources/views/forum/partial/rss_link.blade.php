c755a9ed3421b4f5785b4b8bc7317aef866f79c5
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-01-07 11:00:38 +0100

    fix rss link styles

--- resources/views/forum/partial/rss_link.blade.php
+++ resources/views/forum/partial/rss_link.blade.php
@@ -1,10 +1,13 @@
-<div class="clear" title="Houd deze url privé!&#013;Nieuwe aanvragen: zie je profiel">
-	<a name="RSS"></a>
-	@icon('feed')
+<div class="rss-link input-group mt-4" title="Houd deze url privé!&#013;Nieuwe aanvragen: zie je profiel">
+	<div class="input-group-prepend">
+		<span class="input-group-text">@icon('feed')</span>
+	</div>
 	@if(CsrDelft\model\security\LoginModel::getUid() == 'x999' || CsrDelft\model\security\LoginModel::getAccount()->hasPrivateToken())
-		<input type="text" value="{{CsrDelft\model\security\LoginModel::getAccount()->getRssLink()}}" size="35"
-					 onclick="this.setSelectionRange(0, this.value.length);" readonly/>
+		<input type="text" class="form-control" value="{{CsrDelft\model\security\LoginModel::getAccount()->getRssLink()}}"
+					 size="35" onclick="this.setSelectionRange(0, this.value.length);" readonly title="RSS Link"/>
 	@else
-		<a href="/profiel/{{CsrDelft\model\security\LoginModel::getUid()}}#tokenaanvragen">Privé url aanvragen</a>
+		<span class="input-group-text">
+			<a href="/profiel/{{CsrDelft\model\security\LoginModel::getUid()}}#tokenaanvragen">Privé url aanvragen</a>
+		</span>
 	@endif
 </div>
