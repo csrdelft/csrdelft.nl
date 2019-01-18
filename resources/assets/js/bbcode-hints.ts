83b5bc2172f443fcb7fa00a7f336cefe39de7f8b
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-01-18 15:43:25 +0100

    Fixup bbcode-hints.ts

--- resources/assets/js/bbcode-hints.ts
+++ resources/assets/js/bbcode-hints.ts
@@ -1,49 +1,49 @@
-let { Textcomplete, Textarea } = require('textcomplete');
+const {Textcomplete, Textarea} = require('textcomplete');
 
 export function activeerLidHints(textarea: HTMLElement) {
-    let editor = new Textarea(textarea);
-    let textcomplete = new Textcomplete(editor);
+	const editor = new Textarea(textarea);
+	const textcomplete = new Textcomplete(editor);
 
-    textcomplete.register([{
-        // @...
-        search: search,
-        template: template,
-        match: /(^|\s|])@((?:[^ ]+ ?){1,5})$/,
-        replace: function (data: any) {
-            return '$1[lid=' + data.label + ']';
-        }
-    }, {
-        // [citaat=... of [lid=...
-        search: search,
-        template: template,
-        match: /(^|\s|])\[(citaat|lid)=(?:[0-9]{4}|([^\]]+))$/,
-        index: 3,
-        replace: function (data: any) {
-            return '$1[$2=' + data.label;
-        }
-    }]);
-    textcomplete.on('rendered', function () {
-        if (textcomplete.dropdown.items.length >= 1) {
-            // Activeer eerste keuze standaard
-            textcomplete.dropdown.items[0].activate();
-        }
-    });
+	textcomplete.register([{
+		// @...
+		match: /(^|\s|])@((?:[^ ]+ ?){1,5})$/,
+		replace (data: any) {
+			return '$1[lid=' + data.label + ']';
+		},
+		search,
+		template,
+	}, {
+		// [citaat=... of [lid=...
+		index: 3,
+		match: /(^|\s|])\[(citaat|lid)=(?:[0-9]{4}|([^\]]+))$/,
+		search,
+		template,
+		replace (data: any) {
+			return '$1[$2=' + data.label;
+		},
+	}]);
+	textcomplete.on('rendered', () => {
+		if (textcomplete.dropdown.items.length >= 1) {
+			// Activeer eerste keuze standaard
+			textcomplete.dropdown.items[0].activate();
+		}
+	});
 }
 
-function search(term: string, callback: Function) {
-    if (!term || term.length === 1) {
-        callback([]);
-    } else {
-        $.ajax('/tools/naamsuggesties.php?vorm=user&zoekin=voorkeur&q=' + encodeURI(term))
-            .done(function (data) {
-                callback(data);
-            })
-            .fail(function() {
-                callback([]);
-            });
-    }
+function search(term: string, callback: (data: any) => void) {
+	if (!term || term.length === 1) {
+		callback([]);
+	} else {
+		$.ajax('/tools/naamsuggesties.php?vorm=user&zoekin=voorkeur&q=' + encodeURI(term))
+			.done((data) => {
+				callback(data);
+			})
+			.fail(() => {
+				callback([]);
+			});
+	}
 }
 
 function template(data: any, term: string) {
-    return data.value;
+	return data.value;
 }
