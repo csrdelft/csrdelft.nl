647e94b168681733cd46e721d0e1fe030a5b48cc
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-01-18 13:07:29 +0100

    meer ts

--- resources/assets/js/bbcode.ts
+++ resources/assets/js/bbcode.ts
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import initContext from './context';
+import {singleLineString} from './util';
 
 /**
  * @see templates/courant/courantbeheer.tpl
@@ -15,13 +16,13 @@ export const CsrBBPreview = (sourceId: string, targetId: string) => {
 	if (targetId.charAt(0) !== '#') {
 		targetId = `#${targetId}`;
 	}
-	let bbcode = $(sourceId).val();
+	const bbcode = $(sourceId).val();
 	if (typeof bbcode !== 'string' || bbcode.trim() === '') {
 		$(targetId).html('').hide();
 		return;
 	}
 	$.post('/tools/bbcode.php', {
-		data: encodeURIComponent(bbcode)
+		data: encodeURIComponent(bbcode),
 	}).done((data) => {
 		$(targetId).html(data);
 		initContext($(targetId));
@@ -34,37 +35,45 @@ export const CsrBBPreview = (sourceId: string, targetId: string) => {
 /**
  * @see view/bbcode/CsrBB.class.php
  */
-export const bbvideoDisplay = function (elem: string) {
-	let $previewdiv = $(elem);
-	let params = $previewdiv.data('params');
+export const bbvideoDisplay = (elem: string) => {
+	const $previewdiv = $(elem);
+	const params = $previewdiv.data('params');
 	if (params.iframe) {
-		//vervang de thumb door een iframe
-		$previewdiv.parent().html(
-			'<iframe frameborder="0" width="' + params.width + '" height="' + params.height + '" src="' + params.src + '" allowfullscreen></iframe>'
-		);
+		// vervang de thumb door een iframe
+		$previewdiv
+			.parent()
+			.html(singleLineString`
+<iframe
+	frameborder="0"
+	width="${params.width}"
+	height="${params.height}"
+	src="${params.src}"
+	allowfullscreen></iframe>`);
 	} else {
-		//godtube is nog flash (of alternatief iets met javascript, maar geen iframe versie beschikbaar)
-		$previewdiv.parent().html(
-			'<object type="application/x-shockwave-flash" data="http://www.godtube.com/resource/mediaplayer/5.3/player.swf"  width="' + params.width + '" height="' + params.height + '">' +
-			'<param name="allowfullscreen" value="true" />' +
-			'<param name="allowscriptaccess" value="always" />' +
-			'<param name="wmode" value="opaque" />' +
-			'<param name="movie" value="http://www.godtube.com/resource/mediaplayer/5.3/player.swf" />' +
-			'<param name="autostart" value="true" />' +
-			'<param name="flashvars" value="file=http://www.godtube.com/resource/mediaplayer/' + params.id + '.file' +
-			'&image=http://www.godtube.com/resource/mediaplayer/' + params.id + '.jpg' +
-			'&screencolor=000000' +
-			'&type=video' +
-			'&autostart=true' +
-			'&playonce=true' +
-			'&skin=http://www.godtube.com//resource/mediaplayer/skin/carbon/carbon.zip' +
-			'&logo.file=http://media.salemwebnetwork.com/godtube/theme/default/media/embed-logo.png' +
-			'&logo.link=http://www.godtube.com/watch/?v=' + params.id +
-			'&logo.position=top-left' +
-			'&logo.hide=false' +
-			'&controlbar.position=over">' +
-			'</object>'
-		);
+		// godtube is nog flash (of alternatief iets met javascript, maar geen iframe versie beschikbaar)
+		$previewdiv.parent().html(singleLineString`
+<object
+	type="application/x-shockwave-flash"
+	data="http://www.godtube.com/resource/mediaplayer/5.3/player.swf"
+	width="${params.width}"
+	height="${params.height}">
+		<param name="allowfullscreen" value="true" />
+		<param name="allowscriptaccess" value="always" />
+		<param name="wmode" value="opaque" />
+		<param name="movie" value="http://www.godtube.com/resource/mediaplayer/5.3/player.swf" />
+		<param name="autostart" value="true" />
+		<param name="flashvars" value="file=http://www.godtube.com/resource/mediaplayer/${params.id}.file
+						&image=http://www.godtube.com/resource/mediaplayer/${params.id}.jpg
+						&screencolor=000000
+						&type=video
+						&autostart=true
+						&playonce=true
+						&skin=http://www.godtube.com//resource/mediaplayer/skin/carbon/carbon.zip
+						&logo.file=http://media.salemwebnetwork.com/godtube/theme/default/media/embed-logo.png
+						&logo.link=http://www.godtube.com/watch/?v=${params.id}
+						&logo.position=top-left
+						&logo.hide=false
+						&controlbar.position=over"></object>`);
 	}
 
 	return false;
