5dc5c8edd03ac56dc84235f2e2794bf510533acb
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-03-23 12:23:58 +0100

    Fix domupdate voor instellingen

--- resources/assets/js/util.ts
+++ resources/assets/js/util.ts
@@ -155,7 +155,7 @@ export function html(strings: TemplateStringsArray, ...values: string[]): HTMLEl
 }
 
 export function htmlParse(htmlString: string) {
-	return jQuery.parseHTML(htmlString, null, true);
+	return jQuery.parseHTML(htmlString, null, true) as Node[];
 }
 
 export function preloadImage(url: string, callback: () => void) {
