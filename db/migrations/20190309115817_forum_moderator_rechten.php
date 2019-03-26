47df7dd159064b1d48cbc0765af9bb6f193f9e49
Mark Bekooy <mark.bekooy@gmail.com>
2019-03-26 23:08:49 +0100

    Hernoem FiscaatRechten naar ForumModeratorRechten

--- db/migrations/20190309115817_forum_moderator_rechten.php
+++ db/migrations/20190309115817_forum_moderator_rechten.php
@@ -2,7 +2,7 @@
 
 use Phinx\Migration\AbstractMigration;
 
-class FiscaatRechten extends AbstractMigration {
+class ForumModeratorRechten extends AbstractMigration {
 	public function up() {
 		$this->query("ALTER TABLE accounts CHANGE perm_role perm_role ENUM('R_NOBODY','R_ETER','R_OUDLID','R_LID','R_BASF','R_MAALCIE','R_BESTUUR','R_PUBCIE','R_FISCAAT','R_VLIEGER', 'R_FORUM_MOD') NOT NULL;");
 	}
