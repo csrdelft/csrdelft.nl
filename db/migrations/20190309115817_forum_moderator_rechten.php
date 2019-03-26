83f01db25285e326622bff452b034cf60afbfbf1
Mark Bekooy <mark.bekooy@gmail.com>
2019-03-26 12:09:13 +0100

    Voeg nog R_FISCAAT toe in down functie

--- db/migrations/20190309115817_forum_moderator_rechten.php
+++ db/migrations/20190309115817_forum_moderator_rechten.php
@@ -8,6 +8,6 @@ class FiscaatRechten extends AbstractMigration {
 	}
 
 	public function down() {
-		$this->query("ALTER TABLE accounts CHANGE perm_role perm_role ENUM('R_NOBODY','R_ETER','R_OUDLID','R_LID','R_BASF','R_MAALCIE','R_BESTUUR','R_PUBCIE','R_VLIEGER') NOT NULL;");
+		$this->query("ALTER TABLE accounts CHANGE perm_role perm_role ENUM('R_NOBODY','R_ETER','R_OUDLID','R_LID','R_BASF','R_MAALCIE','R_BESTUUR','R_PUBCIE','R_FISCAAT','R_VLIEGER') NOT NULL;");
 	}
 }
