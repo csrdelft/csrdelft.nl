95061316ce1e91bb148b47bfce71bfc693ca9296
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-04-19 19:24:16 +0200

    Sta toe dat leden hun eigen bestellinglog kunnen zien
    Fixes #539

--- lib/controller/fiscaat/BeheerCiviBestellingController.class.php
+++ lib/controller/fiscaat/BeheerCiviBestellingController.class.php
@@ -2,6 +2,7 @@
 
 namespace CsrDelft\controller\fiscaat;
 
+use CsrDelft\common\CsrToegangException;
 use CsrDelft\controller\framework\QueryParamTrait;
 use CsrDelft\model\fiscaat\CiviBestellingInhoudModel;
 use CsrDelft\model\fiscaat\CiviBestellingModel;
@@ -23,6 +24,8 @@ class BeheerCiviBestellingController {
 	}
 
 	public function overzicht($uid = null) {
+		$this->checkToegang($uid);
+
 		return view('fiscaat.pagina', [
 			'titel' => 'Beheer bestellingen',
 			'view' => new CiviBestellingTable($uid)
@@ -30,6 +33,7 @@ class BeheerCiviBestellingController {
 	}
 
 	public function lijst($uid = null) {
+		$this->checkToegang($uid);
 		$uid = $uid == null ? LoginModel::getUid() : $uid;
 		if ($this->hasParam("deleted") && $this->getParam("deleted") == "true") {
 			$data = $this->model->find('uid = ?', array($uid));
@@ -43,5 +47,16 @@ class BeheerCiviBestellingController {
 		$data = CiviBestellingInhoudModel::instance()->find('bestelling_id = ?', [$bestelling_id]);
 
 		return new CiviBestellingInhoudTableResponse($data);
+	}
+
+	/**
+	 * Alleen leden met P_FISCAAT_READ mogen het overzicht van andere leden zien.
+	 *
+	 * @param string $uid
+	 */
+	private function checkToegang($uid) {
+		if (!LoginModel::mag(P_FISCAAT_READ) && $uid) {
+			throw new CsrToegangException();
+		}
 	}
 }
