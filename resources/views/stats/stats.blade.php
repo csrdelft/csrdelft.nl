52948184733ddf26b3fbe98c5ad5710b3d32b47d
Jorai Rijsdijk <jorairijsdijk@gmail.com>
2019-05-27 09:33:22 +0200

    Verwerk PR feedback
    - Gebruikt nu ul
    - `@php()` vervangen door `{!! !!}`
    - `maak-stemming` weggehaald (ook geen idee waar het voor was, maar zat
    in de originele implementatie

--- resources/views/stats/stats.blade.php
+++ resources/views/stats/stats.blade.php
@@ -4,8 +4,10 @@
 
 @section('content')
 	Opties:<br>
-	- <a href="?ip=192.168.1.33">/tools/stats?ip=192.168.1.33</a><br>
-	- <a href="?uid=x101">/tools/stats?uid=x101</a>
+	<ul class="insidebullets">
+		<li><a href="?ip=192.168.1.33">/tools/stats?ip=192.168.1.33</a></li>
+		<li><a href="?uid=x101">/tools/stats?uid=x101</a></li>
+	</ul>
 	<table class="table">
 		<tr>
 			<td>tijd</td>
@@ -18,31 +20,29 @@
 		@foreach($log as $logRegel)
 			<tr>
 				<td>
-					{{date('D H:i', strtotime($logRegel->moment))}}
+					{{ date('D H:i', strtotime($logRegel->moment)) }}
 				</td>
 				<td>
-					<a href="?uid={{$logRegel->uid}}">+</a>
-					@php(print \CsrDelft\model\ProfielModel::getLink($logRegel->uid, 'volledig'))
+					<a href="?uid={{ $logRegel->uid }}">+</a>
+					{!! \CsrDelft\model\ProfielModel::getLink($logRegel->uid, 'volledig') !!}
 				</td>
 				<td>
-					<a href="?ip={{$logRegel->ip}}">+</a> {{gethostbyaddr($logRegel->ip)}}
-					<span class="dikgedrukt">{{$logRegel->locatie}}</span>
+					<a href="?ip={{ $logRegel->ip }}">+</a> {{ gethostbyaddr($logRegel->ip) }}
+					<span class="dikgedrukt">{{ $logRegel->locatie }}</span>
 				</td>
 				@if(preg_match('/toevoegen/', $logRegel->url))
 					<td style="background-color: yellow">
-				@elseif(preg_match('/maak-stemming/', $logRegel->url))
-					<td style="background-color: #C00">
 				@elseif(preg_match('/zoeken/', $logRegel->url))
 					<td style="background-color: #3F9">
 				@else
 					<td>
 				@endif
-						<a href="{{ CSR_ROOT . $logRegel->url }}" target="_blank">{{$logRegel->url}}</a>
+						<a href="{{ CSR_ROOT . $logRegel->url }}" target="_blank">{{ $logRegel->url }}</a>
 					</td>
-				<td>{{$logRegel->useragent}}</td>
+				<td>{{ $logRegel->useragent }}</td>
 				<td>
 					@if($logRegel->referer != '')
-						<a href="{{$logRegel->referer}}" target="_blank">{{$logRegel->getFormattedReferer()}}</a>
+						<a href="{{ $logRegel->referer }}" target="_blank">{{ $logRegel->getFormattedReferer() }}</a>
 					@else
 						-
 					@endif
