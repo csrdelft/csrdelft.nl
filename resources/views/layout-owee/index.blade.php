bb102a1053e59d8cf12e059d0ef4a5b0576dd6c6
Mark Bekooy <mark.bekooy@gmail.com>
2019-05-27 20:51:04 +0200

    Maak externe stek al een beetje in het thema van OWee 2019

--- resources/views/layout-owee/index.blade.php
+++ resources/views/layout-owee/index.blade.php
@@ -10,7 +10,7 @@
 	<!-- Banner -->
 	<section id="banner">
 		<div class="inner">
-			<img src="/images/logo-bonfire.svg" alt="Logo Bonfire">
+			<img src="/images/logo-light-it-up.png" alt="Logo Light It Up">
 		</div>
 		<div style="margin-top:40px" class="inner">
 			<a href="https://www.youtube.com/watch?v=a7hhtoo_kzY&list=PLXBOhyG24-WnNgg2RloapxC5X73J1Zxvi" rel="noreferrer" target="_blank"
