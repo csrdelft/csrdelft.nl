2f7de31feb0216f75100b68651dbb087235af020
Gerben <g.j.w.oolbekkink@gmail.com>
2019-07-19 22:41:01 +0200

    Fix aanroepen naar autoload.php

--- phpstan.autoload.php
+++ phpstan.autoload.php
@@ -6,7 +6,6 @@ define('MODE', '');
 include 'defines.defaults.php';
 include 'common/common.functions.php';
 include 'common/common.view.functions.php';
-include 'autoload.php';
 
 foreach (glob("lib/smarty_plugins/*.php") as $filename)
 {
