5cb8966a07e3eaa044b26f953090e40b19c8b3e5
Gerben <g.j.w.oolbekkink@gmail.com>
2019-08-02 21:44:15 +0200

    Blade + symfony routes voor instellingenbeheer

---
+++ resources/views/instellingenbeheer/beheer.blade.php
@@ -0,0 +1,38 @@
+@extends('layout')
+
+@section('titel', 'Instellingenbeheer')
+
+@section('content')
+	<h1>Instellingenbeheer</h1>
+	<p>
+		Op deze pagina kunt u instellingen wijzigen en resetten voor elke module op de stek.
+		Onderstaande tabel toont alle instellingen van de gekozen module.
+	</p>
+	<p>
+		N.B. Deze instellingen zijn essentieel voor de werking van de stek!
+	</p>
+	<ul class="nav nav-tabs">
+		@foreach($modules as $m)
+			<li class="nav-item">
+				@link(ucfirst($m), '/instellingenbeheer/module/' . $m, 'nav-link', 'active')
+			</li>
+		@endforeach
+	</ul>
+	@if($module)
+		<table class="table table-striped">
+			<thead>
+			<tr>
+				<th>Wijzig</th>
+				<th>Id</th>
+				<th>Waarde</th>
+				<th>Reset</th>
+			</tr>
+			</thead>
+			<tbody>
+			@foreach($instellingen as $id)
+				@include('instellingenbeheer.regel', ['waarde' => instelling($module, $id), 'id' => $id, 'module' => $module])
+			@endforeach
+			</tbody>
+		</table>
+	@endif
+@endsection
