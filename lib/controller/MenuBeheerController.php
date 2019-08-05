4de72445e6fcb07c83210c4938b89775e7ada9a0
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-08-05 20:58:38 +0200

    Menubeheer naar symfony  routes

--- lib/controller/MenuBeheerController.php
+++ lib/controller/MenuBeheerController.php
@@ -2,57 +2,37 @@
 
 namespace CsrDelft\controller;
 
-use CsrDelft\controller\framework\AclController;
+use CsrDelft\common\CsrToegangException;
+use CsrDelft\controller\framework\QueryParamTrait;
 use CsrDelft\model\MenuModel;
 use CsrDelft\model\security\LoginModel;
-use CsrDelft\view\CsrLayoutPage;
 use CsrDelft\view\JsonResponse;
 use CsrDelft\view\MeldingResponse;
-use CsrDelft\view\menubeheer\MenuBeheerView;
 use CsrDelft\view\menubeheer\MenuItemForm;
 
 /**
  * MenuBeheerController.class.php
  *
  * @author P.W.G. Brussee <brussee@live.nl>
- *
- * @property MenuModel $model
  */
-class MenuBeheerController extends AclController {
+class MenuBeheerController {
+	use QueryParamTrait;
 
-	public function __construct($query) {
-		parent::__construct($query, MenuModel::instance());
-		if ($this->getMethod() == 'GET') {
-			$this->acl = array(
-				'beheer' => P_LOGGED_IN
-			);
-		} else {
-			$this->acl = array(
-				'toevoegen' => P_LOGGED_IN,
-				'bewerken' => P_LOGGED_IN,
-				'verwijderen' => P_LOGGED_IN,
-				'zichtbaar' => P_LOGGED_IN
-			);
-		}
-	}
+	private $model;
 
-	public function performAction(array $args = array()) {
-		$this->action = 'beheer';
-		if ($this->hasParam(2)) {
-			$this->action = $this->getParam(2);
-		}
-		parent::performAction($this->getParams(3));
+	public function __construct() {
+		$this->model = MenuModel::instance();
 	}
 
 	public function beheer($menu_name = 'main') {
 		if ($menu_name != LoginModel::getUid() AND !LoginModel::mag(P_ADMIN)) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
 		$root = $this->model->getMenu($menu_name);
 		if (!$root OR !$root->magBeheren()) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
-		$this->view = view('menubeheer.tree', [
+		return view('menubeheer.tree', [
 			'root' => $root,
 			'menus' => MenuModel::instance()->getMenuBeheerLijst(),
 		]);
@@ -65,28 +45,28 @@ class MenuBeheerController extends AclController {
 			$parent = $this->model->getMenuItem((int)$parent_id);
 		}
 		if (!$parent OR !$parent->magBeheren()) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
 		$item = $this->model->nieuw($parent->item_id);
 		if (!$item OR !$item->magBeheren()) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
-		$form = new MenuItemForm($item, $this->action, $parent_id); // fetches POST values itself
+		$form = new MenuItemForm($item, 'toevoegen', $parent_id); // fetches POST values itself
 		if ($form->validate()) { // form checks if hidden fields are modified
 			$this->model->create($item);
 			setMelding('Toegevoegd: ' . $item->tekst, 1);
-			$this->view = new MeldingResponse();
+			return new MeldingResponse();
 		} else {
-			$this->view = $form;
+			return $form;
 		}
 	}
 
 	public function bewerken($item_id) {
 		$item = $this->model->getMenuItem((int)$item_id);
 		if (!$item OR !$item->magBeheren()) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
-		$form = new MenuItemForm($item, $this->action, $item->item_id); // fetches POST values itself
+		$form = new MenuItemForm($item, 'bewerken', $item->item_id); // fetches POST values itself
 		if ($form->validate()) { // form checks if hidden fields are modified
 			$rowCount = $this->model->update($item);
 			if ($rowCount > 0) {
@@ -94,29 +74,29 @@ class MenuBeheerController extends AclController {
 			} else {
 				setMelding($item->tekst . ' ongewijzigd', 0);
 			}
-			$this->view = new JsonResponse(true);
+			return new JsonResponse(true);
 		} else {
-			$this->view = $form;
+			return $form;
 		}
 	}
 
 	public function verwijderen($item_id) {
 		$item = $this->model->getMenuItem((int)$item_id);
 		if (!$item OR !$item->magBeheren()) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
 		$rowCount = $this->model->removeMenuItem($item);
 		setMelding($item->tekst . ' verwijderd', 1);
 		if ($rowCount > 0) {
 			setMelding($rowCount . ' menu-items niveau omhoog verplaatst.', 2);
 		}
-		$this->view = new JsonResponse(true);
+		return new JsonResponse(true);
 	}
 
 	public function zichtbaar($item_id) {
 		$item = $this->model->getMenuItem((int)$item_id);
 		if (!$item OR !$item->magBeheren()) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
 		$item->zichtbaar = !$item->zichtbaar;
 		$rowCount = $this->model->update($item);
@@ -125,7 +105,6 @@ class MenuBeheerController extends AclController {
 		} else {
 			setMelding($item->tekst . ' ongewijzigd', 0);
 		}
-		$this->view = new JsonResponse(true);
+		return new JsonResponse(true);
 	}
-
 }
