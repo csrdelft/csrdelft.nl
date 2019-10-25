807c1b305039cd1892e9f968119ce0ab24418f1b
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-10-25 11:27:43 +0200

    Fix typefeut in ErrorController en fix POST errors

--- lib/controller/ErrorController.php
+++ lib/controller/ErrorController.php
@@ -7,23 +7,38 @@ namespace CsrDelft\controller;
 use CsrDelft\common\CsrGebruikerException;
 use CsrDelft\common\CsrToegangException;
 use Symfony\Component\Debug\Exception\FlattenException;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpKernel\Log\DebugLoggerInterface;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ErrorController {
-	public function handleException(Request $request, FlattenException $exception, DebugLoggerInterface $logger = null) {
+	public function handleException(RequestStack $requestStack, FlattenException $exception) {
+		if ($requestStack->getMasterRequest()->getMethod() == 'POST') {
+			return new Response($exception->getMessage(), $this->getCode($exception));
+		}
+
+		switch ($this->getCode($exception)) {
+			case Response::HTTP_BAD_REQUEST: return view('fout.400', ['bericht' => $exception->getMessage()]);
+			case Response::HTTP_NOT_FOUND: return view('fout.404');
+			case Response::HTTP_FORBIDDEN: return view('fout.403');
+		}
+
+		return view('fout.500');
+	}
+
+	private function getCode(FlattenException $exception) {
 		switch ($exception->getClass()) {
 			case CsrGebruikerException::class:
-				return view('fout.400', ['bericth' => $exception->getMessage()]);
+				return Response::HTTP_BAD_REQUEST;
 			case NotFoundHttpException::class:
-				return view('fout.404');
+				return Response::HTTP_NOT_FOUND;
 			case AccessDeniedException::class:
 			case CsrToegangException::class:
-				return view('fout.403');
+				return Response::HTTP_FORBIDDEN;
 		}
-		return view('fout.500');
+
+		return Response::HTTP_INTERNAL_SERVER_ERROR;
 	}
 
 }
