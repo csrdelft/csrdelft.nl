26c9e58421059abe482dd91adb712a37bfe55996
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-10-30 22:24:34 +0100

    Fix spatie tussen attributen

--- lib/view/cms/CmsPaginaView.php
+++ lib/view/cms/CmsPaginaView.php
@@ -40,7 +40,7 @@ class CmsPaginaView implements View, ToResponse {
 	public function view() {
 		echo getMelding();
 		if ($this->pagina->magBewerken()) {
-			echo '<a href="/pagina/bewerken/' . $this->pagina->naam . '" class="btn float-right"title="Bewerk pagina&#013;' . $this->pagina->laatst_gewijzigd . '">' . Icon::getTag('bewerken') . '</a>';
+			echo '<a href="/pagina/bewerken/' . $this->pagina->naam . '" class="btn float-right" title="Bewerk pagina&#013;' . $this->pagina->laatst_gewijzigd . '">' . Icon::getTag('bewerken') . '</a>';
 		}
 		echo CsrBB::parseHtml(htmlspecialchars_decode($this->pagina->inhoud), $this->pagina->inline_html);
 	}
