e0251ff68d4f1bf38f2b613aa903948e49cb0a69
Sander Borst <s.borst@live.nl>
2019-11-07 17:08:22 +0100

    Fix goede statuscode CsrGebruikersException

--- lib/common/CsrGebruikerException.php
+++ lib/common/CsrGebruikerException.php
@@ -7,5 +7,8 @@ namespace CsrDelft\common;
  * @since 20170824
  */
 class CsrGebruikerException extends CsrException {
-
+	public function __construct($message, $statusCode = 200, \Exception $previous = null, array $headers = [], $code = 0)
+	{
+		parent::__construct($message, $statusCode, $previous, $headers, $code);
+	}
 }
