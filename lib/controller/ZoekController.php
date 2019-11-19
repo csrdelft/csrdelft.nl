44b676498fa469b9505f190b8acdd3dd8bd1635a
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-11-19 16:03:25 +0100

    Verwijder ongebruikte code ZoekController

--- lib/controller/ZoekController.php
+++ lib/controller/ZoekController.php
@@ -19,14 +19,11 @@ class ZoekController {
 		$zoekterm = filter_input(INPUT_GET, 'q', FILTER_SANITIZE_STRING);
 		$resultaat = [];
 
-		$bronnen = [];
 		$instelling = lid_instelling('zoeken', 'leden');
 		if ($instelling !== 'nee') {
-			$bronnen['Leden'] = '/tools/naamsuggesties?zoekin=leden&status=' . $instelling . '&q=';
 			$resultaat['Leden'] = (new ToolsController())->naamsuggesties('leden', 'instelling', $zoekterm)->getModel();
 		}
 
-		// TODO: bundelen om simultane verbindingen te sparen
 		$groepen = [
 			'commissies' => CommissiesController::class,
 			'kringen' => KringenController::class,
@@ -37,7 +34,6 @@ class ZoekController {
 		];
 		foreach ($groepen as $option => $controller) {
 			if (lid_instelling('zoeken', $option) === 'ja') {
-				$bronnen[ucfirst($option)] = '/groepen/' . $option . '/zoeken?q=';
 				/** @var AbstractGroepenController $groepController */
 				$groepController = new $controller('');
 				$resultaat[ucfirst($option)] = $groepController->zoeken($request, $zoekterm)->getModel();
@@ -45,27 +41,22 @@ class ZoekController {
 		}
 
 		if (lid_instelling('zoeken', 'forum') === 'ja') {
-			$bronnen['Forum'] = '/forum/titelzoeken?q=';
 			$resultaat['Forum'] = (new ForumController())->titelzoeken($request, $zoekterm)->getModel();
 		}
 
 		if (lid_instelling('zoeken', 'fotoalbum') === 'ja') {
-			$bronnen['Fotoalbum'] = '/fotoalbum/zoeken?q=';
 			$resultaat['Fotoalbum'] = (new FotoAlbumController())->zoeken($request, $zoekterm)->getModel();
 		}
 
 		if (lid_instelling('zoeken', 'agenda') === 'ja') {
-			$bronnen['Agenda'] = '/agenda/zoeken?q=';
 			$resultaat['Agenda'] = (new AgendaController())->zoeken($request, $zoekterm)->getModel();
 		}
 
 		if (lid_instelling('zoeken', 'documenten') === 'ja') {
-			$bronnen['Documenten'] = '/documenten/zoeken?q=';
 			$resultaat['Documenten'] = (new DocumentenController())->zoeken($request, $zoekterm)->getModel();
 		}
 
 		if (lid_instelling('zoeken', 'boeken') === 'ja') {
-			$bronnen['Boeken'] = '/bibliotheek/zoeken?q=';
 			$resultaat['Boeken'] = (new BibliotheekController())->zoeken($request, $zoekterm)->getModel();
 		}
 
