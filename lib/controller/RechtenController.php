1ae5a50f0f88ffc169c4611537424c7f4ed733b5
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-11-19 15:41:16 +0100

    RechtenController naar router

--- lib/controller/RechtenController.php
+++ lib/controller/RechtenController.php
@@ -2,7 +2,7 @@
 
 namespace CsrDelft\controller;
 
-use CsrDelft\controller\framework\AclController;
+use CsrDelft\common\CsrToegangException;
 use CsrDelft\model\entity\security\AccessControl;
 use CsrDelft\model\security\AccessModel;
 use CsrDelft\view\datatable\RemoveRowsResponse;
@@ -17,72 +17,52 @@ use CsrDelft\view\RechtenTable;
  * @author P.W.G. Brussee <brussee@live.nl>
  *
  * Controller van de ACL.
- *
- * @property AccessModel $model
  */
-class RechtenController extends AclController {
+class RechtenController {
+	private $model;
 
-	public function __construct($query) {
-		parent::__construct($query, AccessModel::instance());
-		if ($this->getMethod() == 'GET') {
-			$this->acl = array(
-				'bekijken' => P_LOGGED_IN
-			);
-		} else {
-			$this->acl = array(
-				'bekijken' => P_LOGGED_IN,
-				'aanmaken' => P_LOGGED_IN,
-				'wijzigen' => P_LOGGED_IN,
-				'verwijderen' => P_LOGGED_IN
-			);
-		}
-	}
-
-	public function performAction(array $args = array()) {
-		if ($this->hasParam(2)) {
-			$this->action = $this->getParam(2);
-		}
-		parent::performAction($this->getParams(3));
+	public function __construct() {
+		$this->model = AccessModel::instance();
 	}
 
 	public function bekijken($environment = null, $resource = null) {
-		if ($this->getMethod() == 'POST') {
-			$acl = $this->model->getTree($environment, $resource);
-			$this->view = new RechtenData($acl);
-		} else {
-			$table = new RechtenTable($this->model, $environment, $resource);
-			$this->view = view('default', ['content' => $table]);
-		}
+		return view('default', [
+			'content' => new RechtenTable($this->model, $environment, $resource)
+		]);
+	}
+
+	public function data($environment = null, $resource = null) {
+		return new RechtenData($this->model->getTree($environment, $resource));
 	}
 
 	public function aanmaken($environment = null, $resource = null) {
 		$ac = $this->model->nieuw($environment, $resource);
-		$form = new RechtenForm($ac, $this->action);
+		$form = new RechtenForm($ac, 'aanmaken');
 		if ($form->validate()) {
 			$this->model->setAcl($ac->environment, $ac->resource, array(
 				$ac->action => $ac->subject
 			));
-			$this->view = new RechtenData(array($ac));
+			return new RechtenData(array($ac));
 		} else {
-			$this->view = $form;
+			return $form;
 		}
 	}
 
 	public function wijzigen() {
 		$selection = filter_input(INPUT_POST, 'DataTableSelection', FILTER_SANITIZE_STRING, FILTER_FORCE_ARRAY);
 		if (!isset($selection[0])) {
-			$this->exit_http(403);
+			throw new CsrToegangException();
 		}
 		/** @var AccessControl $ac */
 		$ac = $this->model->retrieveByUUID($selection[0]);
-		$form = new RechtenForm($ac, $this->action);
+		$form = new RechtenForm($ac, 'wijzigen');
 		if ($form->validate()) {
 			$this->model->setAcl($ac->environment, $ac->resource, array(
 				$ac->action => $ac->subject
 			));
-			$this->view = new RechtenData(array($ac));
+			return new RechtenData(array($ac));
 		} else {
-			$this->view = $form;
+			return $form;
 		}
 	}
 
@@ -97,7 +77,7 @@ class RechtenController extends AclController {
 			));
 			$response[] = $ac;
 		}
-		$this->view = new RemoveRowsResponse($response);
+		return new RemoveRowsResponse($response);
 	}
 
 }
