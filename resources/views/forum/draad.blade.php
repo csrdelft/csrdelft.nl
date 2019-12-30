db582f74d9bcc3a78845eadccd2ade7ec26204ac
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2019-12-30 23:17:26 +0100

    Verwijder oude post form

--- resources/views/forum/draad.blade.php
+++ resources/views/forum/draad.blade.php
@@ -170,7 +170,6 @@
 	@if($draad->magPosten())
 		@include('forum.partial.draad_reageren')
 		@php($postform->view())
-{{--		@include('forum.partial.post_form', ['deel' => $draad->getForumDeel()])--}}
 	@endif
 </div>
 
