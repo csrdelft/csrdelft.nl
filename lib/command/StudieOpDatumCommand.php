81f519796d5d524917b7e14ab1ebf8e7c726dfc5
Tim Nederveen <jptned@hotmail.nl>
2020-01-06 18:35:07 +0100

    Command om studies op datum op te halen

---
+++ lib/command/StudieOpDatumCommand.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace CsrDelft\command;
+
+use CsrDelft\model\entity\LidStatus;
+use CsrDelft\model\entity\profiel\Profiel;
+use CsrDelft\model\entity\profiel\ProfielLogValueChange;
+use CsrDelft\model\entity\profiel\ProfielUpdateLogGroup;
+use CsrDelft\model\ProfielModel;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
+class StudieOpDatumCommand extends ContainerAwareCommand {
+	private $profielModel;
+
+	public function __construct(ProfielModel $profielModel) {
+		$this->profielModel = $profielModel;
+
+		parent::__construct();
+	}
+
+	protected function configure() {
+		$this
+			->setName('leden:studie:datum')
+			->setDescription('Haal de studies van leden op op een bepaalde datum.');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		$helper = $this->getHelper('question');
+
+		// Bepaal datum
+		$datum = null;
+		do {
+			$question = new Question("Per welke datum? [dd-mm-jjjj] ");
+			$antwoord = $helper->ask($input, $output, $question);
+			if ($antwoord) {
+				$datum = \DateTime::createFromFormat('d-m-Y', $antwoord);
+			}
+		} while (!$datum);
+
+		$output->writeln("");
+
+		// Haal leden op
+		/** @var Profiel $lid */
+		foreach ($this->profielModel->find() as $lid) {
+
+			// Check of lid al lid was
+			$lidVanaf = \DateTime::createFromFormat('d-m-Y', '01-09-' . $lid->lidjaar);
+			if ($lidVanaf > $datum) continue;
+
+			$watch = ['status', 'studie'];
+			$values = [];
+			foreach ($watch as $field) {
+				$values[$field] = $lid->$field;
+			}
+
+			$logs = array_filter($lid->changelog, function($a) { return $a instanceof ProfielUpdateLogGroup; });
+			usort($logs, function (ProfielUpdateLogGroup $a, ProfielUpdateLogGroup $b) {
+				if ($a->timestamp == $b->timestamp) {
+					return 0;
+				}
+
+				return $a->timestamp < $b->timestamp ? -1 : 1;
+			});
+			foreach ($logs as $log) {
+				if (empty($watch)) break;
+				foreach ($log->entries as $entry) {
+					if ($entry instanceof ProfielLogValueChange) {
+						if (in_array($entry->field, $watch)) {
+							$value = $log->timestamp < $datum ? $entry->newValue : $entry->oldValue;
+							$values[$entry->field] = $value;
+							if ($log->timestamp > $datum) {
+								$watch = array_diff($watch, [$entry->field]);
+							}
+						}
+					}
+				}
+			}
+
+			if (!LidStatus::isLidLike($values['status'])) continue;
+			$output->writeln("{$lid->uid};{$lid->getNaam()};{$values['studie']}");
+		}
+
+		$output->writeln("");
+	}
+}
