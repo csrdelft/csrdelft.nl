fe492a454f34dafb9fffa5d1669c443913341545
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-02-20 23:16:27 +0100

    Haal verkeerd gekopieerde code uit with-preload

--- resources/assets/js/fotoalbum/with-preload.ts
+++ resources/assets/js/fotoalbum/with-preload.ts
@@ -1,29 +1,11 @@
 import load from 'jgallery/src/utils/load';
 import AlbumItem from 'jgallery/types/album-item';
 import {GalleryDecorator} from 'jgallery/types/gallery';
-import Params from 'jgallery/types/gallery/parameters';
 
 const withPreload: GalleryDecorator = (constructor) =>
 	class extends constructor {
-		constructor(albums: AlbumItem[], params: Params) {
-			super(albums, params);
-
-			document.addEventListener('keydown', (e) => {
-				switch (e.key) {
-					case 'ArrowLeft':
-						this.prev();
-						break;
-					case 'ArrowRight':
-						this.next();
-						break;
-				}
-			});
-		}
-
 		protected goToItem(item: AlbumItem) {
-			const {album} = this;
-
-			const {items} = album;
+			const {items} = this.album;
 			const next = items[items.indexOf(item) + 1];
 			if (next) {
 				load(next.url!);
