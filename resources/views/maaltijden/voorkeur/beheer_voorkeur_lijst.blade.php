acc0765546e84a7f14514b287c48b3192b0688f6
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-04-25 14:14:45 +0200

    Fix verkeerde functie aanroep

--- resources/views/maaltijden/voorkeur/beheer_voorkeur_lijst.blade.php
+++ resources/views/maaltijden/voorkeur/beheer_voorkeur_lijst.blade.php
@@ -7,7 +7,7 @@
 	@if($voorkeur)
 		@if($loop->first)
 			<tr id="voorkeur-row-{{$voorkeur->van_uid}}">
-				<td>{!! \CsrDelft\repository\ProfielRepository::getLink($voorkeur->getVanUid(),instelling('corvee', 'weergave_ledennamen_beheer')) !!}</td>
+				<td>{!! \CsrDelft\repository\ProfielRepository::getLink($voorkeur->van_uid,instelling('corvee', 'weergave_ledennamen_beheer')) !!}</td>
 				@endif
 				@include('maaltijden.voorkeur.beheer_voorkeur_veld', ['voorkeur' => $voorkeur, 'crid' => $voorkeur->crv_repetitie_id, 'uid' => $voorkeur->uid])
 				@if($loop->last)
