ea1854ff744da7e33c1805e188c6646a9fa99f04
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-05-09 21:58:27 +0200

    getUUID voor groepen

--- lib/entity/groepen/AbstractGroep.php
+++ lib/entity/groepen/AbstractGroep.php
@@ -14,6 +14,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping as ORM;
 use PDO;
 use Symfony\Component\Serializer\Annotation as Serializer;
+use function common\short_class;
 
 
 /**
@@ -249,4 +250,7 @@ abstract class AbstractGroep implements DataTableEntry {
 		return count($keuzes) == count($correct);
 	}
 
+	public function getUUID() {
+		return $this->id . '@' . short_class($this) . '.csrdelft.nl';
+	}
 }
