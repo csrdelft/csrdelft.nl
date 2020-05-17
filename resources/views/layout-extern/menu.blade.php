b60ddbbc3b5af23837cd42928ec46c991f1bdf54
Rick <r.m.huizer@student.tudelft.nl>
2020-05-17 16:32:35 +0200

    pas tekst in menu op vereniging paginas aan

--- resources/views/layout-extern/menu.blade.php
+++ resources/views/layout-extern/menu.blade.php
@@ -2,7 +2,7 @@
 	<li class="rotate"><a href="/vereniging/geloof">Geloof</a></li>
 	<li class="rotate"><a href="/vereniging/vorming">Vorming</a></li>
 	<li class="rotate"><a href="/vereniging/gezelligheid">Gezelligheid</a></li>
-	<li class="rotate"><a href="/vereniging/sport">Sport</a></li>
+	<li class="rotate"><a href="/vereniging/onderverenigingen">Onderverenigingen</a></li>
 	<li class="rotate"><a href="/vereniging/ontspanning">Ontspanning</a></li>
 	<!--li class="rotate"><a href="/vereniging/maaltijden">Maaltijden</a></li-->
 	<li class="rotate"><a href="/vereniging/societeit">Sociëteit</a></li>
