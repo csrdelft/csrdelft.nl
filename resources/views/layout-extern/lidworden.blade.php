28d4bd9fe9dc2a5cd8c661d32cb6887485d87c86
Mark Bekooy <mark.bekooy@gmail.com>
2020-05-17 14:12:29 +0200

    Zet interesse formulier op de rechter zijde van de pagina op groot scherm en onder de tekst op klein scherm

--- resources/views/layout-extern/lidworden.blade.php
+++ resources/views/layout-extern/lidworden.blade.php
@@ -10,7 +10,11 @@
 	@if($showmenu)
 		@include('layout-extern.menu')
 	@endif
-	@php($body->view())
-	<h2 class="major" style="margin-top:1.5em;">Interesse Formulier</h2>
-	@include('layout-extern.form')
+	<div id="lid-worden-tekst">
+		@php($body->view())
+	</div>
+	<div id="interesse-formulier">
+		<h2 class="major" style="margin-top:1.5em;">Interesse Formulier</h2>
+		@include('layout-extern.form')
+	</div>
 @endsection
