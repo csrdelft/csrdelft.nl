7441d33757c0620563f613e64af9a1774a3f09d0
Tim Nederveen <jptned@hotmail.nl>
2020-06-04 00:02:37 +0200

    Word lid blok

--- resources/views/layout-extern/owee.blade.php
+++ resources/views/layout-extern/owee.blade.php
@@ -1,6 +1,6 @@
 @extends('layout-extern.layout')
 
-@section('titel', $titel)
+@section('titel', 'Machtig mooi - OWee 2020')
 
 @section('styles')
 	@stylesheet('extern.css')
@@ -21,11 +21,28 @@
 
 	<div class="owee">
 		<div class="bbl atl hero">
-			<div class="content"></div>
+			<div class="content">
+				<img src="/images/owee2020.svg" alt="C.S.R. - Machtig Mooi">
+			</div>
 		</div>
 
-		<div class="content">
+		<div class="content pt-5 pb-5">
+			<div class="row align-items-center">
+				<div class="col-md-6 mb-4 mb-md-0">
+					<h1>Word lid van C.S.R.</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac nisi vitae purus placerat finibus. Ut eros mauris, euismod eget ullamcorper non, aliquam facilisis nunc. Nulla porta congue nunc, eu lobortis lorem rhoncus sed. In id luctus risus, nec auctor nibh. Vivamus at nibh eget velit tincidunt finibus nec ac turpis.</p>
 
+					<div class="mt-4">
+						<a href="#" class="cta primary">Ik wil lid worden</a>
+						<a href="#" class="cta secondary">Eerst een lid spreken</a>
+					</div>
+				</div>
+				<div class="col-md-6">
+					<div class="iframe-container">
+						<iframe src="https://www.youtube-nocookie.com/embed/AE8RE8e5qI4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+					</div>
+				</div>
+			</div>
 		</div>
 
 		<div class="bbl atl buttons">
