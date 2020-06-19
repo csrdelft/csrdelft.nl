7e176e6507f79dd03d2bbcc97768be682c122e66
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-06-19 16:48:08 +0200

    Draai wat aan de ruimteeffect knopjes

--- resources/assets/js/entry/fxspace.ts
+++ resources/assets/js/entry/fxspace.ts
@@ -14,9 +14,9 @@ shaderToy(`
 
 #define zoom   0.800
 #define tile   0.850
-#define speed  0.010
+#define speed  0.005
 
-#define brightness 0.0015
+#define brightness 0.0035
 #define darkmatter 0.300
 #define distfading 0.730
 #define saturation 0.850
