a43efd2865fdb4f7ea5d26bf4c44230e269ac967
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-07-05 20:00:05 +0200

    Hoofdlettergevoeligheid :( Deze migratie is al gerund op Syrinx

--- db/migrations/20200704101317_civi_saldo_pk_migratie.php
+++ db/migrations/20200704101317_civi_saldo_pk_migratie.php
@@ -4,19 +4,19 @@ use Phinx\Migration\AbstractMigration;
 
 class CiviSaldoPkMigratie extends AbstractMigration {
 	public function up() {
-		$this->query('ALTER TABLE `civibestelling` DROP FOREIGN KEY `CiviBestelling_ibfk_1`;');
-		$this->query('ALTER TABLE civisaldo MODIFY id INT NOT NULL');
-		$this->query('DROP INDEX uid ON civisaldo');
-		$this->query('ALTER TABLE civisaldo DROP PRIMARY KEY');
-		$this->query('ALTER TABLE civisaldo DROP id');
-		$this->query('ALTER TABLE civisaldo ADD PRIMARY KEY (uid)');
-		$this->query('ALTER TABLE `civibestelling` ADD CONSTRAINT `CiviBestelling_ibfk_1` FOREIGN KEY (uid) REFERENCES civisaldo (uid)');
+		$this->query('ALTER TABLE `CiviBestelling` DROP FOREIGN KEY `CiviBestelling_ibfk_1`;');
+		$this->query('ALTER TABLE CiviSaldo MODIFY id INT NOT NULL');
+		$this->query('DROP INDEX uid ON CiviSaldo');
+		$this->query('ALTER TABLE CiviSaldo DROP PRIMARY KEY');
+		$this->query('ALTER TABLE CiviSaldo DROP id');
+		$this->query('ALTER TABLE CiviSaldo ADD PRIMARY KEY (uid)');
+		$this->query('ALTER TABLE `CiviBestelling` ADD CONSTRAINT `CiviBestelling_ibfk_1` FOREIGN KEY (uid) REFERENCES CiviSaldo (uid)');
 	}
 
 	public function down() {
-		$this->query('ALTER TABLE `civibestelling` DROP FOREIGN KEY `CiviBestelling_ibfk_1`;');
+		$this->query('ALTER TABLE `CiviBestelling` DROP FOREIGN KEY `CiviBestelling_ibfk_1`;');
 		$this->query('ALTER TABLE CiviSaldo ADD id INT AUTO_INCREMENT NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (id)');
 		$this->query('CREATE UNIQUE INDEX uid ON CiviSaldo (uid)');
-		$this->query('ALTER TABLE `civibestelling` ADD CONSTRAINT `CiviBestelling_ibfk_1` FOREIGN KEY (uid) REFERENCES civisaldo (uid)');
+		$this->query('ALTER TABLE `CiviBestelling` ADD CONSTRAINT `CiviBestelling_ibfk_1` FOREIGN KEY (uid) REFERENCES CiviSaldo (uid)');
 	}
 }
