54e8d8d5e913418a6f6afa1769b730307b8d4ca1
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-07-16 11:16:44 +0200

    Verwijder ongebruikte bundles

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
