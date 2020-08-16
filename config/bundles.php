2ba66c7ac99b369a08b871d38c1bea835f8596aa
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-08-16 15:23:06 +0200

    Voeg sentry toe

--- config/bundles.php
+++ config/bundles.php
@@ -10,4 +10,5 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
 ];
