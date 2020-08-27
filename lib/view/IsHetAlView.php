f9a8df2d4fbad9c430538d7db8e75d487327dd59
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-08-27 19:44:08 +0200

    Verwijder red een kind dingen, is niet meer nodig

--- lib/view/IsHetAlView.php
+++ lib/view/IsHetAlView.php
@@ -34,18 +34,11 @@ class IsHetAlView implements View {
 		'u het forum kan volgen met RSS?' => '/profiel#forum',
 	);
 
-	private static $redEenKindActief = true;
-	private static $redEenKindHuidig = 280;
-	private static $redEenKindDoel = 280;
-
 	public function __construct($ishetal) {
 		$this->model = $ishetal;
 		if ($this->model == 'willekeurig') {
 			$lidInstellingenRepository = ContainerFacade::getContainer()->get(LidInstellingenRepository::class);
 			$opties = array_slice($lidInstellingenRepository->getTypeOptions('zijbalk', 'ishetal'), 2);
-			if (!self::$redEenKindActief && ($key = array_search('red een kind', $opties)) !== false) {
-				unset($opties[$key]);
-			}
 			$this->model = $opties[array_rand($opties)];
 		}
 		switch ($this->model) {
@@ -53,10 +46,6 @@ class IsHetAlView implements View {
 			case 'foutmelding':
 			case 'sponsorkliks':
 				$this->ja = null;
-				break;
-
-			case 'red een kind':
-				$this->ja = self::$redEenKindHuidig >= self::$redEenKindDoel;
 				break;
 
 			case 'dies' :
@@ -136,10 +125,6 @@ class IsHetAlView implements View {
 				echo '<iframe src="https://banner.sponsorkliks.com/skinfo.php?&background-color=F5F5F5&text-color=000000&header-background-color=F5F5F5&header-text-color=F5F5F5&odd-row=FFFFFF&even-row=09494a&odd-row-text=09494a&even-row-text=ffffff&type=financial&club_id=3605&width=193" frameborder="0" referrerpolicy="no-referrer" class="sponsorkliks-zijbalk"></iframe>';
 				break;
 
-			case 'red een kind':
-				echo 'Hebben we al genoeg geld voor Red een Kind?';
-				break;
-
 			case 'jarig':
 				echo 'Ben ik al jarig?';
 				break;
@@ -183,10 +168,6 @@ class IsHetAlView implements View {
 			echo '<div class="nee">OVER ' . $this->ja . ' ' . ($this->ja == 1 ? 'DAG' : 'DAGEN') . '!</div>';
 		} else {
 			// wist u dat
-		}
-
-		if ($this->model === 'red een kind') {
-			echo '<progress id="file" value="' . self::$redEenKindHuidig . '" max="' . self::$redEenKindDoel . '" title="€ ' . self::$redEenKindHuidig . ',- van € ' . self::$redEenKindDoel . ',-"></progress>';
 		}
 
 		echo '</div>';
