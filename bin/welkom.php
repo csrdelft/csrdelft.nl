480b60e321b9508edc459b1fa4214743d67b3153
Job Kanis <jobkanis@gmail.com>
2020-08-30 12:16:11 +0200

    Verander welkomstmail voor lichting 20

--- bin/welkom.php
+++ bin/welkom.php
@@ -11,9 +11,14 @@ $kernel = require_once 'configuratie.include.php';
 
 # Scriptje om voor sjaars een wachtwoord te genereren en dat toe te mailen.
 # Vergeet niet voor gebruik hieronder het jaar aan te passen.
-$jaar = '19';
+$jaar = '20';
 
 $profielRepository = $kernel->getContainer()->get(ProfielRepository::class);
+
+# Tekst toe te voegen aan de mail als de novieten een abonnement op de maaltijd krijgen:
+# 	Je hebt een abonnement op de donderdagmaaltijd in Confide. Dit om jullie te stimuleren deel te nemen aan C.S.R. activiteiten en te integreren met leden. Als je inlogt op de webstek zie je meteen op de hoofdpagina een blokje waarin je jezelf kunt af- of aanmelden voor de eerstvolgende maaltijd.
+# 	Kun je niet op een maaltijd aanwezig zijn, meld je dan af op de webstek, voor omstreeks 15:00 op de dag van de maaltijd, want na dat tijdstip beginnen de koks met koken en wordt er op jou gerekend met boodschappen doen. Als je er na 15:00 achter komt dat je juist wel of juist niet aanwezig kunt zijn, bel dan even de koks of bel Confide. Met een goede reden kan je dan eventueel doorgestreept of toegevoegd worden op de maaltijdlijst.
+# 	Waarom is dit belangrijk? Omdat een maaltijd €3,50 kost en als je jezelf vergeet af te melden en niet komt, dit bedrag toch van je CiviSaldo wordt afgeschreven. Het is goed om naar maaltijden te gaan, maar als je niet kunt, meld je dan af! Dat scheelt je pieken.
 
 foreach ($profielRepository->getNovieten($jaar) as $profiel) {
     $url = CSR_ROOT . '/wachtwoord/aanvragen';
@@ -26,10 +31,7 @@ Via de webstek kun je onder andere:
 - Berichten lezen en plaatsen op het forum
 - Berichten plaatsen in de C.S.R.-courant, die wekelijks aan alle leden wordt verzonden
 - Gegevens van andere leden opzoeken
-- Aan/afmelden voor de maaltijd, hierover het volgende:
-Je hebt een abonnement op de donderdagmaaltijd in Confide. Dit om jullie te stimuleren deel te nemen aan C.S.R. activiteiten en te integreren met leden. Als je inlogt op de webstek zie je meteen op de hoofdpagina een blokje waarin je jezelf kunt af- of aanmelden voor de eerstvolgende maaltijd.
-Kun je niet op een maaltijd aanwezig zijn, meld je dan af op de webstek, voor omstreeks 15:00 op de dag van de maaltijd, want na dat tijdstip beginnen de koks met koken en wordt er op jou gerekend met boodschappen doen. Als je er na 15:00 achter komt dat je juist wel of juist niet aanwezig kunt zijn, bel dan even de koks of bel Confide. Met een goede reden kan je dan eventueel doorgestreept of toegevoegd worden op de maaltijdlijst.
-Waarom is dit belangrijk? Omdat een maaltijd €3,50 kost en als je jezelf vergeet af te melden en niet komt, dit bedrag toch van je CiviSaldo wordt afgeschreven. Het is goed om naar maaltijden te gaan, maar als je niet kunt, meld je dan af! Dat scheelt je pieken.
+- Aan/afmelden voor de maaltijd
 
 Gebruik je emailadres om in te loggen op de webstek: {$profiel->email}
 Gebruik de eerste keer de [url={$url}]wachtwoord aanvragen[/url] functie om je eigen wachtwoord in te stellen.
@@ -40,7 +42,7 @@ Stuur dan een e-mail: pubcie@csrdelft.nl
 
 Met amicale groet,
 
-Mark Bekooy,
+Job Kanis,
 h.t. PubCie-Praeses der Civitas Studiosorum Reformatorum
 TEXT;
     $mail = new Mail(array($profiel->email => $profiel->voornaam), 'Inloggegevens C.S.R.-webstek', $tekst);
