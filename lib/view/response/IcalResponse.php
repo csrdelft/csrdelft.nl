f4e19926198dbead09c2b494cc793ee8a60cc408
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-09-01 23:45:33 +0200

    Zet ical naar propere IcalResponse

--- lib/view/response/IcalResponse.php
+++ lib/view/response/IcalResponse.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IcalResponse extends Response {
 	public function __construct() {
-		parent::__construct(null, 200);//, ['content-type' => 'text/calendar']);
+		parent::__construct(null, 200, ['content-type' => 'text/calendar']);
 		$this->setCharset('UTF-8');
 	}
 
