7761a3f5660f00daa6b36b9ef6457da4bd588885
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2020-09-27 21:15:02 +0200

    Fix rollback

--- db/doctrine_migrations/Version20200927164704.php
+++ db/doctrine_migrations/Version20200927164704.php
@@ -21,7 +21,7 @@ final class Version20200927164704 extends AbstractMigration
 
 	public function down(Schema $schema): void
 	{
-		$this->addSql('CREATE TABLE login_sessions (session_hash VARCHAR(191) COMMENT \'(DC2Type:stringkey)\' CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, uid VARCHAR(4) COMMENT \'(DC2Type:uid)\' CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, login_moment DATETIME NOT NULL, expire DATETIME NOT NULL, user_agent VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, ip VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, lock_ip TINYINT(1) NOT NULL, authentication_method VARCHAR(255) CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_general_ci`, INDEX IDX_B4C4BD8C539B0606 (uid), PRIMARY KEY(session_hash)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+		$this->addSql('CREATE TABLE login_sessions (session_hash VARCHAR(191) COMMENT \'(DC2Type:stringkey)\' NOT NULL COLLATE `utf8mb4_general_ci`, uid VARCHAR(4) COMMENT \'(DC2Type:uid)\' NOT NULL COLLATE `utf8mb4_general_ci`, login_moment DATETIME NOT NULL, expire DATETIME NOT NULL, user_agent VARCHAR(255) NOT NULL COLLATE `utf8mb4_general_ci`, ip VARCHAR(255) NOT NULL COLLATE `utf8mb4_general_ci`, lock_ip TINYINT(1) NOT NULL, authentication_method VARCHAR(255) NOT NULL COLLATE `utf8mb4_general_ci`, INDEX IDX_B4C4BD8C539B0606 (uid), PRIMARY KEY(session_hash))');
 		$this->addSql('ALTER TABLE login_sessions ADD CONSTRAINT login_sessions_ibfk_1 FOREIGN KEY (uid) REFERENCES accounts (uid)');
 	}
 }
