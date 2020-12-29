15149d74197b013f35ac3c2e423695b45cc45292
Tim Nederveen <jptned@hotmail.nl>
2020-12-29 17:39:42 +0100

    Haal auto-generate annotatie weg

--- db/doctrine_migrations/Version20201229163542.php
+++ db/doctrine_migrations/Version20201229163542.php
@@ -7,9 +7,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20201229163542 extends AbstractMigration {
 	public function getDescription(): string {
 		return 'Voeg tabellen toe voor CiviMelder';
