5056e300157fd169b07c78ff211663addacca83a
Mark Bekooy <mark.bekooy@gmail.com>
2021-01-14 16:29:48 +0100

    Voeg ketzers pagina en livestream pagina toe

--- lib/controller/DiesController.php
+++ lib/controller/DiesController.php
@@ -21,4 +21,26 @@ class DiesController extends AbstractController
 	public function dies() {
 		return $this->render('dies/index.html.twig');
 	}
+
+	/**
+	 * Ketzers voor dies stek 2021
+	 *
+	 * @return Response
+	 * @Route("/gateway-of-india/ketzers")
+	 * @Auth(P_LOGGED_IN)
+	 */
+	public function ketzers() {
+		return $this->render('dies/ketzers.html.twig');
+	}
+
+	/**
+	 * Livestream voor dies stek 2021
+	 *
+	 * @return Response
+	 * @Route("/gateway-of-india/livestream")
+	 * @Auth(P_LOGGED_IN)
+	 */
+	public function livestream() {
+		return $this->render('dies/livestream.html.twig');
+	}
 }
