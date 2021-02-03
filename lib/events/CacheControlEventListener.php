9fe38d38851bb45a96859a7e95a0058d1bb94de9
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-02-03 11:57:18 +0100

    Zet Cache-Control: no-store op interne requests
    Het lijkt erop dat problemen met automatisch ingloggen worden
    veroorzaakt door de huidige (http) cache policy. Interne pagina's
    mogen nu gecached worden met:
    `Cache-Control: max-age=0, must-revalidate, private`, dit zorgt ervoor
    dat de terugknop in de browser geen nieuwe pagina ophaalt.
    
    Dit in combinatie met een verlopen sessie zorgt ervoor dat de pagina
    geladen wordt zonder dat er een nieuwe sessie aangemaakt wordt. Daarna
    zijn er misschien een paar requests die gedaan moeten worden die
    toevallig niet in de cache zitten, deze requests bevaten een verlopen
    CSRSESSID en een REMEMBERME cookie. De eerste request zorgt voor een
    nieuwe CSRSESSID en REMEMBERME cookie. Daarop volgende requests (die
    er al uitgestuurd zijn) bevatten een verkeerde CSRSESSID (op zich niet
    erg) en een verbruikte REMEMBERME cookie. Een verbruikte REMEMBERME
    cookie zorgt ervoor dat er een error wordt getriggerd en de sessie van
    de gebruiker wordt weggegooid.
    
    De oplossing nu is om `no-store` toe te voegen aan de cache control
    header. Dit zorgt ervoor dat ook bij terug gaan in de browser er een
    nieuwe pagina wordt opgehaald. Hierdoor wordt de REMEMBERME cookie
    ververst in de eerste request en dus klopt deze in verdere requests van
    dezelfde pagina. Dit is alleen nodig bij interne requests omdat bij
    externe requests (niet ingelogd) dit probleem niet bestaat.
    
    Dit fixt ook #815 en de vervelende visuele bug dat draadjes ongelezen
    zijn als je terug gaat naar /forum/recent na het lezen van een draadje.

---
+++ lib/events/CacheControlEventListener.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace CsrDelft\events;
+
+
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\Security\Core\Security;
+
+class CacheControlEventListener
+{
+	/**
+	 * @var Security
+	 */
+	private $security;
+
+	public function __construct(Security $security) {
+		$this->security = $security;
+	}
+
+	public function onKernelResponse(ResponseEvent $event) {
+		// Voorkom caching van interne bestanden, zolang ze geen bestanden zijn.
+		if ($this->security->getUser() && !($event->getResponse() instanceof BinaryFileResponse)) {
+			$event->getResponse()->headers->set('Cache-Control', 'no-store');
+		}
+	}
+
+}
