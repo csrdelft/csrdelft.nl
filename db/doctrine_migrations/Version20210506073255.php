edd56d766e4e0f35a9447204ce87834e27b6750c
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2021-05-06 09:33:21 +0200

    Verplaats remote login migratie naar na de uft8mb4_general_ci migratie

--- db/doctrine_migrations/Version20210506073255.php
+++ db/doctrine_migrations/Version20210506073255.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210505185405 extends AbstractMigration
+final class Version20210506073255 extends AbstractMigration
 {
     public function getDescription(): string
     {
