bf3c648bf78debbd6a995408ef5f3507953777a6
Gerben Oolbekkink <qurben@users.noreply.github.com>
2021-05-07 20:19:31 +0000

    Fix docker setup

--- lib/configuratie.include.php
+++ lib/configuratie.include.php
@@ -49,7 +49,7 @@ date_default_timezone_set('Europe/Amsterdam');
 if ($trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false) {
 	Request::setTrustedProxies(
 		explode(',', $trustedProxies),
-		Request::HEADER_X_FORWARDED_ALL ^ Request::HEADER_X_FORWARDED_HOST
+		Request::HEADER_X_FORWARDED_ALL
 	);
 }
 
