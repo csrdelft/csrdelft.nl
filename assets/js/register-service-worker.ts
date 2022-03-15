67f0bec7ac99e6a18ffcea23ce8781b6305a893d
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-03-15 17:23:35 +0100

    window event load toevoegen

--- assets/js/register-service-worker.ts
+++ assets/js/register-service-worker.ts
@@ -1,5 +1,7 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/sw.js', {
-		scope: '/',
-	});
-}
+window.addEventListener('load', () => {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js', {
+			scope: '/',
+		});
+	}
+});
