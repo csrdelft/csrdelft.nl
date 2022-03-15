9f2c761bd1c9cd30428d8e494ae27dbdc7965f59
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-03-15 01:46:05 +0100

    Offline fallback

--- htdocs/sw.js
+++ htdocs/sw.js
@@ -1,10 +1,59 @@
-self.addEventListener('install', (e) => {
-	console.log('[Service Worker] Install');
+const OFFLINE_VERSION = 1;
+const CACHE_NAME = 'offline';
+const OFFLINE_URL = '/offline.html';
+
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		(async () => {
+			const cache = await caches.open(CACHE_NAME);
+			// Cache 'reload' is om alleen cache van het netwerk te laden
+			await cache.add(new Request(OFFLINE_URL, { cache: 'reload' }));
+		})()
+	);
+
+	// Wacht niet op handmatig registreren
+	self.skipWaiting();
 });
 
-// Voor het laden van cache als de stek offline is: https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
-self.addEventListener('fetch', (e) => {
-	console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+self.addEventListener('activate', (event) => {
+	event.waitUntil(
+		(async () => {
+			if ('navigationPreload' in self.registration) {
+				await self.registration.navigationPreload.enable();
+			}
+		})()
+	);
+
+	self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+	// Kijk of de gebruiker een nieuwe pagina laad
+	if (event.request.mode === 'navigate') {
+		event.respondWith(
+			(async () => {
+				try {
+					const preloadResponse = await event.preloadResponse;
+					if (preloadResponse) {
+						return preloadResponse;
+					}
+
+					// Nog even proberen
+					const networkResponse = await fetch(event.request);
+					return networkResponse;
+				} catch (error) {
+					// Als we hier zijn is er geen netwerkverbinding
+					console.log('Gebruiker is offline: ', error);
+
+					const cache = await caches.open(CACHE_NAME);
+					const cachedResponse = await cache.match(OFFLINE_URL);
+					return cachedResponse;
+				}
+			})()
+		);
+	} else {
+		console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+	}
 });
 
 // Laat een notificatie zien als het een bericht krijgt van de WebPush API
