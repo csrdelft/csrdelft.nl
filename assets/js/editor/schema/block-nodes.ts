be8ea17f37e0eccfb8485edc43c08422f5ba448b
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-05-11 19:57:50 +0200

    Fixes nav PR comments

--- assets/js/editor/schema/block-nodes.ts
+++ assets/js/editor/schema/block-nodes.ts
@@ -57,64 +57,65 @@ const createGroepBlockSpec = (
 				}
 
 				el.innerHTML = '';
-				el.appendChild(html` <div class="bb-groep">
-					<div class="groep-samenvatting">
-						<div class="float-end">
-							<a
-								class="btn"
-								target="_blank"
-								href="/groepen/${type}/${json.id}/wijzigen"
-								title="Wijzig ${json.naam}"
-							>
-								<i class="fas fa-pencil"></i>
-							</a>
-						</div>
-						<h3>${json.naam}</h3>
-						${json.samenvatting_html ?? ''}
-					</div>
+				el.appendChild(html`
+<div class="bb-groep">
+<div class="groep-samenvatting">
+<div class="float-end">
+<a
+class="btn"
+target="_blank"
+href="/groepen/${type}/${json.id}/wijzigen"
+title="Wijzig ${json.naam}"
+>
+<i class="fas fa-pencil"></i>
+</a>
+</div>
+<h3>${json.naam}</h3>
+${json.samenvatting_html ?? ''}
+</div>
 
-					<div id="groep-leden-2152" class="groep-leden">
-						<ul class="groep-tabs nobullets">
-							<li class="geschiedenis">
-								<a class="btn disabled" href="#" title="Bekijk geschiedenis">
-									<i class="fas fa-clock-rotate-left"></i>"></i>
-								</a>
-							</li>
-							<li>
-								<a
-									class="btn btn-primary disabled"
-									href="#"
-									title="Pasfoto's tonen"
-								>
-									<i class="fas fa-user"></i>
-								</a>
-							</li>
-							<li>
-								<a class="btn disabled" href="#" title="Lijst tonen">
-									<i class="fas fa-list"></i>
-								</a>
-							</li>
-							<li>
-								<a class="btn disabled" href="#" title="Statistiek tonen">
-									<i class="fas fa-chart-pie"></i>
-								</a>
-							</li>
-							<li>
-								<a class="btn disabled" href="#" title="E-mails tonen">
-									<i class="fas fa-envelope"></i>
-								</a>
-							</li>
-							<li>
-								<a class="btn disabled" href="#" title="Allergie/dieet tonen">
-									<i class="fas fa-heartbeat"></i>
-								</a>
-							</li>
-							<li class="knop-vergroot"></li>
-						</ul>
-						${ledenDiv}
-					</div>
-					<div class="clear">&nbsp;</div>
-				</div>`);
+<div id="groep-leden-2152" class="groep-leden">
+<ul class="groep-tabs nobullets">
+<li class="geschiedenis">
+<a class="btn disabled" href="#" title="Bekijk geschiedenis">
+<i class="fas fa-clock-rotate-left"></i>"></i>
+</a>
+</li>
+<li>
+<a
+class="btn btn-primary disabled"
+href="#"
+title="Pasfoto's tonen"
+>
+<i class="fas fa-user"></i>
+</a>
+</li>
+<li>
+<a class="btn disabled" href="#" title="Lijst tonen">
+<i class="fas fa-list"></i>
+</a>
+</li>
+<li>
+<a class="btn disabled" href="#" title="Statistiek tonen">
+<i class="fas fa-chart-pie"></i>
+</a>
+</li>
+<li>
+<a class="btn disabled" href="#" title="E-mails tonen">
+<i class="fas fa-envelope"></i>
+</a>
+</li>
+<li>
+<a class="btn disabled" href="#" title="Allergie/dieet tonen">
+<i class="fas fa-heartbeat"></i>
+</a>
+</li>
+<li class="knop-vergroot"></li>
+</ul>
+${ledenDiv}
+</div>
+<div class="clear">&nbsp;</div>
+</div>`);
 			})
 			.catch(() => {
 				el.innerText = 'Groep niet gevonden';
