b50014bd3783f9e95cc718d646157d3b93c573a5
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2022-06-11 12:29:09 +0200

    Verbeter migratie

--- db/doctrine_migrations/Version20220608182201.php
+++ db/doctrine_migrations/Version20220608182201.php
@@ -16,6 +16,27 @@ final class Version20220608182201 extends AbstractMigration
 
 	public function up(Schema $schema): void
 	{
+		$this->addSql("UPDATE cms_paginas SET rechten_bewerken = REPLACE(rechten_bewerken, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE cms_paginas SET rechten_bekijken = REPLACE(rechten_bekijken, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE document SET leesrechten = REPLACE(leesrechten, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE document_categorie SET schrijfrechten = REPLACE(schrijfrechten, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE document_categorie SET leesrechten = REPLACE(leesrechten, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE agenda SET rechten_bekijken = REPLACE(rechten_bekijken, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE forum_delen SET rechten_lezen = REPLACE(rechten_lezen, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE forum_delen SET rechten_modereren = REPLACE(rechten_modereren, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE forum_delen SET rechten_posten = REPLACE(rechten_posten, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE forum_categorien SET rechten_lezen = REPLACE(rechten_lezen, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE menus SET rechten_bekijken = REPLACE(rechten_bekijken, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE groep SET rechten_aanmelden = REPLACE(rechten_aanmelden, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE declaratie_wachtrij SET rechten = REPLACE(rechten, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE mlt_maaltijden SET aanmeld_filter = REPLACE(aanmeld_filter, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE mlt_repetities SET abonnement_filter = REPLACE(abonnement_filter, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_activiteit SET rechten_aanmelden = REPLACE(rechten_aanmelden, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_activiteit SET rechten_lijst_beheren = REPLACE(rechten_lijst_beheren, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_activiteit SET rechten_lijst_bekijken = REPLACE(rechten_lijst_bekijken, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_reeks SET rechten_aanmelden = REPLACE(rechten_aanmelden, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_reeks SET rechten_lijst_beheren = REPLACE(rechten_lijst_beheren, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_reeks SET rechten_lijst_bekijken = REPLACE(rechten_lijst_bekijken, 'P_PUBLIC', 'PUBLIC_ACCESS') WHERE TRUE;");
 		$this->addSql("UPDATE cms_paginas SET rechten_bewerken = REPLACE(rechten_bewerken, 'P_', 'ROLE_') WHERE TRUE;");
 		$this->addSql("UPDATE cms_paginas SET rechten_bekijken = REPLACE(rechten_bekijken, 'P_', 'ROLE_') WHERE TRUE;");
 		$this->addSql("UPDATE document SET leesrechten = REPLACE(leesrechten, 'P_', 'ROLE_') WHERE TRUE;");
@@ -42,6 +63,27 @@ final class Version20220608182201 extends AbstractMigration
 
 	public function down(Schema $schema): void
 	{
+		$this->addSql("UPDATE cms_paginas SET rechten_bewerken = REPLACE(rechten_bewerken, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE cms_paginas SET rechten_bekijken = REPLACE(rechten_bekijken, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE document SET leesrechten = REPLACE(leesrechten, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE document_categorie SET schrijfrechten = REPLACE(schrijfrechten, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE document_categorie SET leesrechten = REPLACE(leesrechten, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE agenda SET rechten_bekijken = REPLACE(rechten_bekijken, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE forum_delen SET rechten_lezen = REPLACE(rechten_lezen, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE forum_delen SET rechten_modereren = REPLACE(rechten_modereren, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE forum_delen SET rechten_posten = REPLACE(rechten_posten, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE forum_categorien SET rechten_lezen = REPLACE(rechten_lezen, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE menus SET rechten_bekijken = REPLACE(rechten_bekijken, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE groep SET rechten_aanmelden = REPLACE(rechten_aanmelden, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE declaratie_wachtrij SET rechten = REPLACE(rechten, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE mlt_maaltijden SET aanmeld_filter = REPLACE(aanmeld_filter, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE mlt_repetities SET abonnement_filter = REPLACE(abonnement_filter, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_activiteit SET rechten_aanmelden = REPLACE(rechten_aanmelden, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_activiteit SET rechten_lijst_beheren = REPLACE(rechten_lijst_beheren, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_activiteit SET rechten_lijst_bekijken = REPLACE(rechten_lijst_bekijken, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_reeks SET rechten_aanmelden = REPLACE(rechten_aanmelden, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_reeks SET rechten_lijst_beheren = REPLACE(rechten_lijst_beheren, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
+		$this->addSql("UPDATE aanmelder_reeks SET rechten_lijst_bekijken = REPLACE(rechten_lijst_bekijken, 'PUBLIC_ACCESS', 'P_PUBLIC') WHERE TRUE;");
 		$this->addSql("UPDATE cms_paginas SET rechten_bewerken = REPLACE(rechten_bewerken, 'ROLE_', 'P_') WHERE TRUE;");
 		$this->addSql("UPDATE cms_paginas SET rechten_bekijken = REPLACE(rechten_bekijken, 'ROLE_', 'P_') WHERE TRUE;");
 		$this->addSql("UPDATE document SET leesrechten = REPLACE(leesrechten, 'ROLE_', 'P_') WHERE TRUE;");
