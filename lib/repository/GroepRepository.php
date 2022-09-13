b21f01e1c80a75f3b1b802ecab07a53e0cde9679
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2022-09-13 09:51:58 +0200

    Als er een specifieke role wordt gevraagd willen we alleen de h.t. variant van de groep hebben.

--- lib/repository/GroepRepository.php
+++ lib/repository/GroepRepository.php
@@ -121,7 +121,9 @@ abstract class GroepRepository extends AbstractRepository
 
 			if ($role) {
 				$qb = $qb
+					->andWhere('groep.status = :status')
 					->andWhere('leden.opmerking = :role')
+					->setParameter('status', GroepStatus::HT)
 					->setParameter('role', $role);
 			}
 
