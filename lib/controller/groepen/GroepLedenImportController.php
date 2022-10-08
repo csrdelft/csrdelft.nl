0f197e526374cbdec15b0495b63a10275e0eeade
Tim Nederveen <ik@tim365.nl>
2022-10-08 22:59:26 +0200

    Fix Prettier

--- lib/controller/groepen/GroepLedenImportController.php
+++ lib/controller/groepen/GroepLedenImportController.php
@@ -82,15 +82,13 @@ class GroepLedenImportController extends AbstractController
 		if (empty($data)) {
 			return $this->quickMelding('Geen regels gevonden');
 		}
-		if (
-			array_keys($data[0]) !== ['groepID', 'uid', 'opmerking']
-		) {
+		if (array_keys($data[0]) !== ['groepID', 'uid', 'opmerking']) {
 			return $this->quickMelding('Ongeldige kolommen in de CSV');
 		}
 
 		// Sla data op in sessie
 		$key = uniqid();
-		$session->set("groepimport-{$key}", $data);
+		$session->set("groepimport-$key", $data);
 
 		// Redirect naar check pagina
 		return $this->redirectToRoute('groepimport_controle', ['key' => $key]);
@@ -114,16 +112,24 @@ class GroepLedenImportController extends AbstractController
 		$groepRepository = $em->getRepository(Groep::class);
 
 		// Haal data op
-		$data = $session->get("groepimport-{$key}");
+		$data = $session->get("groepimport-$key");
 		if (empty($data)) {
-			return $this->quickMelding('Er ging iets fout bij het inladen van de CSV');
+			return $this->quickMelding(
+				'Er ging iets fout bij het inladen van de CSV'
+			);
 		}
 
 		// Ga regels langs
-		$groeplidRegels = GroepLedenImportDTO::convert($profielRepository, $groepRepository, $data);
-		$aantalSucces = count(array_filter($groeplidRegels, function(GroepLedenImportDTO $dto) {
-			return $dto->succes;
-		}));
+		$groeplidRegels = GroepLedenImportDTO::convert(
+			$profielRepository,
+			$groepRepository,
+			$data
+		);
+		$aantalSucces = count(
+			array_filter($groeplidRegels, function (GroepLedenImportDTO $dto) {
+				return $dto->succes;
+			})
+		);
 		$aantalGefaald = count($groeplidRegels) - $aantalSucces;
 
 		// Overzicht tonen
@@ -158,23 +164,25 @@ class GroepLedenImportController extends AbstractController
 		$groepRepository = $em->getRepository(Groep::class);
 
 		// Haal data op
-		$data = $session->get("groepimport-{$key}");
+		$data = $session->get("groepimport-$key");
 		if (empty($data)) {
-			return $this->quickMelding('Er ging iets fout bij het inladen van de CSV');
+			return $this->quickMelding(
+				'Er ging iets fout bij het inladen van de CSV'
+			);
 		}
 
 		// Zet lock
-		if ($session->has("groepimport-{$key}-locked")) {
+		if ($session->has("groepimport-$key-locked")) {
 			return $this->quickMelding('Deze CSV wordt al verwerkt');
 		} else {
-			$session->set("groepimport-{$key}-locked", true);
+			$session->set("groepimport-$key-locked", true);
 		}
 
 		if (
 			!$request->request->has('gecheckt') ||
 			!$request->request->has('foutenAkkoord')
 		) {
-			$session->remove("groepimport-{$key}-locked");
+			$session->remove("groepimport-$key-locked");
 			return $this->quickMelding(
 				'Geef akkoord voor verwerking',
 				$this->generateUrl('groepimport_controle', ['key' => $key])
@@ -187,7 +195,11 @@ class GroepLedenImportController extends AbstractController
 		$em->getConnection()->beginTransaction();
 		try {
 			foreach ($data as $regel) {
-				$regel = new GroepLedenImportDTO($profielRepository, $groepRepository, $regel);
+				$regel = new GroepLedenImportDTO(
+					$profielRepository,
+					$groepRepository,
+					$regel
+				);
 				$aangemaakt = $regel->aanmelden($groepLidRepository);
 				if ($aangemaakt) {
 					$aantalSucces++;
@@ -195,12 +207,12 @@ class GroepLedenImportController extends AbstractController
 			}
 
 			$em->getConnection()->commit();
-			$session->remove("groepimport-{$key}");
+			$session->remove("groepimport-$key");
 		} catch (Exception $e) {
 			$em->getConnection()->rollBack();
 		}
 
-		$session->remove("groepimport-{$key}-lock");
+		$session->remove("groepimport-$key-lock");
 
 		// Overzicht tonen
 		return $this->render('groepen/groepimport-succes.html.twig', [
--- lib/entity/groepen/GroepLedenImportDTO.php
+++ lib/entity/groepen/GroepLedenImportDTO.php
@@ -11,14 +11,17 @@ class GroepLedenImportDTO
 {
 	public $succes = false;
 	private $lid;
-	public $lidnaam = "";
+	public $lidnaam = '';
 	private $groep;
-	public $groepnaam = "";
+	public $groepnaam = '';
 	public $opmerking = null;
 	public $waarschuwingen = [];
 
-	public function __construct(ProfielRepository $profielRepository, EntityRepository $groepRepository, array $regel)
-	{
+	public function __construct(
+		ProfielRepository $profielRepository,
+		EntityRepository $groepRepository,
+		array $regel
+	) {
 		// Check keys
 		if (array_keys($regel) !== ['groepID', 'uid', 'opmerking']) {
 			$this->succes = false;
@@ -27,7 +30,9 @@ class GroepLedenImportDTO
 		}
 
 		// Haal lid op
-		$this->lid = $profielRepository->find((strlen($regel['uid']) === 3 ? '0' : '') . $regel['uid']);
+		$this->lid = $profielRepository->find(
+			(strlen($regel['uid']) === 3 ? '0' : '') . $regel['uid']
+		);
 		if (!$this->lid) {
 			$this->waarschuwingen[] = "Profiel {$regel['uid']} niet gevonden";
 		} else {
@@ -43,12 +48,12 @@ class GroepLedenImportDTO
 
 			// Check rechten
 			if (!$this->groep->mag(AccessAction::Beheren())) {
-				$this->waarschuwingen[] = "Geen rechten om groep te beheren";
+				$this->waarschuwingen[] = 'Geen rechten om groep te beheren';
 			}
 
 			// Check of lid al in groep zit
 			if ($this->groep->getLid($this->lid->uid)) {
-				$this->waarschuwingen[] = "Lid zit al in groep";
+				$this->waarschuwingen[] = 'Lid zit al in groep';
 			}
 		}
 
@@ -56,7 +61,7 @@ class GroepLedenImportDTO
 		if (!empty($regel['opmerking'])) {
 			$this->opmerking = $regel['opmerking'];
 			if (strlen($this->opmerking) > 255) {
-				$this->waarschuwingen[] = "Opmerking is te lang";
+				$this->waarschuwingen[] = 'Opmerking is te lang';
 			}
 		}
 
@@ -69,18 +74,31 @@ class GroepLedenImportDTO
 	 * @param array $data
 	 * @return GroepLedenImportDTO[]
 	 */
-	public static function convert(ProfielRepository $profielRepository, EntityRepository $groepRepository, array $data): array
-	{
-		return array_map(function(array $regel) use ($profielRepository, $groepRepository) {
-			return new GroepLedenImportDTO($profielRepository, $groepRepository, $regel);
-		}, $data);
+	public static function convert(
+		ProfielRepository $profielRepository,
+		EntityRepository $groepRepository,
+		array $data
+	): array {
+		return array_map(function (array $regel) use (
+			$profielRepository,
+			$groepRepository
+		) {
+			return new GroepLedenImportDTO(
+				$profielRepository,
+				$groepRepository,
+				$regel
+			);
+		},
+		$data);
 	}
 
-	public function waarschuwing(): string {
+	public function waarschuwing(): string
+	{
 		return join('<br />', $this->waarschuwingen);
 	}
 
-	public function aanmelden(GroepLidRepository $groepLidRepository): bool {
+	public function aanmelden(GroepLidRepository $groepLidRepository): bool
+	{
 		if ($this->succes) {
 			$groeplid = $groepLidRepository->nieuw($this->groep, $this->lid->uid);
 			if ($this->opmerking) {
