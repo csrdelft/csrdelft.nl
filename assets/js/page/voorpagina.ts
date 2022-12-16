8d98c5aafd71c5b96e28adcc0fcc0f325359869a
Sebastiaan Benjamins <45468731+cbasje@users.noreply.github.com>
2022-12-16 15:07:10 +0100

    Maak auto carousel indicators aan op voorpagina

--- assets/js/page/voorpagina.ts
+++ assets/js/page/voorpagina.ts
@@ -1,4 +1,19 @@
+import { select, selectAll } from '../lib/dom';
 import { laadForumIds, slaOpForumIds } from '../lib/forum';
 
 slaOpForumIds('.cell-forum > div');
 laadForumIds();
+
+// Carousel indicators
+const indicatorContainer = select('.carousel-indicators');
+const indicatorButton = select('.carousel-indicators > button');
+const carouselItems = selectAll('.carousel-inner > *');
+
+for (let n = 1; n < carouselItems.length; n++) {
+    let clone = indicatorButton.cloneNode(true) as HTMLElement;
+    clone.setAttribute('data-bs-slide-to', String(n));
+    clone.setAttribute('aria-current', String(false));
+    clone.setAttribute('aria-label', `Slide ${n+1}`);
+    clone.classList.remove('active');
+    indicatorContainer.appendChild(clone);
+}
