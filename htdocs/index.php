0f65229e08f218f901600731a84a030415f61bce
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2023-02-15 20:01:57 +0100

    Automatisch verversen bij onderhoud

--- htdocs/index.php
+++ htdocs/index.php
@@ -14,6 +14,7 @@ if (file_exists(__DIR__ . '/../.onderhoud')) {
 <title>C.S.R. Delft - Onderhoud</title>
 <meta charset=utf-8>
 <meta name=viewport content="width=device-width, initial-scale=1.0">
+<meta http-equiv="refresh" content="5">
 
 <body style="font-family: sans-serif; text-align: center;">
 <h1>Onderhoud</h1>
