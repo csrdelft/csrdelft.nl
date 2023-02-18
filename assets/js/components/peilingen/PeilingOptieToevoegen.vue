8212a4ae1cc98a44a48353ee2dde5cc96b28de71
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2023-02-18 14:50:16 +0100

    Vue peilingen naar props
    De oude aanpak is niet compatible met Vue 3, dus als we verder willen moeten we via properties based components

--- assets/js/components/peilingen/PeilingOptieToevoegen.vue
+++ assets/js/components/peilingen/PeilingOptieToevoegen.vue
@@ -1,47 +1,52 @@
 <template>
   <div>
-    <a @click="toevoegen"> <Icon :icon="icon" /> {{ text }} </a>
+    <a @click="toevoegen">
+      <Icon :icon="icon" />
+      {{ text }}
+    </a>
   </div>
 </template>
 
 <script lang="ts">
 import axios from 'axios';
 import Vue from 'vue';
-import { Component, Prop } from 'vue-property-decorator';
 import { domUpdate } from '../../lib/domUpdate';
 import Icon from '../common/Icon.vue';
 
-@Component({
+export default Vue.extend({
   components: { Icon },
-})
-export default class PeilingOptieToevoegen extends Vue {
-  icon = 'plus';
-  text = 'Optie toevoegen';
-
-  @Prop({
-    type: Number,
-  })
-  id: number;
-
-  private get optieToevoegenUrl() {
-    return `/peilingen/opties/${this.id}/toevoegen`;
-  }
-
-  private toevoegen(event: MouseEvent) {
-    event.preventDefault();
-    this.icon = 'spinner fa-spin';
-    axios
-      .post(this.optieToevoegenUrl.toString())
-      .then((response) => {
-        domUpdate(response.data);
-        this.icon = 'plus';
-      })
-      .catch(() => {
-        this.icon = 'ban';
-        this.text = 'Mag geen optie meer toevoegen';
-      });
-  }
-}
+  props: {
+    id: {
+      default: 0,
+      type: Number,
+    },
+  },
+  data: () => ({
+    icon: 'plus',
+    text: 'Optie toevoegen',
+  }),
+  computed: {
+    optieToevoegenUrl() {
+      return `/peilingen/opties/${this.id}/toevoegen`;
+    },
+  },
+  methods: {
+    toevoegen(event: MouseEvent) {
+      event.preventDefault();
+      this.icon = 'spinner fa-spin';
+      axios
+        .post(this.optieToevoegenUrl.toString())
+        .then((response) => {
+          domUpdate(response.data);
+          this.icon = 'plus';
+        })
+        .catch(() => {
+          this.icon = 'ban';
+          this.text = 'Mag geen optie meer toevoegen';
+        });
+    },
+  },
+});
 </script>
 
 <style scoped></style>
