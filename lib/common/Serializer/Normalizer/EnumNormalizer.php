ffc96cab1d5a8da830c68f82f811393f98c276ce
Nathan Huisman <huis.nathan@gmail.com>
2024-05-08 18:33:14 +0200

    Fix normalizers

--- lib/common/Serializer/Normalizer/EnumNormalizer.php
+++ lib/common/Serializer/Normalizer/EnumNormalizer.php
@@ -17,7 +17,7 @@ class EnumNormalizer implements NormalizerInterface
 	 * @param array $context
 	 * @return array|ArrayObject|bool|float|int|string|null
 	 */
-	public function normalize($enum, string $format = null, array $context = [])
+	public function normalize($enum, string $format = null, array $context = []): string|int|float|bool|\ArrayObject|array|null
 	{
 		return $enum->getDescription();
 	}
@@ -28,5 +28,12 @@ class EnumNormalizer implements NormalizerInterface
 		array $context = []
 	): bool {
 		return $data instanceof Enum;
+	}
+
+	public function getSupportedTypes(?string $format): array
+	{
+		return [
+			"object" => true
+		];
 	}
 }
