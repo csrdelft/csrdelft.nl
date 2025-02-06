df61b34a1c1440798cb8a8b17c59e661a40c08b5
Nathan Huisman <36598276+NathanHuisman@users.noreply.github.com>
2025-02-06 13:19:24 +0100

    Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,108 +1,108 @@
-const path = require("path");
+const path = require('path');
 
-const contextPath = path.resolve(__dirname, "assets");
+const contextPath = path.resolve(__dirname, 'assets');
 
-const terserPlugin = require("terser-webpack-plugin");
-const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
-const RemoveEmptyScriptsPlugin = require("webpack-remove-empty-scripts");
-const { VueLoaderPlugin } = require("vue-loader");
-const MomentLocalesPlugin = require("moment-locales-webpack-plugin");
-const WebpackAssetsManifest = require("webpack-assets-manifest");
-const ESLintPlugin = require("eslint-webpack-plugin");
+const terserPlugin = require('terser-webpack-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
+const { VueLoaderPlugin } = require('vue-loader');
+const MomentLocalesPlugin = require('moment-locales-webpack-plugin');
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
 module.exports = (env, argv) => {
 	let styleEntries = {
-		"thema-civitasia": "./scss/thema/civitasia.scss",
-		"thema-dies": "./scss/thema/dies.scss",
-		"thema-lustrum": "./scss/thema/lustrum.scss",
-		"thema-normaal": "./scss/thema/normaal.scss",
-		"thema-owee": "./scss/thema/owee.scss",
-		"thema-roze": "./scss/thema/roze.scss",
-		"thema-koevoet": "./scss/thema/Koevoet.scss",
-		"thema-sineregno": "./scss/thema/sineregno.scss",
+		'thema-civitasia': './scss/thema/civitasia.scss',
+		'thema-dies': './scss/thema/dies.scss',
+		'thema-lustrum': './scss/thema/lustrum.scss',
+		'thema-normaal': './scss/thema/normaal.scss',
+		'thema-owee': './scss/thema/owee.scss',
+		'thema-roze': './scss/thema/roze.scss',
+		'thema-koevoet': './scss/thema/Koevoet.scss',
+		'thema-sineregno': './scss/thema/sineregno.scss',
 	};
 
 	if (process.env.THEMA in styleEntries) {
-		console.log("Maak alleen thema: " + process.env.THEMA);
+		console.log('Maak alleen thema: ' + process.env.THEMA);
 
 		styleEntries = { [process.env.THEMA]: styleEntries[process.env.THEMA] };
 	}
 
 	return {
-		mode: "development",
+		mode: 'development',
 		context: contextPath,
 		entry: {
-			app: "./js/entry/app.ts",
-			sentry: "./js/entry/sentry.ts",
-			ledenmemory: "./js/entry/ledenmemory.ts",
-			fxtrein: "./js/entry/fxtrein.ts",
-			fxraket: "./js/entry/fxraket.ts",
-			fxdruif: "./js/entry/fxdruif.ts",
-			fxminion: "./js/entry/fxminion.ts",
-			extern: ["./js/entry/extern.ts", "./scss/extern.scss"],
-			bredeletters: "./scss/bredeletters.scss",
-			common: "./scss/common.scss",
-			"extern-forum": "./scss/extern-forum.scss",
-			"extern-fotoalbum": "./scss/extern-fotoalbum.scss",
-			"extern-owee": ["./js/entry/extern-owee.ts", "./scss/extern-owee.scss"],
-			maaltijdlijst: "./scss/maaltijdlijst.scss",
+			app: './js/entry/app.ts',
+			sentry: './js/entry/sentry.ts',
+			ledenmemory: './js/entry/ledenmemory.ts',
+			fxtrein: './js/entry/fxtrein.ts',
+			fxraket: './js/entry/fxraket.ts',
+			fxdruif: './js/entry/fxdruif.ts',
+			fxminion: './js/entry/fxminion.ts',
+			extern: ['./js/entry/extern.ts', './scss/extern.scss'],
+			bredeletters: './scss/bredeletters.scss',
+			common: './scss/common.scss',
+			'extern-forum': './scss/extern-forum.scss',
+			'extern-fotoalbum': './scss/extern-fotoalbum.scss',
+			'extern-owee': ['./js/entry/extern-owee.ts', './scss/extern-owee.scss'],
+			maaltijdlijst: './scss/maaltijdlijst.scss',
 			// Donker wordt altijd geladen
-			"thema-donker": "./scss/thema/donker.scss",
+			'thema-donker': './scss/thema/donker.scss',
 			...styleEntries,
-			"effect-civisaldo": "./scss/effect/civisaldo.scss",
+			'effect-civisaldo': './scss/effect/civisaldo.scss',
 			// lustrum-page related scss
-			lustrum: "./scss/lustrum12/style.scss",
-			lustrumthema: "./scss/lustrum12/thema.scss",
-			lustrumweek: "./scss/lustrum12/lustrumweek.scss",
-			lustrumweek2: "./scss/lustrum12/lustrumweek2.scss",
-			lustrummerch: "./scss/lustrum12/dikkemerch.scss",
-			lustrumdiesthema: "./scss/lustrum12/diesthema.scss",
-			lustrumdies: "./scss/lustrum12/dies.scss",
-			lustrumreis: "./scss/lustrum12/lustrumreis.scss",
-			dies2023: "./scss/dies2023/dies2023.scss",
-			dies2024: "./scss/dies2024/dies2024.scss",
-			dies2025: "./scss/dies2025/dies2025.scss",
+			lustrum: './scss/lustrum12/style.scss',
+			lustrumthema: './scss/lustrum12/thema.scss',
+			lustrumweek: './scss/lustrum12/lustrumweek.scss',
+			lustrumweek2: './scss/lustrum12/lustrumweek2.scss',
+			lustrummerch: './scss/lustrum12/dikkemerch.scss',
+			lustrumdiesthema: './scss/lustrum12/diesthema.scss',
+			lustrumdies: './scss/lustrum12/dies.scss',
+			lustrumreis: './scss/lustrum12/lustrumreis.scss',
+			dies2023: './scss/dies2023/dies2023.scss',
+			dies2024: './scss/dies2024/dies2024.scss',
+			dies2025: './scss/dies2025/dies2025.scss',
 		},
 		output: {
 			// De map waarin alle bestanden geplaatst worden.
-			path: path.resolve(__dirname, "htdocs/dist"),
+			path: path.resolve(__dirname, 'htdocs/dist'),
 			// Alle javascript bestanden worden in de map js geplaatst.
 			filename:
-				argv.mode !== "production"
-					? "js/[name].bundle.js"
-					: "js/[name].[contenthash].bundle.js",
+				argv.mode !== 'production'
+					? 'js/[name].bundle.js'
+					: 'js/[name].[contenthash].bundle.js',
 			chunkFilename:
-				argv.mode !== "production"
-					? "js/[name].chunk.js"
-					: "js/[name].[contenthash].chunk.js",
-			publicPath: "/dist/",
-			assetModuleFilename: "images/[hash][ext][query]",
+				argv.mode !== 'production'
+					? 'js/[name].chunk.js'
+					: 'js/[name].[contenthash].chunk.js',
+			publicPath: '/dist/',
+			assetModuleFilename: 'images/[hash][ext][query]',
 		},
-		devtool: "source-map",
+		devtool: 'source-map',
 		resolve: {
 			// Vanuit javascript kun je automatisch .js en .ts bestanden includen.
-			extensions: [".ts", ".js"],
+			extensions: ['.ts', '.js'],
 			fallback: {
 				stream: false,
 				util: false,
 			},
 		},
 		cache: {
-			type: "filesystem",
+			type: 'filesystem',
 		},
 		optimization: {
 			minimizer: [new OptimizeCssAssetsWebpackPlugin({}), new terserPlugin()],
 			splitChunks: {
-				chunks: "all",
+				chunks: 'all',
 			},
 		},
 		plugins: [
-			new (require("mini-css-extract-plugin"))({
+			new (require('mini-css-extract-plugin'))({
 				// Css bestanden komen in de map css terecht.
 				filename:
-					argv.mode !== "production"
-						? "css/[name].css"
-						: "css/[name].[contenthash].css",
+					argv.mode !== 'production'
+						? 'css/[name].css'
+						: 'css/[name].[contenthash].css',
 			}),
 			new RemoveEmptyScriptsPlugin(),
 			new VueLoaderPlugin(),
@@ -113,7 +113,7 @@ module.exports = (env, argv) => {
 				entrypointsUseAssets: true,
 			}),
 			new MomentLocalesPlugin({
-				localesToKeep: ["nl"],
+				localesToKeep: ['nl'],
 			}),
 		],
 		module: {
@@ -124,7 +124,7 @@ module.exports = (env, argv) => {
 					test: /\.ts$/,
 					use: [
 						{
-							loader: "ts-loader",
+							loader: 'ts-loader',
 							options: {
 								appendTsSuffixTo: [/\.vue$/],
 								// We compilen jgallery ts
@@ -135,7 +135,7 @@ module.exports = (env, argv) => {
 				},
 				{
 					test: /\.vue$/,
-					loader: "vue-loader",
+					loader: 'vue-loader',
 				},
 				// Verwerk sass bestanden.
 				// `sass-loader` >
@@ -152,32 +152,32 @@ module.exports = (env, argv) => {
 					test: /\.scss$/,
 					use: [
 						{
-							loader: require("mini-css-extract-plugin").loader,
+							loader: require('mini-css-extract-plugin').loader,
 							options: {
 								// De css bestanden zitten in de css map, / is dus te vinden op ../
-								publicPath: "../",
+								publicPath: '../',
 							},
 						},
 						{
-							loader: "css-loader",
+							loader: 'css-loader',
 							options: {
 								importLoaders: 3,
 							},
 						},
 						{
-							loader: "postcss-loader",
+							loader: 'postcss-loader',
 							options: {
 								postcssOptions: {
-									plugins: [require("autoprefixer")],
+									plugins: [require('autoprefixer')],
 								},
 							},
 						},
 						{
-							loader: "resolve-url-loader",
+							loader: 'resolve-url-loader',
 							options: {},
 						},
 						{
-							loader: "sass-loader",
+							loader: 'sass-loader',
 							options: {
 								// Source maps moeten aan staan om `resolve-url-loader` te laten werken.
 								sourceMap: true,
@@ -187,22 +187,22 @@ module.exports = (env, argv) => {
 				},
 				{
 					test: /\.css$/,
-					use: ["style-loader", "css-loader"],
+					use: ['style-loader', 'css-loader'],
 				},
 				// Sla fonts op in de fonts map.
 				{
 					test: /\.(woff|woff2|eot|ttf|otf)$/,
-					type: "asset",
+					type: 'asset',
 					generator: {
-						filename: "fonts/[hash][ext][query]",
+						filename: 'fonts/[hash][ext][query]',
 					},
 				},
 				// Sla plaetjes/videos op in de images map.
 				{
 					test: /\.(png|svg|jpg|gif|webm|mp4)$/,
-					type: "asset/resource",
+					type: 'asset/resource',
 					generator: {
-						filename: "images/[hash][ext][query]",
+						filename: 'images/[hash][ext][query]',
 					},
 				},
 			],
